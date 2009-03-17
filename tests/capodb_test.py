dff4ed1fd0dd35e8625fd372c6352016611069b3
Aaron Parsons <aparsons@astron.berkeley.edu>
2009-03-17 11:35:55 -0400

    ARP: changed capodb_test.py to use unittest.

--- tests/capodb_test.py
+++ tests/capodb_test.py
@@ -6,28 +6,28 @@
 #  Created by Danny Jacobs on 3/12/09.
 #  Copyright (c) 2009 __MyCompanyName__. All rights reserved.
 #
-import capodb as cdb, sys, aipy as a,numpy as n
+import unittest
+import capodb as cdb, aipy as a, numpy as n
 
-lat, lon = '45:00', '90:00'
-freqs = a.loc.get_freqs(0.1,0.1/256,256)
-beam = a.fit.BeamFlat(freqs)
-aa = cdb.AntennaArray((lat,lon), [],beam=beam)
-prms = aa.get_params()
+class TestCapodb(unittest.TestCase):
+    def setUp(self):
+        lat, lon = '45:00', '90:00'
+        freqs = a.loc.get_freqs(0.1,0.1/256,256)
+        beam = a.fit.BeamFlat(freqs)
+        self.aa = cdb.AntennaArray((lat,lon), [],beam=beam)
+    def testdbinit(self):
+        """Testing initialization from paper_test1.db"""
+        prms = self.aa.get_params()
+        self.aa[0].set_params(prms[0])
+        self.assertEqual(prms[0]['x'], 8.98)
+    def testsavereload(self):
+        """Testing save/reload: """
+        oldamp = self.aa[0].amp
+        self.aa[0].amp += 1
+        self.aa[0].save(self.aa.c,self.aa.aa_db_params)
+        self.aa[0].prm_update(self.aa.c,self.aa.aa_db_params)
+        prms2 = self.aa.get_params()
+        self.assertAlmostEqual(self.aa[0].amp, oldamp+1, 10)
 
-aa[0].set_params(prms[0])
-print "Testing initialization from paper_test1.db: ",
-if prms[0]['x']==8.98:
-   print "Passed"
-else: print "Failed"; sys.exit()
-
-print "Testing save/reload: ",
-oldamp = aa[0].amp
-
-aa[0].amp += 1
-aa[0].save(aa.c,aa.aa_db_params)
-aa[0].prm_update(aa.c,aa.aa_db_params)
-prms2 = aa.get_params()
-#print prms[0],prms2[0],aa[0].amp-oldamp-1
-if n.abs(aa[0].amp-oldamp-1)<10**-10:
-   print "Passed"
-else: print "Failed"; sys.exit()
+if __name__ == '__main__':
+    unittest.main()
