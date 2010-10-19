ac6ecd098eae09916d43eea06370b2f6d96c7f90
Danny Jacobs <jacobsda@sas.upenn.edu>
2010-10-19 14:42:40 -0600

    Indexing doesnt work, try multiplying.

--- plot_map.py
+++ plot_map.py
@@ -237,7 +237,7 @@ for i,file in enumerate(args):
     map.drawmeridians(n.arange(-180, 180, 30))
     #if not opts.proj.startswith('ortho'): map.drawparallels(n.arange(-90,90,30)[1:], labels=[0,1,0,0], labelstyle='+/-')
     # Set up data to plot
-    if not opts.blank is None: data[n.argwhere(data<opts.blank)] = 0
+    if not opts.blank is None: data *= n.where(data<opts.blank,0,1)
     if opts.mode.startswith('log'): data = n.log10(n.abs(data))
     elif opts.mode.startswith('atan'): data = n.arctan(data)
     if opts.max is None: max = data.max()
