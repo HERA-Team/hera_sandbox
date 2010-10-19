220877e1cb91208084abc2cfa5ede4bd66e7c41b
Danny Jacobs <jacobsda@sas.upenn.edu>
2010-10-19 09:33:06 -0600

    forgot the snap option. duh

--- batch_mk_img_snap.sh
+++ batch_mk_img_snap.sh
@@ -14,5 +14,5 @@ SNAP=168
 #echo "mk_img.py --use_head --altmin=60 --skip=${SKIP} --cnt=${CNT} --facet_dec=-90_10  -c 144_370,475_900 --size=400 --res=0.4 -pxx -C psa331_v009_gc --fmt=${FILE}_%04d $FILE"
 #mk_img.py --use_head --altmin=60 --skip=${SKIP} --cnt=${CNT} --facet_dec=-90_10  -c 144_370,475_900 --size=400 --res=0.4 -pxx -C psa331_v009_gc --fmt=${FILE}_%04d $FILE
 
-echo "mk_img.py -a cross,-24 --minuv=20 --altmin=30 -s zen --size=400 --res=0.4 -pxx -C psa455_v003_gc --fmt=${FILE}_%s_%04d $FILE"
-mk_img.py -a cross,-24 --minuv=20 --altmin=30 -s zen --size=400 --res=0.4 -pxx -C psa455_v003_gc --fmt=${FILE}_%s_%04d $FILE
+echo "mk_img.py -a cross,-24 --minuv=20 --altmin=30 --snap=${SNAP} -s zen --size=400 --res=0.4 -pxx -C psa455_v003_gc --fmt=${FILE}_%s_%04d $FILE"
+mk_img.py -a cross,-24 --minuv=20 --altmin=30 -s zen --snap=${SNAP} --size=400 --res=0.4 -pxx -C psa455_v003_gc --fmt=${FILE}_%s_%04d $FILE
