2f284560a518b74e7148744d292e2e46a79f8d05
Danny Jacobs <jacobsda@sas.upenn.edu>
2010-11-02 09:57:25 -0600

    Upgrades to table_to_vot.

--- table_to_vot.py
+++ table_to_vot.py
@@ -8,7 +8,7 @@
 #
 
 import aipy as a, numpy as n, pylab as p,math as m
-import sys, optparse,atpy
+import sys, optparse,atpy,ephem as e
 
 o = optparse.OptionParser()
 #a.scripting.add_standard_options(o, cal=True)
@@ -16,16 +16,42 @@ o.add_option('--add',type=str,
     help="Add variables to be applied across the board. ie --add mfreq:0.15,seq:100")
 opts, args = o.parse_args(sys.argv[1:])
 
-
+types = {'ra':str,'dec':str,'name':str,'npix':int,'_ra':float,'_dec':float,
+'s_nu_':float,'s_nu_int_':float,'dr':float}
+ucds = {"pos.eq.ra;meta.main":'_Ra',
+        "pos.eq.dec;meta.main":'_Dec',
+        "phot.flux.density":'S_nu_',
+        "stat.error":'e_S_nu_',
+        "meta.id;meta.main":'Name',
+        "meta.record":'Seq',
+        "spect.index":'n',
+        "em.freq":'nu'}
+ucds = dict(zip(ucds.values(),ucds.keys()))
 for file in args:
     lines = open(file).readlines()
+    names = [s.strip() for s in lines[0][1:].split('\t')]
     t = atpy.Table()
-    t.add_column('Name',[s.split('\t')[0].strip() for s in lines if not s.startswith('#')],dtype=n.str)
-    t.add_column('Ra',[s.split('\t')[1].strip() for s in lines if not s.startswith('#')],dtype=n.str)
-    t.add_column('Dec',[s.split('\t')[2].strip() for s in lines if not s.startswith('#')],dtype=n.str)
-    t.add_column('S_nu_',[s.split('\t')[3].strip() for s in lines if not s.startswith('#')],dtype='<f8')
-    try: t.add_column('e_S_nu_',[s.split('\t')[4].strip() for s in lines if not s.startswith('#')],dtype='<f8')
-    except(IndexError): t.add_column('e_S_nu_',['0' for s in lines if not s.startswith('#')],dtype='<f8')
+    for i,name in enumerate(names):
+        col = [s.split('\t')[i].strip() for s in lines if not s.startswith('#')]
+        try: 
+            float(col[0])
+            tipe = '<f8'
+        except(ValueError):tipe = n.str
+        if name in ucds.keys():
+            ucd = ucds[name]
+        else: ucd=None
+        t.add_column(name,col,dtype=tipe,ucd=ucd)
+        if name.lower()=='ra':
+            t.add_column('_Ra',[e.hours(s)*180/n.pi for s in col],dtype='<f8',ucd="pos.eq.ra;meta.main")    
+        if name.lower()=='dec':
+            t.add_column('_Dec',[e.degrees(s)*180/n.pi for s in col],dtype='<f8',ucd="pos.eq.dec;meta.main")    
+
+#    t.add_column('Name',[s.split('\t')[0].strip() for s in lines if not s.startswith('#')],dtype=n.str)
+#    t.add_column('Ra',[s.split('\t')[1].strip() for s in lines if not s.startswith('#')],dtype=n.str)
+#    t.add_column('Dec',[s.split('\t')[2].strip() for s in lines if not s.startswith('#')],dtype=n.str)
+#    t.add_column('S_nu_',[s.split('\t')[3].strip() for s in lines if not s.startswith('#')],dtype='<f8')
+#    try: t.add_column('e_S_nu_',[s.split('\t')[4].strip() for s in lines if not s.startswith('#')],dtype='<f8')
+#    except(IndexError): t.add_column('e_S_nu_',['0' for s in lines if not s.startswith('#')],dtype='<f8')
     if not opts.add is None:
         adds = opts.add.split(',')
         for add in adds:
