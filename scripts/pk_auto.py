d174d3854c8688a19cfacdf686e5412d1fe52dba
Aaron Parsons <aparsons@astron.berkeley.edu>
2010-12-05 21:18:06 -0800

    Cleaned things up in pk_auto.py and ported fully to use capo.

--- scripts/pk_auto.py
+++ scripts/pk_auto.py
@@ -9,22 +9,18 @@ o.add_option('--clean', dest='clean', type='float', default=1e-3,
     help='Deconvolve delay-domain data by the response that results from flagged data.  Specify a tolerance for termination (usually 1e-2 or 1e-3).')
 opts, args = o.parse_args(sys.argv[1:])
 
-#MIN_CH,MAX_CH,SUBBAND = 150,900,50
-MIN_CH,MAX_CH,SUBBAND = 250,750,50
-#MIN_CH,MAX_CH,SUBBAND = 300,1800,50
+MIN_CH,MAX_CH,SUBBAND = 250,750,50  # for 1024ch GB data
 NTAPS = 3
 
 #SCALE = 30e3
 SCALE = 6*30e3
-FoV = 0.76
-X2Y = 730.
+DO_K3PK = False
 
 #DECONV1WIDTH = 5
 DECONV1WIDTH = 10
 #NSIG = 2
 NSIG = 1.5
 #NSIG = 1
-PLOT1 = False
 PLOT2 = False
 
 # Read data from npz files (which are already averaged over an hour)
@@ -32,60 +28,49 @@ dat,wgt = {},{}
 for filename in args:
     print 'Reading', filename
     buf = n.load(filename)
-    freqs = buf['freqs'][MIN_CH:MAX_CH]
+    freqs = buf['freqs']
     for dbl in buf.files:
         if not dbl.startswith('d_'): continue
         wbl = 'w'+dbl[1:]
-        #d = n.abs(buf[dbl][MIN_CH:MAX_CH] * SCALE)
-        d = buf[dbl][MIN_CH:MAX_CH] * SCALE
-        #if (len(times) + i) % 2 == 0: d *= -1
-        #if (len(times)) % 4 < 2: d *= -1
-        #if (len(times)) >= 1344/2: d *= -1
-        w = buf[wbl][MIN_CH:MAX_CH]
+        # Fudge calibration by using an approximate scaling
+        d = buf[dbl] * SCALE
+        w = buf[wbl]
         val = n.where(d == 0, 0, 1)
         gain = n.sqrt(n.average(w**2))
         if gain == 0: continue
-        #mdl = n.ones_like(freqs)
+        # 1st pass: deconv by power law model across entire band
         poly = n.polyfit(n.log10(freqs.compress(val)), n.log10(n.abs(d.compress(val))), deg=3)
         mdl = 10**(n.polyval(poly, n.log10(freqs)))
         ker = n.fft.ifft(w*mdl)
         _d = n.fft.ifft(d)
         __d, info = a.deconv.clean(_d, ker, tol=opts.clean)
-        #__d[1:] = 0; _d -= n.fft.ifft(n.fft.fft(__d) * val)
         if DECONV1WIDTH == 0: __d[1:] = 0
         else: __d[DECONV1WIDTH+1:-DECONV1WIDTH] = 0
-        #if PLOT1:
-        if PLOT1:
-            p.plot(freqs.compress(val), (d/w.clip(1,n.Inf)).compress(val))
-            #p.semilogy(freqs.compress(val), ((n.fft.fft(__d) * n.fft.fft(ker))/w.clip(1,n.Inf)).compress(val), ',')
-            #p.plot(freqs.compress(val), ((d - n.fft.fft(__d) * n.fft.fft(ker))/w.clip(1,n.Inf)).compress(val))
-        _d -= n.fft.ifft(n.fft.fft(__d) * n.fft.fft(ker))
-        if True:
-            ker = n.fft.ifft(w)
-            __d, info = a.deconv.clean(_d, ker, tol=opts.clean)
-            if DECONV1WIDTH == 0: __d[1:] = 0
-            else: __d[DECONV1WIDTH+1:-DECONV1WIDTH] = 0
-            if PLOT1:
-                #p.plot(freqs, n.fft.fft(__d) * n.fft.fft(ker))
-                p.plot(freqs.compress(val), ((n.fft.fft(_d) - n.fft.fft(__d) * n.fft.fft(ker))/w.clip(1,n.Inf)).compress(val), ',')
-                #p.plot(freqs, n.fft.fft(_d) - n.fft.fft(__d) * n.fft.fft(ker), '.')
-            _d -= n.fft.ifft(n.fft.fft(__d) * n.fft.fft(ker))
-        d = n.fft.fft(_d) * val
+        d -= n.fft.fft(__d) * n.fft.fft(ker)
+        # 2nd pass: deconv across entire band (no power law)
+        _d = n.fft.ifft(d)
+        ker = n.fft.ifft(w)
+        __d, info = a.deconv.clean(_d, ker, tol=opts.clean)
+        if DECONV1WIDTH == 0: __d[1:] = 0
+        else: __d[DECONV1WIDTH+1:-DECONV1WIDTH] = 0
+        d -= n.fft.fft(__d) * n.fft.fft(ker)
+        # 3rd pass: deconv only in band of interest
+        _d = n.fft.ifft(d[MIN_CH:MAX_CH])
+        ker = n.fft.ifft(w[MIN_CH:MAX_CH])
+        __d, info = a.deconv.clean(_d, ker, tol=opts.clean)
+        if DECONV1WIDTH == 0: __d[1:] = 0
+        else: __d[DECONV1WIDTH+1:-DECONV1WIDTH] = 0
+        d[MIN_CH:MAX_CH] -= n.fft.fft(__d) * n.fft.fft(ker)
         i = a.miriad.bl2ij(int(dbl[2:]))[0]
-        dat[i] = dat.get(i,0) + d
-        wgt[i] = wgt.get(i,0) + w
-    if PLOT1: p.show()
+        #dat[i] = dat.get(i,0) + d[MIN_CH:MAX_CH]
+        dat[i] = dat.get(i,0) + d[MIN_CH:MAX_CH] * 2*(n.random.randint(2)-.5)
+        wgt[i] = wgt.get(i,0) + w[MIN_CH:MAX_CH]
+freqs = freqs[MIN_CH:MAX_CH]
 
 # Compute the median per-antenna score and flag off antennas above it
 scores = {}
 for i in dat:
     avg = dat[i] / wgt[i].clip(1,n.Inf)
-    #p.plot(freqs, n.abs(avg), ',', label=str(i))
-    if False:
-        valid = n.where(n.abs(avg) < 1e3, 1, 0)
-        print valid
-        dat[i] *= valid
-        wgt[i] *= valid
     scores[i] = n.std(avg[100:-100])
 mscore = n.median(scores.values())
 for i in scores:
@@ -103,7 +88,7 @@ if PLOT2:
     p.plot(freqs, totavg, label='avg')
     p.ylim(-10e3,10e3)
     p.subplot(122)
-    pspec = n.fft.ifft(totavg[100:-100])
+    pspec = n.fft.ifft(totavg)
     pspec = n.concatenate([pspec[pspec.size/2:], pspec[:pspec.size/2]])
     p.semilogy(n.abs(pspec), label='avg')
     p.legend()
@@ -120,11 +105,9 @@ for b in range((MAX_CH-MIN_CH) / (SUBBAND/2)):
         _d = C.pfb.pfb(dat[i][ch0:ch1], taps=NTAPS, window='kaiser3', fft=n.fft.ifft)
         _w = C.pfb.pfb(wgt[i][ch0:ch1], taps=NTAPS, window='kaiser3', fft=n.fft.ifft)
         g1 = n.abs(_w[0])
-        if True:
-            __d, info = a.deconv.clean(_d, _w, tol=opts.clean)
-            __d[1:] = 0
-            #__d[2:6] = 0; __d[7:] = 0
-            _d -= n.fft.ifft(n.fft.fft(__d) * n.fft.fft(_w))
+        __d, info = a.deconv.clean(_d, _w, tol=opts.clean)
+        __d[1:] = 0
+        _d -= n.fft.ifft(n.fft.fft(__d) * n.fft.fft(_w))
         _d1 = _d[:_d.size/2]
         # Cross correlate with other PS measurements
         for _d2,g2 in zip(data.get(b,[]), gain.get(b,[])):
@@ -144,9 +127,8 @@ for b in range((MAX_CH-MIN_CH) / (SUBBAND/2)):
     z = C.pspec.f2z(_freqs)
     eta = C.pspec.f2eta(_freqs)
     k_pl = (eta * C.pspec.dk_deta(z))[:__dat[b].size]
-    scalar = C.pspec.k3pk_from_Trms(1, k_pl, n.average(_freqs), B=B)
-    #scalar = 1
-    print B, scalar[1], k_pl[1]
+    if DO_K3PK: scalar = C.pspec.k3pk_from_Trms(1, k_pl, n.average(_freqs), B=B)
+    else: scalar = 1
     dat.append((__dat[b] / __wgt[b]) * scalar)
 dat = n.array(dat).real
 #avg = n.average(dat,axis=0); avg.shape = (1,avg.size); dat -= avg
@@ -156,7 +138,8 @@ dat1 = n.log10(n.abs(dat))
 #mx,drng = 10,7
 #mx,drng = 7,5
 #mx,drng = 3,4
-mx,drng = 6,4
+if DO_K3PK: mx,drng = 10,4
+else: mx,drng = 6,6
 #mx,drng = dat1.max(), dat1.max()-max(dat1.min(),2)
 p.imshow(dat1, vmax=mx, vmin=mx-drng, aspect='auto', interpolation='nearest')
 p.colorbar(shrink=.5)
