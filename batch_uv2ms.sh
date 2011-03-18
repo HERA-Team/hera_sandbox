9c3fb85a2cc9cd3d665eb81ffe4f8f2037068388
Danny Jacobs <jacobsda@sas.upenn.edu>
2011-03-18 15:30:15 -0600

    Added memory limit of 4000M to batch_uv2ms

--- batch_uv2ms.sh
+++ batch_uv2ms.sh
@@ -4,7 +4,7 @@
 #$ -o grid_output/
 #$ -V
 #$ -cwd
-
+#$ -l h_vmem=4000M
 CAL=psa455_v004_gc
 ARGS=`pull_args.py $*`
 echo casapy --logfile grid_output/casalog_bash_uv2ms_${JOB_ID}_${TASK_ID}.log --nologger -c bash_uv2ms.py -C ${CAL} ${ARGS}
