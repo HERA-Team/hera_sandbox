438fe70b117384f5583a4bacb4699be7c8b5015b
Danny Jacobs <jacobsda@sas.upenn.edu>
2011-03-18 15:06:31 -0600

    Added an echo so I can see whats happening.

--- batch_uv2ms.sh
+++ batch_uv2ms.sh
@@ -1,11 +1,11 @@
 #$ -S /bin/bash
-#$ -N psa332_r
+#$ -N casa_uv2ms
 #$ -j y
 #$ -o grid_output/
 #$ -V
 #$ -cwd
 
 CAL=psa455_v004_gc
-
 ARGS=`pull_args.py $*`
+echo casapy --logfile grid_output/casalog_bash_uv2ms_${JOB_ID}_${TASK_ID}.log --nologger -c bash_uv2ms.py -C ${CAL} ${ARGS}
 casapy --logfile grid_output/casalog_bash_uv2ms_${JOB_ID}_${TASK_ID}.log --nologger -c bash_uv2ms.py -C ${CAL} ${ARGS}
