fcddfd9fef8301c7a1fea97b64d53f19e424e1c2
Danny Jacobs <jacobsda@sas.upenn.edu>
2011-03-22 23:40:46 -0600

    New head node requires "unset DISPLAY" line before starting casapy and maybe other things.

--- batch_uv2ms.sh
+++ batch_uv2ms.sh
@@ -6,6 +6,7 @@
 #$ -cwd
 #$ -l h_vmem=4000M
 CAL=psa455_v004_gc
+unset DISPLAY
 ARGS=`pull_args.py $*`
 echo casapy --logfile grid_output/casalog_bash_uv2ms_${JOB_ID}_${TASK_ID}.log --nologger -c bash_uv2ms.py -C ${CAL} ${ARGS}
 casapy --logfile grid_output/casalog_bash_uv2ms_${JOB_ID}_${TASK_ID}.log --nologger -c bash_uv2ms.py -C ${CAL} ${ARGS}
