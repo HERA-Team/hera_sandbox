5b81219ffcdd9c114c3b28b0562665fc1d3f3382
Danny Jacobs <jacobsda@sas.upenn.edu>
2011-03-22 22:53:30 -0600

    Added batch_flagbadbls.sh

---
+++ batch_flagbadbls.sh
@@ -0,0 +1,11 @@
+#$ -S /bin/bash
+#$ -N casa_flagbadbls
+#$ -j y
+#$ -o grid_output/
+#$ -V
+#$ -cwd
+#$ -l h_vmem=8000M
+ARGS=`pull_args.py $*`
+echo casapy --logfile grid_output/casalog_bash_flagbadbls_${JOB_ID}_${SGE_TASK_ID}.log --nologger -c bash_flagbadbls.py
+--cstart=300 --cstop=1700 ${ARGS}
+casapy --logfile grid_output/casalog_bash_flagbadbls_${JOB_ID}_${SGE_TASK_ID}.log --nologger -c bash_flagbadbls.py --cstart=300 --cstop=1700 ${ARGS}
