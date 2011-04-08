6d05a46b1394fa6ff54c734dd3cb099015f7168c
Danny Jacobs <jacobsda@sas.upenn.edu>
2011-04-08 14:41:28 -0600

    Fixes attributeerror squeeze in grid_uv

--- grid_uv.py
+++ grid_uv.py
@@ -28,7 +28,7 @@ ms.select({'uvdist':ulim})
 #ms.iterinit(columns=['TIME'])
 ms.selectchannel(highchan-lowchan,lowchan,1,1)
 rec = ms.getdata(['axis_info'])
-f = n.median(rec['axis_info']['freq_axis'].squeeze())
+f = n.median(rec['axis_info']['freq_axis']['chan_freq'].squeeze())
 
 #moredata=True
 #while(moredata):
