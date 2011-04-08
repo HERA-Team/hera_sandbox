3c7230907a2a3572d9a0227c6621d33055bf85c4
Danny Jacobs <jacobsda@sas.upenn.edu>
2011-04-08 14:18:35 -0600

    Added grid_uv.py.  Uses casa and aipy to grid to a uvf box.

---
+++ grid_uv.py
@@ -0,0 +1,48 @@
+#mixes AIPY and CASA to grid down uv samples using central freq to compute 
+#uvw
+# DCJ 2011
+#sick socorro suck
+
+uvsise=200
+uvres =0.4 
+ulim = [1,2000]
+highchan = 400
+lowchan = 300
+
+c = 3e8 #m/s
+
+def grid_it(im,us,vs,ws,ds,wgts):
+    #print 'Gridding %d integrations' % n_ints
+    sys.stdout.write('|'); sys.stdout.flush()
+    if len(ds) == 0: raise ValueError('No data to use.')
+    ds,wgts = n.concatenate(ds), n.concatenate(wgts).flatten()
+    us,vs,ws = n.concatenate(us), n.concatenate(vs), n.concatenate(ws)
+    # Grid data into UV matrix
+    (us,vs,ws),ds,wgts = im.append_hermitian((us,vs,ws),ds,wgts)
+    im.put((us,vs,ws), ds, wgts)
+    #im.put((us,vs,ws), ds, wgts, invker2=bm_im)
+
+
+ms.selectinit()
+ms.select({'uvdist':ulim})
+#ms.iterinit(columns=['TIME'])
+ms.selectchannel(highchan-lowchan,lowchan,1,1)
+rec = ms.getdata(['axis_info'])
+f = n.median(rec['axis_info']['chan_freq'].squeeze())
+
+#moredata=True
+#while(moredata):
+rec = ms.getdata(['u','v','w','data','flag'])
+D = n.ma.array(rec['data'],mask=rec['flag'])
+f = f* n.onee_like(D)
+U,V,W = rec['u']*f/c,rec['v']*f/c,rec['w']*f/c
+
+
+im = Img(uvsize, uvres, mf_order=0)
+grid_it(im,U,V,W,D,n.ones_like(D))
+uvs = a.img.recenter(n.abs(im.uv).astype(n.float), (DIM/2,DIM/2))
+
+#output the result in a casa image
+matshow(uvs[:,:,0,0])
+
+
