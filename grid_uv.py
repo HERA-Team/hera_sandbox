d3c72231f4aab109effcb69decc7886611e2451c
Danny Jacobs <jacobsda@sas.upenn.edu>
2011-04-10 13:51:44 -0600

    Wasn't gridding frequencies correctly. Adjusing d loop and U shape in [grid_uv]

--- grid_uv.py
+++ grid_uv.py
@@ -41,9 +41,9 @@ U,V,W = rec['u']*f/c,rec['v']*f/c,rec['w']*f/c
 del(rec)
 ms.close()
 print " done"
-U.shape += (1,)
-V.shape += (1,)
-W.shape += (1,)
+#U.shape += (1,)
+#V.shape += (1,)
+#W.shape += (1,)
 
 
 
@@ -51,7 +51,7 @@ print "gridding.... ",
 uvs = []
 for d in D:
     im = a.img.Img(uvsize, uvres, mf_order=0)
-    grid_it(im,U,V,W,D,n.ones_like(D))
+    grid_it(im,U,V,W,d,n.ones_like(d))
     print ".",
     UV = a.img.recenter(im.uv, (DIM/2,DIM/2))
     uvs.append(UV)
