832e75e6891ba27cfcb950c0357791ad32e64d91
Danny Jacobs <jacobsda@sas.upenn.edu>
2011-04-12 19:47:17 -0600

    fixing nologger non-error in batch_grid_uv

--- batch_grid_uv.sh
+++ batch_grid_uv.sh
@@ -12,12 +12,12 @@ UVSIZE=200
 UVRES=4
 CHANS=300_450 
 echo casapy --logfile grid_output/casalog_selfcal_${JOB_ID}_${SGE_TASK_ID}.log\
---nologger -c ~/scripts/bash_grid_uv.py \
+ --nologger -c ~/scripts/bash_grid_uv.py \
 --uvsize=${UVSIZE} --uvres=${UVRES} --chan=${CHANS} \
 $ARGS
 
 casapy --logfile grid_output/casalog_selfcal_${JOB_ID}_${SGE_TASK_ID}.log\
---nologger -c ~/scripts/bash_grid_uv.py \
+ --nologger -c ~/scripts/bash_grid_uv.py \
 --uvsize=${UVSIZE} --uvres=${UVRES} --chan=${CHANS} \
 $ARGS
 
