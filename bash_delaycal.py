122d78335d49c81e6a67218cf57f28676f9ac6bf
Danny Jacobs <jacobsda@sas.upenn.edu>
2011-06-11 09:18:45 -0600

    tweaking sky model in delaycal [8]

--- bash_delaycal.py
+++ bash_delaycal.py
@@ -126,7 +126,7 @@ for msfile in args:
     
     use_src = ['pic']#,'144']
     print "Choosing a cal source(s)"
-    use_src = findcalsrcs(aa,cat,Nsrcs=3,altmin=25)
+    use_src = findcalsrcs(aa,cat,Nsrcs=5,altmin=25)
     print "Using",','.join(use_src)
     
     
