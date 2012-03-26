e77ea8a41832d9ab6da8a3534d6f875fbaec6f5d
Jonathan Pober <pober@folio.sas.upenn.edu>
2012-03-26 16:17:42 -0400

    Pspec pipeline tweaks and flat bandpass hack.

--- pspec_pipline/pspec_pipeline.sh
+++ pspec_pipline/pspec_pipeline.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-CAL=psa746_v008
+CAL=psa746_v010
 RFI_CHAN=0_130,755_777,1540,1704,1827,1868,1901_2047
 HORIZON=1.
 for FILE in $*; do
@@ -8,5 +8,5 @@ for FILE in $*; do
     xrfi_simple.py -a 0 --dt=4 --df=6 -c $RFI_CHAN --combine -t 20 $FILE
     pspec_prep.py -C $CAL --horizon=$HORIZON ${FILE}R
     xrfi_simple.py -n 5 ${FILE}RB
-    pspec_to_npz_quick_n_dirty.py -C $CAL ${FILE}RBR
+    pspec_to_npz_quick_n_dirty.py -p xx -C $CAL ${FILE}RBR
 done
