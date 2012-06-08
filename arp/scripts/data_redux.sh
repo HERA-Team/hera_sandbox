2dd8b134b0148de6761a6ddcfba99cbeb3f01eec
Aaron Parsons <aparsons@astron.berkeley.edu>
2012-06-08 09:47:43 -0700

    Added get_uv_neighbor.py script for generating file triplets.  Used this in data_redux.sh to modularize processing.

--- arp/scripts/data_redux.sh
+++ arp/scripts/data_redux.sh
@@ -1,22 +1,19 @@
 #! /bin/bash
 
-UV=$*
-UV_R=`python -c "print ' '.join(map(lambda x: x+'R','${UV}'.split()))"`
-UV_RE=`python -c "print ' '.join(map(lambda x: x+'E','${UV_R}'.split()[1:-1]))"`
-UV_RE_R=`python -c "print ' '.join(map(lambda x: x[:-1],'${UV_RE}'.split()))"`
+UVR=$*
 
-xrfi_simple.py -c 0_130,755_777,1540,1704,1827,1868,1885_2047 --df=6 $UV
-# XXX is the "pol" argument below necessary?
-ddr_filter_coarse.py -p xx --clean=1e-3 --maxbl=300 --output=ddr --invert $UV_R
-xrfi_simple.py -n 4 $UV_RE
-for FILE in $UV_RE_R; do
-    echo Copying ${FILE}ER/flags to ${FILE}
-    cp ${FILE}/flags ${FILE}/flags_bk
-    cp ${FILE}ER/flags ${FILE}/flags
-    #rm -rf ${FILE}[DEF]
+#xrfi_simple.py -c 0_130,755_777,1540,1704,1827,1868,1885_2047 --df=6 $UV
+# XXX is the "pol" argument in ddr_filter_coarse necessary?
+for FILE in $UVR; do
+    TRIPLET=`get_uv_neighbor.py $FILE`
+    if ! ls ${FILE}/flags_bk &> /dev/null; then
+        ddr_filter_coarse.py -p xx -a all --clean=1e-3 --maxbl=300 --output=ddr --invert $TRIPLET
+        xrfi_simple.py -n 4 ${FILE}E
+        rm -rf ${FILE}E
+        echo Copying ${FILE}ER/flags to ${FILE}
+        cp ${FILE}/flags ${FILE}/flags_bk
+        cp ${FILE}ER/flags ${FILE}/flags
+        #rm -rf ${FILE}ER
+    fi
+    ddr_filter_coarse.py -p xx -a all --clean=1e-4 --maxbl=300 $TRIPLET
 done
-rm -rf $UV_RE
-ddr_filter_coarse.py -p xx --clean=1e-4 --maxbl=300 $UV_R
-#rm -rf *.uvGCRER
-#    #if ! ls ${FILE}drxam &> /dev/null; then
-#    #fi
