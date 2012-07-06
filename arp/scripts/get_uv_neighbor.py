b0bfc2cc709eadcb6a6768a1a27c8cb980ecb339
Aaron Parsons <aparsons@astron.berkeley.edu>
2012-07-05 18:15:17 -0700

    Added error handling to get_uv_neighbor.py.

--- arp/scripts/get_uv_neighbor.py
+++ arp/scripts/get_uv_neighbor.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python
 import sys, os, glob
-f = sys.argv[1]
+try: f = sys.argv[1]
+except(IndexError): sys.exit(0)
 filelist = glob.glob('*.'+f.split('.')[-1])
 filelist.sort()
-i = filelist.index(f)
+try: i = filelist.index(f)
+except(ValueError): sys.exit(0)
 print ' '.join(filelist[i-1:i+2])
 
 
