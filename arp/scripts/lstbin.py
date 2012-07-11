28b7a4c746b42bb143cba8f86da8cc452bbb8f15
Aaron Parsons <aparsons@astron.berkeley.edu>
2012-07-11 14:41:44 -0700

    Made lstbin.py polarization-compliant.

--- arp/scripts/lstbin.py
+++ arp/scripts/lstbin.py
@@ -60,6 +60,14 @@ for f in args:
         # Never bail if both wrap...
     nargs.append(f)
 
+def ijp2blp(i,j,pol):
+    return a.miriad.ij2bl(i,j) * 16 + (pol + 9)
+
+def blp2ijp(blp):
+    bl,pol = int(blp) / 16, (blp % 16) - 9
+    i,j = a.miriad.bl2ij(bl)
+    return i,j,pol
+
 jds = {}
 for f in nargs:
     uv = a.miriad.UV(f)
@@ -81,10 +89,10 @@ for f in nargs:
             if dat.has_key(lst): jds[lst] = min(jds.get(lst,n.Inf), t)
         # Only take this LST if we have a bin for it already allocated
         if not dat.has_key(lst): continue
-        bl = a.miriad.ij2bl(i,j)
-        crds[bl] = uvw
-        dat[lst][bl] = dat[lst].get(bl,0) + n.where(f,0,d)
-        cnt[lst][bl] = cnt[lst].get(bl,0) + n.logical_not(f).astype(n.int)
+        blp = ijp2blp(i,j,uv['pol'])
+        crds[blp] = uvw
+        dat[lst][blp] = dat[lst].get(blp,0) + n.where(f,0,d)
+        cnt[lst][blp] = cnt[lst].get(blp,0) + n.logical_not(f).astype(n.int)
 
 lsts = [lst for lst in dat if len(dat[lst]) > 0]
 lsts.sort()
@@ -103,12 +111,12 @@ for lst in lsts:
     print 'LST:', a.ephem.hours(lst), '(%f)' % lst, ' -> JD:', t
     sys.stdout.flush()
     uvo['lst'], uvo['ra'], uvo['obsra'] = lst, lst, lst
-    for bl in dat[lst]:
-        i,j = a.miriad.bl2ij(bl)
-        preamble = (crds[bl], t, (i,j))
-        cmax = n.max(cnt[lst][bl])
-        d = dat[lst][bl] / cnt[lst][bl].clip(1, n.Inf)
-        f = n.where(cnt[lst][bl] < cmax * opts.flag_thresh, 1, 0)
+    for blp in dat[lst]:
+        i,j,uvo['pol'] = blp2ijp(blp)
+        preamble = (crds[blp], t, (i,j))
+        cmax = n.max(cnt[lst][blp])
+        d = dat[lst][blp] / cnt[lst][blp].clip(1, n.Inf)
+        f = n.where(cnt[lst][blp] < cmax * opts.flag_thresh, 1, 0)
         uvo.write(preamble, d, f)
 del(uvo)
 print 'Finished writing', filename
