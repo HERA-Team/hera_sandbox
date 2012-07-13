31950966f79ead8b2a333bb6206dd0c2c1f37975
Aaron Parsons <aparsons@astron.berkeley.edu>
2012-07-12 17:43:15 -0700

    Modified get_uv_neighbor to work outside of cwd

--- arp/scripts/get_uv_neighbor.py
+++ arp/scripts/get_uv_neighbor.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python
 import sys, os, glob
+# Make sure we have an argument
 try: f = sys.argv[1]
 except(IndexError): sys.exit(0)
-filelist = glob.glob('*.'+f.split('.')[-1])
+path = os.path.dirname(f)
+filelist = glob.glob(path+'/*.'+f.split('.')[-1])
 filelist.sort()
 try: i = filelist.index(f)
 except(ValueError): sys.exit(0)
