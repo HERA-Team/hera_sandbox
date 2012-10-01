399474b426b9097c8d4759deb1ffbe9a8d0917c1
Danny Jacobs <daniel.c.jacobs@asu.edu>
2012-10-01 15:34:43 -0700

    added pull_args option to work without gridengine

--- dcj/scripts/pull_args.py
+++ dcj/scripts/pull_args.py
@@ -3,7 +3,22 @@ import sys, os, math, optparse
 o = optparse.OptionParser()
 o.add_option('-w', '--wrap', dest='wrap', action='store_true',
     help='Instead of pulling nothing for indices off the end of the list, wrap around and repull arguments from the beginning.')
+o.add_option('-t', type='str',
+    help='Same as -t qsub. ex -t 1:10 Use if you want to multitask without grid engine. Must specifiy task_id as well.')
+o.add_option('--taskid',type=int,default=1,
+    help='current taskid. Use in conjunction with -t for non-gridengine list splitting mode.')
 opts,args = o.parse_args(sys.argv[1:])
+
+if not opts.t is None:
+    n = int(opts.t.split(':')[0])
+    m = int(opts.t.split(':')[1])
+    i = opts.taskid-1
+    if (m-n) <= len(args) or not opts.wrap:
+        num = int(math.ceil(float(len(args)) / (m - n + 1)))
+        print ' '.join(args[num*i:num*(i+1)])
+    else:
+        print args[i % len(args)]
+    sys.exit(0)    
 try:
     n = int(os.environ['SGE_TASK_FIRST'])
     m = int(os.environ['SGE_TASK_LAST'])
