a83d2439fc0d4e6296e3b2a9e1682dbbdf57d4df
David Moore <damo@sas.upenn.edu>
2012-11-01 15:34:52 -0400

    Added AntFit function to dfm.py

--- src/dfm.py
+++ src/dfm.py
@@ -19,6 +19,49 @@ def Phi(Nsig):
     """
     return 0.5 * erfc(Nsig/np.sqrt(2.))
 
+def AntFit(y,dly=False):
+    """
+    Perform a least-squares fit for y_ij = y_i + y_j (if dly=True, y_ij = y_i - y_j). 
+    input = dictionary, y_ij, labeled with a.pol.ijp2blp numbers.
+    output = dictionary, X['%d%s'%(i,pi)] = y_i
+    """
+    Nblp = len(y.keys())
+    ants,pols = [],[]
+    for blp in y:
+        i,j,p = a.pol.blp2ijp(blp) 
+        if not i in ants: ants.append(i)
+        if not j in ants: ants.append(j)
+        _p = a.miriad.pol2str[p]
+        if not _p[0] in pols: pols.append(_p[0])
+        if not _p[1] in pols: pols.append(_p[1])
+    blps = [blp for blp in y]
+    blps.sort();ants.sort()
+    Nants = len(ants)
+    def ANTIND(a): return ants.index(a)
+    def BLPIND(b): return blps.index(b)
+    _y,A = np.zeros(Nblp),np.zeros((Nblp,len(pols)*Nants))
+    for blp in blps:
+        #What index do I give everybody?
+        i,j,p = a.pol.blp2ijp(blp)
+        i = ANTIND(i);j = ANTIND(j);pi,pj = a.miriad.pol2str[p]
+        bi = BLPIND(blp)
+        ii = i + {'x':0,'y':Nants}[pi] 
+        ji = j + {'x':0,'y':Nants}[pj] 
+        #populate matrices.
+        _y[bi] = y[blp]
+        A[bi,ii] = 1.
+        A[bi,ji] = 1.
+        if dly: A[bi,ji] = -1.
+    _x = np.dot(np.linalg.pinv(A),_y)
+    X = {}
+    for i,xi in enumerate(_x):
+        if i < Nants: X['%d%s'%(ants[i],'x')] = xi
+        else: X['%d%s'%(ants[i-Nants],'y')] = xi
+    return X
+
+######################################
+# Stuff used for averaging down data #
+######################################
 
 ij2bl,bl2ij = a.miriad.ij2bl,a.miriad.bl2ij
 def grid2ij(GRID):
