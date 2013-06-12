9ed29abe12340ccb2cd12d0da1cc2773745f9fd3
Danny Jacobs <daniel.c.jacobs@asu.edu>
2013-06-12 10:18:35 -0700

    works with FHD. corrected a bug that made all files have the same phase center. epoch is now set to phase location

--- dcj/scripts/FHD_prep.py
+++ dcj/scripts/FHD_prep.py
@@ -10,22 +10,6 @@ opts,args = o.parse_args(sys.argv[1:])
 
 uv = a.miriad.UV(args[0])
 aa = a.cal.get_aa(opts.cal,uv['sdf'],uv['sfreq'],uv['nchan'])
-
-if opts.onephs:
-    (uvw,t,bl),d = uv.read()
-    aa.set_jultime(t+5.*60./a.const.s_per_day)
-    RA = str(aa.sidereal_time())
-    dec= str(aa.lat)
-    print "opts.onesrc is True: setting phase to %s_%s"%(RA,dec)
-    opts.src = RA+'_'+dec
-
-if not opts.src is None:
-    if not opts.src.startswith('zen'):
-        srclist,cutoff,catalogs = a.scripting.parse_srcs(opts.src, opts.cat)
-        src = a.cal.get_catalog(opts.cal, srclist, cutoff, catalogs).values()[0]
-    else: src = 'z'
-else: src = None
-
 del(uv)
 
 curtime = None
@@ -37,6 +21,21 @@ for filename in args:
         continue
     
     uvi = a.miriad.UV(filename)
+    if opts.onephs:
+        (uvw,t,bl),d = uvi.read()
+        aa.set_jultime(t+5.*60./a.const.s_per_day)
+        RA = str(aa.sidereal_time())
+        dec= str(aa.lat)
+        print "opts.onesrc is True: setting phase to %s_%s"%(RA,dec)
+        opts.src = RA+'_'+dec
+        epoch = aa.epoch
+    
+    if not opts.src is None:
+        if not opts.src.startswith('zen'):
+            srclist,cutoff,catalogs = a.scripting.parse_srcs(opts.src, opts.cat)
+            src = a.cal.get_catalog(opts.cal, srclist, cutoff, catalogs).values()[0]
+        else: src = 'z'
+    else: src = None
     
     D = {}
     for (uvw,t,(i,j)),d in uvi.all():
@@ -63,8 +62,8 @@ for filename in args:
     antpos.shape = antpos.shape[0]*antpos.shape[1]
     
     uvo = a.pol.UV(filename+'M',status='new')
-    ra = src.get_params()['ra']*180/np.pi
-    uvo.init_from_uv(uvi,override={'antpos':antpos,'obsra':ra,'ra':ra})
+    ra = src.get_params()['ra']
+    uvo.init_from_uv(uvi,override={'antpos':antpos,'obsra':ra,'ra':ra,'epoch':epoch*2000/36525.})
     uvo.add_var('resfreq','d')
     uvo['restfreq'] = uvi['sfreq']
     for t in D:
