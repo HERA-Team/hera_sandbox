64e0879fcdfd74cf1d370136ab5008c38165bf44
Danny Jacobs <daniel.c.jacobs@asu.edu>
2013-06-30 12:09:53 -0400

    added more nodes to grid_ex

--- dcj/scripts/grid_ex.sh
+++ dcj/scripts/grid_ex.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-for i in `python -c 'for i in range(1,16): print "%02d"%i,'`; do echo node${i}; ssh -fCT node${i}  $*; sleep 2; done
+for i in `python -c 'for i in range(1,24): print "%02d"%i,'`; do echo node${i}; ssh node${i}  $*; sleep 2; done
