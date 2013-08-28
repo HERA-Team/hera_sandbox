82d69fcdc02ee9a9e7f663fff6152c5df984c787
Zaki Ali <zakiali@folio.sas.upenn.edu>
2013-08-28 19:33:31 -0400

    a few changes to batch scripts.
    pullants : change ants
    redcal   : per dir basis
    xtalk    : xtalk remove only if file size is 1.1G (1Hr)

--- zsa/scripts/batch_redcal.sh
+++ zsa/scripts/batch_redcal.sh
@@ -1,13 +1,18 @@
 #$ -S /bin/bash
 #$ -V
 #$ -cwd
-
+#$ -l h_vmem=4G
 
 ARGS=`pull_args.py $*`
 for dir in $ARGS; do 
-    echo red_cal_v003.py --calpos=0,0 $dir/*.uvcRREcAzx
+    for file in $dir/*.uvcRREcAzx; do
+        echo red_cal_v003.py --calpos=0,0 $file
+        red_cal_v003.py --calpos=0,0 $file
+        
+#    echo red_cal_v003.py --calpos=0,0 $dir/*.uvcRREcAzx
 #xtalk3.py $ARGS
 #xARGS=`python -c "print ' '.join(map(lambda x: x+'x', '$ARGS'.split()))"`
 #echo $xARGS
-    red_cal_v003.py --calpos=0,0 $dir/*.uvcRREcAzx
+#    red_cal_v003.py --calpos=0,0 $dir/*.uvcRREcAzx
+    done;
 done;
--- zsa/scripts/batch_xtalk.sh
+++ zsa/scripts/batch_xtalk.sh
@@ -1,10 +1,16 @@
 #$ -S /bin/bash
 #$ -V
 #$ -cwd
+#$ -l paper
 
 DIR=`pull_args.py $*`
 for dir in $DIR; do 
     echo working on $dir 
-    echo xtalk3.py  $dir/*.uvcRREcAz
-    xtalk3.py  $dir/*.uvcRREcAz
-done;
+    for f in $dir/*.uvcRREcAz; do
+        fname=`du -h $f | awk '{if ($1 ~ 1.1G) print $2}'`
+        if [ $fname ]; then
+            echo xtalk3.py $fname
+            xtalk3.py $fname
+        fi
+    done
+done
