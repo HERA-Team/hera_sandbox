4adfb752542791d7d0a9708e94234a8e5c00c2e0
Zaki Ali <zakiali@folio.sas.upenn.edu>
2014-02-02 00:54:13 -0500

    plot_red_cal_coeffs.py now plots against lst as well.

--- zsa/scripts/plot_red_cal_coeffs.py
+++ zsa/scripts/plot_red_cal_coeffs.py
@@ -2,16 +2,28 @@
 ''''exec python -u -- "$0" ${1+"$@"} # '''
 # vi: syntax=python
 import numpy as n, pylab as p
+import aipy as a
 import sys
+import optparse
+
+o = optparse.OptionParser()
+a.scripting.add_standard_options(o, cal=True)
+opts, args = o.parse_args(sys.argv[1:])
+
+srclist, cutoff, catalogs = [], None, []
+cat = a.cal.get_catalog(opts.cal, srclist, cutoff, catalogs)
+aa = a.cal.get_aa(opts.cal, n.array([.15]))
+
 
 #PLOT_GAIN = False
-PLOT_GAIN = 0
+PLOT_GAIN = 1
 colors = 'kkbgcmry'
 
 dly = {}
 gain = {}
 times = []
-for filename in sys.argv[1:]:
+lsts = []
+for filename in args[1:]:
     print 'Reading', filename
     f = open(filename)
     npz = n.load(f)
@@ -22,6 +34,9 @@ for filename in sys.argv[1:]:
 #    print time[0], time[-1]
 #    pols = npz['pols']
     pols = ['xx']
+    aa.set_jultime(time)
+    print aa.sidereal_time()
+    lsts.append(aa.sidereal_time()*(12./n.pi))
     times.append(time)
     try:
         print times[-1] - times[-2]
@@ -33,8 +48,18 @@ for filename in sys.argv[1:]:
             gain[pi][i] = gain[pi].get(i,[]) + [g]
     f.close()
 
-
+lsts = n.array(lsts)
+print lsts
+if lsts[0] > lsts[-1]:
+    diff = 24 - lsts[0]
+    lsts = ((lsts + diff)%24) - diff
+print lsts
+print times
 print antpos
+fig = p.figure()
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
+ax1t= ax1.twiny()
 for pi in dly:
 #  for i in dly[pi]:
   for ik,i in enumerate(antpos.flatten()):
@@ -53,10 +78,17 @@ for pi in dly:
 #    if len(n.where(g >= 1.4)[0]) > 0: print i,c
 
     if PLOT_GAIN:
-        p.subplot(211); p.plot(times, d, c+',',label='%d,%s'%(i,pols[pi]))
-        p.subplot(212); p.plot(times, g, c, label='%d,%s'%(i,pols[pi]))
+        ax1.plot(times, d, c,label='%d,%s'%(i,pols[pi]))
+        ax2.plot(times, g, c, label='%d,%s'%(i,pols[pi]))
+        ax1t.plot(lsts, d, c+'.',label='%d,%s'%(i,pols[pi]))
     else:
         p.plot(times, g, c+'.', label='%d,%s'%(i,pols[pi]))
 
 #p.legend()
+ax1t.set_xlim(lsts[0],lsts[-1])
+#ax1t.xaxis.set_ticks(range(int(lsts[0]),int(lsts[-1])))
+ax1.set_xlim(times[0],times[-1])
+ax2.set_xlim(times[0],times[-1])
+ax1.grid()
+ax2.grid()
 p.show()
