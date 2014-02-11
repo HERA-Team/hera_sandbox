4854407ee3c0df2b0605185c4fcc21caebdc1b7e
Danny Jacobs <daniel.c.jacobs@asu.edu>
2014-02-11 12:38:51 -0700

    working version of rfi summary

--- dcj/scripts/dailyRFIreport.py
+++ dcj/scripts/dailyRFIreport.py
@@ -34,17 +34,32 @@ JD = times - JD0
 fig1 = figure(figsize=(8,10))
 #plot v frequency
 axf = fig1.add_subplot(311)
-axf.plot(freqs,vFreq)
-axf.set_xlabel('Frequency [GHz]')
+axf.plot(freqs*1e3,vFreq)
+axf.set_xlabel('Frequency [MHz]')
 #plot v time
 axt = fig1.add_subplot(312)
-axt.plot(JD,vTime,'.')
-axt.set_xlabel('Days since JD %d'%JD0)
+local_hours = (JD*24.+2+12)%24
+RD = times-1721424.5
+datetimes = matplotlib.dates.num2date(RD) #mpl uses Rata Die dates
+axt.plot_date(datetimes,vTime,'.',tz='Africa/Windhoek',)
+axt.set_xlabel('SAST [hours]')
+#axt.xaxis.set_major_locator(matplotlib.dates.HourLocator)
+axt.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%H"))
+#axt.set_minor_locator()
 axwf = fig1.add_subplot(313)
-axwf.imshow(flag_arr,aspect='auto',cmap='binary',extent=(freqs.min()*1e3,freqs.max()*1e3,times.max()*24,times.min()*24))
+axwf.imshow(np.flipud(flag_arr),aspect='auto',interpolation='nearest',cmap='binary',extent=(freqs.min()*1e3,freqs.max()*1e3,RD[0],RD[-1]))
+
+axwf.yaxis_date(tz='Africa/Windhoek')
+axwf.invert_yaxis()
+axwf.yaxis.set_major_locator(matplotlib.dates.HourLocator(interval=1))
+axwf.yaxis.set_major_formatter(matplotlib.dates.DateFormatter("%H"))
+
+
 axwf.set_ylabel('time [hrs]')
 axwf.set_xlabel('freq [MHz]')
 #title
-fig1.suptitle('RFI summary for JD'%(JD))
+fig1.suptitle('RFI summary for %s (JD %d)'%(datetimes[0].strftime('%d-%b-%Y'),JD0))
+if opts.outfile == 'today.png':
+    opts.outfile = 'RFI_summary_'+datetimes[0].strftime('%d-%b-%Y')+'.png'
 print 'saving to %s' % opts.outfile
 fig1.savefig(opts.outfile,fmt='png')
