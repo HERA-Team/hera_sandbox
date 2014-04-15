0024929bcda3d7f4aed81a1f7831b16fa0c30603
David F. Moore <damo@sas.upenn.edu>
2014-04-15 16:03:10 -0400

    added resetDB.sh to help with testing.

--- Compress/resetDB.sh
+++ Compress/resetDB.sh
@@ -4,3 +4,4 @@ mysql < killDB.sql
 mysql < createDB.sql
 ./initDB.py
 ./add_host.py qmaster 10.0.1.30 obs
+./new_observation.py qmaster zen.245666.456456.xx.uv zen.245666.23452.xx.uv zen.245666.234626.xx.uv
