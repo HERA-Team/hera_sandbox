411293bd57996a3ec66f10ceb8f2aa5af69aff36
PAPER Observer (test still) <obs@qmaster.paper.pvt>
2014-05-16 09:59:08 -0700

    tests/end2end.sh
    run this script to begin end-to-end testing.

--- Compress/tests/end2end.sh
+++ Compress/tests/end2end.sh
@@ -1,11 +1,17 @@
 #! /bin/bash
 
-hosts="still4 still5"
-for h in $hosts; do
-    ssh ${h} "rm /data/zen*" 
-done
-rm /home/obs/test_pot/*E
+TEST=micro
 
+hosts="still4 still5"
+#Clear off the scratch space on the execution hosts
+for h in $hosts; do
+    ssh ${h} "rm -rf /data/zen* 2> /dev/null" 
+done
+rm -r /home/obs/test_pot/test${TEST}/*[DEFz] 2> /dev/null
+
+#clear out all the data from the test db
 mysql test --host=10.0.1.20 --user=obs --password=P9ls4R*@ -e "truncate observations; truncate files; truncate orders; truncate history;"
-/home/obs/Compress/tests/PopulateTestDB.sh
-/home/obs/Compress/tests/qdaemon.test.sh
+echo Compress/tests/PopulateTestDB.sh
+/home/obs/Compress/tests/PopulateTestDB.sh /home/obs/test_pot/testmicro/zen*uv
+echo starting Compress/qdaemon.sh
+/home/obs/Compress/qdaemon.sh
