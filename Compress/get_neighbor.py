097320a0995afe5538f378362a433d8c529b29d6
PAPER Observer (test still) <obs@qmaster.paper.pvt>
2014-05-16 09:49:21 -0700

    get_neighbor.py
    Consults the database to return the two adjacent files to argv[1]

--- Compress/get_neighbor.py
+++ Compress/get_neighbor.py
@@ -16,16 +16,20 @@ pdb.verbose=False #just in case...
 jd=re.findall(r'\d+\.\d+',fname)[0]
 
 pol=fname.split('.')[-2]
-assert(pol in ['xx','xy','yx','yy'])
+try:
+    assert(pol in ['xx','xy','yx','yy'])
 
-jd_lo=pdb.get('jd_lo','observations','JD',jd)[0][0]
-jd_hi=pdb.get('jd_hi','observations','JD',jd)[0][0]
+    jd_lo=pdb.get('jd_lo','observations','JD',jd)[0][0]
+    jd_hi=pdb.get('jd_hi','observations','JD',jd)[0][0]
 
-if jd_lo is None:
+    if jd_lo is None:
+        print ""
+    elif jd_hi is None:
+        print ""
+    else:
+        lo_file=fname.replace(jd,jd_lo)
+        hi_file=fname.replace(jd,jd_hi)
+        print ' '.join([lo_file,fname,hi_file])
+except(AssertionError):
+    #this should happen for .npz files.
     print ""
-elif jd_hi is None:
-    print ""
-else:
-    lo_file=fname.replace(jd,jd_lo)
-    hi_file=fname.replace(jd,jd_hi)
-    print ' '.join([lo_file,fname,hi_file])
