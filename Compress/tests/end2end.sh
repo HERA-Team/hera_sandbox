c3f80e34033e270075f4eed8e3b78621895849c6
PAPER Observer (test still) <obs@qmaster.paper.pvt>
2014-05-21 17:25:07 -0700

    moving the test files to Compress/tests

--- Compress/tests/end2end.sh
+++ Compress/tests/end2end.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
 
-TEST=micro
+TEST=minimumtest
 
-hosts="still4 still5"
+hosts="pot0 still4 still5"
 #Clear off the scratch space on the execution hosts
+echo "deleting data from /data on hosts: "${hosts} 
 for h in $hosts; do
     ssh ${h} "rm -rf /data/zen* 2> /dev/null" 
 done
-rm -r /home/obs/test_pot/test${TEST}/*[DEFz] 2> /dev/null
 
 #clear out all the data from the test db
 mysql test --host=10.0.1.20 --user=obs --password=P9ls4R*@ -e "truncate observations; truncate files; truncate orders; truncate history;"
 echo Compress/tests/PopulateTestDB.sh
-/home/obs/Compress/tests/PopulateTestDB.sh /home/obs/test_pot/testmicro/zen*uv
+/home/obs/Compress/tests/PopulateTestDB.sh /home/obs/Compress/tests/${TEST}/zen*uv
+/home/obs/Compress/get_files_to_distill.py 300
 echo starting Compress/qdaemon.sh
 /home/obs/Compress/qdaemon.sh
