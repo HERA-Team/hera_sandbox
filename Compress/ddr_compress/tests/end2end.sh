83c86d419a6d80ea1dbfaafd340789fe2483fb35
PAPER Observer (test still) <obs@qmaster.paper.pvt>
2014-06-05 09:04:26 -0700

    end2end resets db and initializes with sim4 data on pot as designed

--- Compress/ddr_compress/tests/end2end.sh
+++ Compress/ddr_compress/tests/end2end.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 #copy the test data to the pot
+echo "clearing the database for a fresh run"
+PW=`python -c "from ddr_compress.dbi import dbinfo; print dbinfo['password']"`
+DB=`python -c "from ddr_compress.dbi import dbinfo; print dbinfo['dbname']"`
+mysql ${DB} -h qmaster --password=${PW} -e "set foreign_key_checks=0; truncate  neighbors; truncate observation; truncate file; set foreign_key_checks=1;"
 echo "initializing the pot for the test"
-scp -R  ~/test_pot/sim4/z*uv pot0:
+rsync -avP -e "ssh -c arcfour128"  ~/test_pot/sim4/z*uv pot0:/data/
 echo "starting the task servers on still machines"
 echo "starting qdaemon"
 echo "loading the data into the db"
-ssh pot0 add_observations.py /data/z*uv  #NB this step won't work till ddr_compress is installed on the pot
-
+ssh pot0 "add_observations.py /data/z*uv"
+echo "DONE"
