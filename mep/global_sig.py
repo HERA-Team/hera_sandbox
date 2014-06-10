5443f28c81915dc5f85f7f5edca74648c5aff454
mpresley42 <mpresley@princeton.edu>
2014-06-10 11:57:56 -0700

    In global_sig.py I moved file reading to its own file, added a function to fit the global signal with a line through the origin in place of a linear fit. In the test_regress_vary_* functions I added a filter to only plot points with good reduced chi-squared fits.
    I put the calfiles, figures, and coeff data in their own folders.
    
    In useful_functions.py I fixed the reduced chi-squared calculation so that it divided by the correct degrees of freedom and I added functions to fit lines through the origin, power laws, and legendre functions.

--- mep/global_sig.py
+++ mep/global_sig.py
@@ -1,9 +1,14 @@
 #! /usr/bin/env python
 import aipy as a, numpy as n, pylab as p
 import capo as C
-import homemade_plotting as hp
+import useful_functions as uf
 
-def get_coeffs(calfile='psa898_v002',na = 32,freqs = n.arange(.1,.2,.01)):
+def get_coeffs(calfile,na = 32,freqs = n.arange(.1,.2,.01)):
+	"""
+	This function calculates the coefficients in front of the global signal 
+	for the antenna array described in the calfile. The coefficients are 
+	determined by the integral of A(l,m)exp[-i2pi(ul+vm)]dldm.
+	"""
 	aa = a.cal.get_aa(calfile, n.array([.150])) #get antenna array
 	im = a.img.Img(size=200, res=.5) #make an image of the sky to get sky coords
 	tx,ty,tz = im.get_top(center=(200,200)) #get coords of the zenith?
@@ -26,7 +31,7 @@ def get_coeffs(calfile='psa898_v002',na = 32,freqs = n.arange(.1,.2,.01)):
 	        kk=0
 	        for fq in freqs: #loop over frequencies
 	            phs = n.exp(-2j*n.pi*fq * (bx*tx+by*ty+bz*tz)) #fringe pattern
-	            phs.shape = amp.shape = im.uv.shape #?????
+	            phs.shape = amp.shape = im.uv.shape 
 	            amp = n.where(valid, amp, 0)
 	            phs = n.where(valid, phs, 0)
 
@@ -36,43 +41,78 @@ def get_coeffs(calfile='psa898_v002',na = 32,freqs = n.arange(.1,.2,.01)):
 	            coeffs[ll,kk] = dc_response
 	            kk+=1
 	        ll+=1
-	#n.savez(filename,baselines=baselines,frequencies=freqs,coeffs=coeffs)
-	#f = n.load(filename)
-	#freqs = f[frequencies]
-	n.savetxt('{0}_baselines.txt'.format(calfile),baselines)
-	n.savetxt('{0}_frequencies.txt'.format(calfile),freqs)
-	n.savetxt('{0}_coeffs.txt'.format(calfile),coeffs)
+	n.savez('./coeff_data/{0}_data'.format(calfile),baselines=baselines,frequencies=freqs,coeffs=coeffs)
+	# n.savetxt('{0}_baselines.txt'.format(calfile),baselines)
+	# n.savetxt('{0}_frequencies.txt'.format(calfile),freqs)
+	# n.savetxt('{0}_coeffs.txt'.format(calfile),coeffs)
 	return baselines,freqs,coeffs
 
-def test_regress(gs=10,n_sig=5,na=32,readFromFile=False):
+def read_coeffs(calfile,na=32):
+	"""
+	This function reads the coefficients in front of the global signal 
+	from the data file created in get_coeffs run with the smae calfile. 
+	"""
+	# baselines = n.loadtxt('{0}_baselines.txt'.format('psa898_v002'))
+	# freqs = n.loadtxt('{0}_frequencies.txt'.format('psa898_v002'))
+	# coeffs = n.loadtxt('{0}_coeffs.txt'.format('psa898_v002'))	
+	f = n.load('./coeff_data/{0}_data.npz'.format(calfile))
+	freqs = f['frequencies']
+	baselines = f['baselines']
+	coeffs = f['coeffs']
+	baselines = baselines[:na]
+	if len(freqs)>1: freqs = freqs[:na]
+	coeffs = coeffs[:na]
+	return baselines,freqs,coeffs
+
+def test_regress(calfile,gs=1,n_sig=5,na=32,readFromFile=False):
+	"""
+	This function tests the linear regression method of recovering the global 
+	signal by generating a visibility VV with random normal noise of amplitude 
+	n_sig and then performing a linear regression on VV and the coefficients 
+	from get_coeffs(). 
+	"""
 	if readFromFile:
-		baselines = n.loadtxt('{0}_baselines.txt'.format('psa898_v002'))
-		freqs = n.loadtxt('{0}_frequencies.txt'.format('psa898_v002'))
-		coeffs = n.loadtxt('{0}_coeffs.txt'.format('psa898_v002'))
-		baselines = baselines[:na]
-		#freqs = freqs[:na]
-		coeffs = coeffs[:na]
+		baselines,freqs,coeffs = read_coeffs(calfile,na=na)
 	else:
-		baselines,freqs,coeffs = get_coeffs(na=na,freqs=n.array([.1,]))
+		baselines,freqs,coeffs = get_coeffs(calfile,na=na,freqs=n.array([.1,]))
 		coeffs=coeffs[:,0]
-	coeffs=coeffs.T
-	VV = gs*coeffs + n.random.normal(loc=0.0,scale=n_sig,size=len(coeffs))
-	print VV.shape
+	#coeffs=coeffs.T
+	print coeffs.shape
+	VV = gs*coeffs + n.random.normal(loc=0.0,scale=n_sig,size=[len(coeffs),1])
 	VV = VV*n.exp(2*n.pi*1j*n.random.rand())
 	print VV.shape
-	#AA = n.array([coeffs,n.ones(len(coeffs))])
-	# AA = n.vstack([coeffs, n.ones(len(coeffs))]).T
-	# print AA
-	# print VV.shape
-	# print AA.shape
-	#gs_recov,n_recov = n.linalg.lstsq(AA,VV)[0]
-	gs_recov,n_recov,RR,redchi = hp.linear_fit(coeffs,VV)
+	gs_recov,n_recov,RR,redchi = uf.linear_fit(coeffs,VV)
 	print "true gs = {0}\trecovered gs = {1}".format(gs,gs_recov)
 	print "true n = {0}\trecovered n = {1}".format(0.0,n_recov)
 	print "R = ",RR
-	return gs_recov
+	print 'chi = ',redchi
+	return gs_recov, redchi
 
-def test_regress_vary_n():
+def test_regress_thru_origin(calfile,gs=1,n_sig=5,na=32,readFromFile=False):
+	"""
+	This function tests the linear regression method of recovering the global 
+	signal by generating a visibility VV with random normal noise of amplitude 
+	n_sig and then performing a linear regression on VV and the coefficients 
+	from get_coeffs(). 
+	"""
+	if readFromFile:
+		baselines,freqs,coeffs = read_coeffs(calfile,na=na)
+	else:
+		baselines,freqs,coeffs = get_coeffs(calfile,na=na,freqs=n.array([.1,]))
+		coeffs=coeffs[:,0]
+	
+	print coeffs.shape
+	VV = gs*coeffs + n.random.normal(loc=0.0,scale=n_sig,size=[len(coeffs),1])
+	VV = VV*n.exp(2*n.pi*1j*n.random.rand())
+	print VV.shape
+
+	gs_recov,redchi = uf.line_thru_origin_fit(coeffs,VV)
+
+	print "true gs = {0}\trecovered gs = {1}".format(gs,gs_recov)
+	print 'chi = ',redchi
+	return gs_recov, redchi
+
+def test_regress_vary_n(calfile,nants=32):
 	for jj in n.arange(100):
 		gs_diff = n.zeros(20)
 		n_sigs = n.logspace(-3,1,num=20)
@@ -81,47 +121,67 @@ def test_regress_vary_n():
 			#if ii==0 and jj==0:rFF=False 
 			#else:rFF=True
 			rFF=True
-			gs_diff[ii] = test_regress(gs=1,n_sig=n_sig,readFromFile=rFF) - 1.
-		p.plot(n_sigs,gs_diff)
+			gs_recov, redchi = test_regress(calfile,gs=1,n_sig=n_sig,na=nants,readFromFile=rFF)
+			#gs_diff[ii] = gs_recov - 1
+			if n.absolute(redchi-1)<0.1:
+				gs_diff[ii] = gs_recov - 1.
+			else:
+				gs_diff[ii] = None
+		p.scatter(n_sigs,n.absolute(gs_diff))
 		p.xscale('log')
+		p.yscale('log')
+		p.xlim(1e-4,1e2)
 	p.xlabel('Amplitude of noise relative to global signal\n(I.e. true global signal amplitude is 1)')
+	#p.ylabel('Recovered global signal (true gs = 1)')
 	p.ylabel('Difference between true and recovered global signal')
 	#p.show()
-	p.savefig('gs_diff_vs_n.pdf')
+	p.savefig('./figures/gs_diff_vs_n_good_chi.pdf')
 	p.clf()
 
-def test_regress_vary_na():
+def test_regress_vary_na(calfile,nants=32):
 	for jj in n.arange(100):
-		nas = n.arange(3,32)
+		nas = n.arange(2,nants)
 		gs_diff = n.zeros(len(nas))
 		for ii,na in enumerate(nas):
-			gs_diff[ii] = test_regress(gs=1,n_sig=.1,na=na,readFromFile=True) - 1.
-		p.plot(nas,gs_diff)
+			gs_recov, redchi = test_regress(calfile,gs=1,n_sig=.1,na=na,readFromFile=True)
+			#gs_diff[ii] = gs_recov - 1.
+			if n.absolute(redchi-1)<0.1:
+				gs_diff[ii] = gs_recov - 1.
+			else:
+				gs_diff[ii] = None
+		p.scatter(nas,gs_diff)
 	p.xlabel('Number of antenna')
 	p.ylabel('Difference between true and recovered global signal')
 	#p.show()
-	p.savefig('gs_diff_vs_na.pdf')
+	p.savefig('./figures/gs_diff_vs_na_goodchi.pdf')
 	p.clf()
 	
 
-def test_regress_vary_bsln():
+def test_regress_vary_bsln(calfile,nants=32):
 	for jj in n.arange(100):
-		nas = n.arange(3,32)
+		nas = n.arange(2,nants)
 		gs_diff = n.zeros(len(nas))
 		for ii,na in enumerate(nas):
-			gs_diff[ii] = test_regress(gs=1,n_sig=.1,na=na,readFromFile=True) - 1.
-		p.plot(nas*(nas-1)/2,gs_diff)
+			gs_recov, redchi = test_regress(calfile,gs=1,n_sig=.1,na=na,readFromFile=True)
+			#gs_diff[ii] = gs_recov - 1.
+			if n.absolute(redchi-1)<0.1:
+				gs_diff[ii] = gs_recov - 1.
+			else:
+				gs_diff[ii] = None
+		p.scatter(nas*(nas-1)/2,gs_diff)
 	p.xlabel('Number of baselines')
 	p.ylabel('Difference between true and recovered global signal')
 	#p.show()
-	p.savefig('gs_diff_vs_bsln.pdf')
+	p.savefig('./figures/gs_diff_vs_bsln_goodchi.pdf')
 	p.clf()
 
 
 if __name__=='__main__': 
+	calfile='psa898_v002'
+	#calfile='basic_amp_aa'
 	#get_coeffs(na=32,freqs=n.array([.1,]))
-	test_regress_vary_n()
-	test_regress_vary_na()
-	test_regress_vary_bsln()
+	test_regress_vary_n(calfile,nants=32) 
+	test_regress_vary_na(calfile,nants=32)
+	test_regress_vary_bsln(calfile,nants=32)
 
 
--- mep/useful_functions.py
+++ mep/useful_functions.py
@@ -1,5 +1,6 @@
 import pylab as plt 
 import numpy as np
+from scipy import optimize
 
 def linear_fit(xdata,ydata):
 	if len(xdata.shape)==1: 
@@ -22,6 +23,67 @@ def linear_fit(xdata,ydata):
 
 	# calculate reduced chi^2 value
 	sig = ss_tot/len(ydata)
-	redchi = np.sum(np.absolute((ydata-fdata))**2/sig)/len(ydata)
+	redchi = np.sum(np.absolute((ydata-fdata))**2/sig)/(len(ydata) - 2 - 1)
 
 	return A,B,Rsq,redchi
+
+def line_thru_origin_fit(xdata,ydata):
+	# fitfunc = lambda p, x: p*x 
+	# errfunc = lambda p, x, y: (y - fitfunc(p, x))
+	# p0 = 1.0 #initial guess for parameter
+	# out = optimize.leastsq(errfunc, np.array([p0,]), args=(xdata, ydata),full_output=1)
+	# pf = out[0]
+	pf, _, _, _  = np.linalg.lstsq(xdata,ydata)
+	pf = pf[0][0]
+	print pf
+
+	# calculate reduced chi^2 value
+	fdata = pf*xdata
+	sig = np.sum(np.absolute((ydata-np.average(ydata)))**2)/len(ydata)
+	redchi = np.sum(np.absolute((ydata-fdata))**2/sig)/(len(ydata)-1-1)
+
+	return pf,redchi
+
+def power_law_lstsq_fit(xdata,ydata):
+	"""
+	This power-law fitting is done by first converting
+	to a linear equation and then fitting to a straight line.
+	 y = a * x^b
+	 log(y) = log(a) + b*log(x) 
+	""" 
+	logx = np.log10(xdata)
+	logy = np.log10(ydata)
+	#logyerr = yerr / ydata
+ 
+	# define our (line) fitting function
+	fitfunc = lambda p, x: p[0] + p[1] * x
+	errfunc = lambda p, x, y: (y - fitfunc(p, x))
+	#errfunc = lambda p, x, y, err: (y - fitfunc(p, x)) / err
+  
+	prm0 = [1.0, -1.0] #initial guess for parameters
+	out = optimize.leastsq(errfunc, prm0, args=(logx, logy),full_output=1)#, logyerr), full_output=1)
+	prm = out[0] 
+	covar = out[1] 
+	print prm
+	print covar
+
+	index = prm[1]
+	amp = 10.0**prm[0]
+	indexErr = np.sqrt(covar[0][0])
+	ampErr = np.sqrt(covar[1][1])*amp
+
+	# calculate reduced chi^2 value
+	fdata = amp*xdata**index
+	sig = np.sum(np.absolute((ydata-np.average(ydata)))**2)/len(ydata)
+	redchi = np.sum(np.absolute((ydata-fdata))**2/sig)/(len(ydata)-2-1)
+
+	return amp, index, ampErr, indexErr, redchi
+
+def legendre_lstsq_fit(xdata,ydata,deg):
+	coefs = np.polynomial.legendre.legfit(xdata, ydata, deg)
+	fdata = np.polynomial.legendre.legval(xdata,coefs)
+	# calculate reduced chi^2 value
+	sig = np.sum(np.absolute((ydata-np.average(ydata)))**2)/len(ydata)
+	redchi = np.sum(np.absolute((ydata-fdata))**2/sig)/(len(ydata)-deg-1)
+	return coefs, redchi
+
