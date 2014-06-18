7f099d7eb3a6d92c5fc8cd0ae2ef1f9d029876f3
mpresley42 <mpresley@princeton.edu>
2014-06-18 11:05:42 -0700

    Fixed the problem with the coefficient calculation. Now the code will calculate the Q matrix for a set of baselines for as many spherical harmonics as desired. Note that to get one entry in the Q matrix is ~33 sec, so it will take a long time to run.
    Also made a function to plot the Q matrix entry as a function of l and colored according to m.

--- mep/sph_harm_coeffs.py
+++ mep/sph_harm_coeffs.py
@@ -2,21 +2,20 @@ import aipy as a, numpy as n, pylab as p
 import capo as C
 import useful_functions as uf
 from scipy import special
+import matplotlib as mpl
 
-def get_coeffs_lm(calfile,l,m,na = 32,freqs = n.arange(.1,)):
+def get_coeffs_lm(calfile,l,m,na = 32,freqs = n.array([.1,]),savefolderpath=None):
     """
     This function calculates the coefficients in front of the lm spherical
     harmonic for the antenna array described in the calfile. The coefficients 
     are determined by the integral of A(l,m)Y(l,m)exp[-i2pi(ul+vm)]dldm.
-
-    Not working yet. Doesn't give same results for l,m=0,0 as previous get_coeff code.
     """
     aa = a.cal.get_aa(calfile, n.array([.150])) #get antenna array
     im = a.img.Img(size=200, res=.5) #make an image of the sky to get sky coords
     tx,ty,tz = im.get_top(center=(200,200)) #get coords of the zenith?
     valid = n.logical_not(tx.mask)
     tx,ty,tz = tx.flatten(),ty.flatten(),tz.flatten()
-    theta = n.arcsin(ty/tx) # using math convention of theta=[0,2pi], phi=[0,pi]
+    theta = n.arctan(ty/tx) # using math convention of theta=[0,2pi], phi=[0,pi]
     phi = n.arccos(n.sqrt(1-tx*tx-ty*ty))
     #n.set_printoptions(threshold='nan')
     #print theta,phi
@@ -41,41 +40,70 @@ def get_coeffs_lm(calfile,l,m,na = 32,freqs = n.arange(.1,)):
             kk=0
             for fq in freqs: #loop over frequencies
                 phs = n.exp(-2j*n.pi*fq * (bx*tx+by*ty+bz*tz)) #fringe pattern
-                Y = special.sph_harm(m,l,theta,phi)/Ynorm #using math convention of theta=[0,2pi], phi=[0,pi]
+                Y = n.array(special.sph_harm(m,l,theta,phi))/Ynorm #using math convention of theta=[0,2pi], phi=[0,pi]
                 Y.shape = phs.shape = amp.shape = im.uv.shape
                 amp = n.where(valid, amp, 0)
-                phs = n.where(valid, phs, 0)
-                
-                Y = n.where(valid, Y, 0) # What in the world???? Before this line, Y is masked or one. After this line, Y is zero or 0.282
-
-                n.set_printoptions(threshold='nan')
-                print Y
-                quit() 
-
+                phs = n.where(valid, phs, 0)                
+                Y = n.where(valid, Y, 0) 
+                # n.set_printoptions(threshold='nan')
+                # print Y
+                # quit() 
                 dc_response = n.sum(amp*Y*phs)/n.sum(amp) #this integrates the amplitude * fringe pattern; units mK?
+                print 'dc = ',dc_response
                 jy_response = n.real(dc_response * 100 / C.pspec.jy2T(fq)) # jy2T converts flux density in jansky to temp in mK
                 print '\t',fq, dc_response, jy_response
                 coeffs[ll,kk] = dc_response
                 kk+=1
             ll+=1
-    n.savez_compressed('./coeff_data/{0}_data_l_{1}_m_{2}'.format(calfile,l,m),baselines=baselines,frequencies=freqs,coeffs=coeffs)
+    if savefolderpath!=None: n.savez_compressed('{0}{1}_data_l_{1}_m_{2}'.format(savefolderpath,calfile,l,m),baselines=baselines,frequencies=freqs,coeffs=coeffs)
     return baselines,freqs,coeffs
 
-def get_Q(calfile,max_l,savefolderpath=None):
-    for l in range(max_l+1):
-        for m in range(-max_l-1,max_l+1):
+def get_Q(calfile,min_l,max_l,savefolderpath=None):
+    for l in range(min_l,max_l+1):
+        for m in range(-l,l+1):
             print l,m
-            baselines,freqs,coeffs = get_coeffs_lm(calfile,l,m,na = 32,freqs = n.arange(.1,))
-            if l==m==0: Q = coeffs
+            baselines,freqs,coeffs = get_coeffs_lm(calfile,l,m,na = 2,freqs = n.array([.1,]))
+            if l==min_l and m==-min_l: Q = coeffs
             else: Q = n.hstack((Q,coeffs))
+            if l==min_l and m==-min_l: lms = n.array([l,m])
+            else: lms = n.vstack((lms,n.array([l,m])))
     print Q.shape
-    if savefolderpath!=None: n.savez_compressed(savefolderpath+'{0}_Q_max_l_{1}'.format(calfile,max_l),Q=Q,baselines=baselines)
-    return Q
+    if savefolderpath!=None: n.savez_compressed(savefolderpath+'{0}_Q_min_l_{1}_max_l_{2}'.format(calfile,min_l,max_l),Q=Q,baselines=baselines,lms=lms)
+    return Q,baselines,lms
+
+def plot_Q():#Q,lms):
+    # n.savez_compressed('./coeff_data/basic_amp_aa_Q_min_l_0_max_l_8',
+
+    Qstuff = n.load('./coeff_data/basic_amp_aa_Q_min_l_0_max_l_10.npz')
+    Q = Qstuff['Q']
+    lms = Qstuff['lms']
+    p.scatter(lms[:,0],n.absolute(Q[0,:]),c=lms[:,1],cmap=mpl.cm.PiYG,s=50)
+    p.yscale('log')
+    p.ylim([10**-5,10**0.2])
+    p.xlabel('l (color is m)')
+    p.ylabel('Q for a baseline (1,1,0)')
+    p.colorbar()
+    p.show()
+
+def combine_Q(Q1file,Q2file,newfile):
+    Q1stuff = n.load(Q1file)
+    Q2stuff = n.load(Q2file)
+    Q = n.hstack((Q1stuff['Q'],Q2stuff['Q']))
+    lms = n.vstack((Q1stuff['lms'],Q2stuff['lms']))
+    baselines = n.vstack((Q1stuff['baselines'],Q2stuff['baselines']))
+    n.savez_compressed(newfile,Q=Q,baselines=baselines,lms=lms)
+    return Q, baselines, lms
 
 if __name__=='__main__':
     calfile='basic_amp_aa'
-    na=8
-    baselines,freqs,coeffs = get_coeffs_lm(calfile,0,0,na=na,freqs=n.array([.1,]))
-    print coeffs
-
-
+    #na=8
+    #baselines,freqs,coeffs = get_coeffs_lm(calfile,0,0,na=na,freqs=n.array([.1,]))
+    #print coeffs
+    #Q,baselines,lms = get_Q(calfile,9,10,savefolderpath='./coeff_data/')
+    # Q, baselines, lms = combine_Q('./coeff_data/basic_amp_aa_Q_min_l_0_max_l_4.npz',
+    #                             './coeff_data/basic_amp_aa_Q_min_l_5_max_l_8.npz',
+    #                             './coeff_data/basic_amp_aa_Q_min_l_0_max_l_8')
+    # Q, baselines, lms = combine_Q('./coeff_data/basic_amp_aa_Q_min_l_0_max_l_8.npz',
+    #                             './coeff_data/basic_amp_aa_Q_min_l_9_max_l_10.npz',
+    #                             './coeff_data/basic_amp_aa_Q_min_l_0_max_l_10')
+    plot_Q()#Q,lms)
