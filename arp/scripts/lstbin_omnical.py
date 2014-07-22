ee83d47c4f98af7608059f7bb4bee8b12e2af490
Aaron Parsons <aparsons@berkeley.edu>
2014-07-22 10:29:31 -0700

    A few more scripts for dealing with omnical npz files.

--- arp/scripts/lstbin_omnical.py
+++ arp/scripts/lstbin_omnical.py
@@ -2,11 +2,14 @@
 import numpy as n, aipy as a
 import sys
 
+seps = [6,11,12,13,21,24,25,26,28,30,32,35,44,47,48,49,50,52,64,84,85,99]
+seps = ['sep%d'%i for i in seps]
+
 def lstgridwgts(dlst, lstres):
     return n.array([-dlst/lstres, 1-n.abs(dlst/lstres), dlst/lstres]).clip(0,1).transpose()
 
-MASKSUN = True
-INTERPOLATE = True
+MASKSUN = False # already done in lstcmp_omnical
+INTERPOLATE = False
 
 lstres = 2 * n.pi * 42.8 / a.const.sidereal_day
 lstsum,lstwgt = {}, {}
@@ -20,50 +23,82 @@ for filename in sys.argv[1:]:
     npz = n.load(filename)
     lsts = n.around(npz['lsts'] / lstres) * lstres
     wgt = n.where(n.isnan(npz['chi2_lin']), 0, 1./npz['chi2_lin'])
-    if INTERPOLATE:
-        lst_gw = lstgridwgts(lsts-npz['lsts'], lstres)
-        _wgt = []
-        for i in range(len(lsts)):
-            w1,w2,w3 = wgt[max(i-1,0)], wgt[i], wgt[min(i+1,len(lsts)-1)]
-            gw1,gw2,gw3 = lst_gw[i]
-            _wgt.append(w1*gw1+w2*gw2+w3*gw3)
-        _wgt= n.array(_wgt)
-    else: _wgt = wgt
+    lst_gw = lstgridwgts(lsts-npz['lsts'], lstres)
     if MASKSUN:
         for i,t in enumerate(npz['times']):
             aa.set_jultime(t)
             sun.compute(aa)
             if sun.alt > 0: wgt[i] *= 0
-    for sep in npz.files:
+    for sep in seps:
         if not sep.startswith('sep'): continue
         if not lstsum.has_key(sep):
-            lstsum[sep],lstwgt = {}, {}
-        dat = n.where(wgt > 0, npz[sep], 0)
+            lstsum[sep],lstwgt[sep] = {}, {}
+        if 'gain_'+sep in npz.files:
+            #gt = npz['gain_'+sep]
+            gt = npz['gain_'+sep].clip(.5,1.5)
+            gt.shape = (gt.size,1)
+        else: gt = 1
+        if 'wgt_'+sep in npz.files: sep_wgt = wgt * npz['wgt_'+sep]
+        else: sep_wgt = wgt
+        dat = n.where(sep_wgt > 0, npz[sep]/gt, 0)
+        sep_wgt = n.where(n.isnan(dat), 0, sep_wgt)
+        dat = n.where(sep_wgt > 0, dat, 0)
+        print sep, n.any(n.isnan(dat))
         if INTERPOLATE:
-            _dat = []
+            _dat,_wgt = [], []
             for i in range(len(lsts)):
                 d1,d2,d3 = dat[max(i-1,0)], dat[i], dat[min(i+1,len(lsts)-1)]
-                w1,w2,w3 = wgt[max(i-1,0)], wgt[i], wgt[min(i+1,len(lsts)-1)]
+                w1,w2,w3 = sep_wgt[max(i-1,0)], sep_wgt[i], sep_wgt[min(i+1,len(lsts)-1)]
                 gw1,gw2,gw3 = lst_gw[i]
                 _dat.append(d1*w1*gw1+d2*w2*gw2+d3*w3*gw3)
+                _wgt.append(w1*gw1+w2*gw2+w3*gw3)
+            _wgt= n.array(_wgt)
             dat = n.array(_dat) / n.where(_wgt > 0, _wgt, 1)
+        else: _wgt = sep_wgt
+
+        # done in wideband_dly_omnical
+        #if True:
+        #    window = a.dsp.gen_window(dat[:,14:186].shape[1], 'blackman-harris'); 
+        #    window.shape = (1,window.size)
+        #    w = n.where(_wgt > 0, 1., 0)
+        #    _d = n.fft.ifft(dat[:,14:186]*window)
+        #    _w = n.fft.ifft(w[:,14:186]*window)
+
+        #    area = n.ones(dat[:,14:186].shape[1], dtype=n.int)
+        #    area[19:-18] = 0 # XXX gotta make this bl dependent
+
+        #    for i in range(dat.shape[0]):
+        #        g = n.sqrt(n.average(w[i]**2))
+        #        if g < .5 :
+        #            dat[i], w[i], _wgt[i] = 0, 0, 0
+        #            continue
+        #        _dcl,info = a.deconv.clean(_d[i], _w[i], tol=1e-9, area=area, stop_if_div=False, maxiter=100)
+        #        dmdl = n.fft.fft(_dcl)
+        #        dat[i,14:186] -= dmdl * w[i,14:186]
+
+        #    xtalk = n.sum(dat, axis=0) / n.sum(w, axis=0)
+        #    xtalk.shape = (1,xtalk.size)
+        #    dat = n.where(w > 0, dat-xtalk, 0)
+
         
         for i,lst in enumerate(lsts):
             lstsum[sep][lst] = lstsum[sep].get(lst,0) + dat[i] * _wgt[i]
-            lstwgt[lst] = lstwgt.get(lst,0) + _wgt[i]
+            lstwgt[sep][lst] = lstwgt[sep].get(lst,0) + _wgt[i]
 
-lsts = lstwgt.keys(); lsts.sort()
-data = {'wgt': n.array([lstwgt[lst] for lst in lsts])}
+lsts = lstwgt.values()[0].keys(); lsts.sort()
+data = {}
 
 for sep in lstsum.keys():
     print sep
-    d = []
+    d,w = [],[]
     for lst in lsts:
-        _d,_w = lstsum[sep].pop(lst),lstwgt[lst]
-        d.append(_d / n.where(_w > 0, _w, 1))
-    del(lstsum[sep])
-    data[sep] = n.array(d)
+        d.append(lstsum[sep].pop(lst))
+        w.append(lstwgt[sep].pop(lst))
+    del(lstsum[sep]); del(lstwgt[sep])
+    d,w = n.array(d), n.array(w)
+    data[sep] = d / n.where(w > 0, w, 1)
+    data['wgt_'+sep] = w
 
-filename = 'omnical_lstbin.npz'
+filename = 'omnical_lstbin_v2Bf_xx_1.npz'
 print 'Writing', filename
 n.savez(filename, lsts=n.array(lsts), **data)
--- arp/scripts/lstcmp_omnical.py
+++ arp/scripts/lstcmp_omnical.py
@@ -8,7 +8,7 @@ def lstgridwgts(dlst, lstres):
 
 MASKSUN = True
 
-SEP = 'sep6'
+SEP = 'sep34'
 
 lstres = 2 * n.pi * 42.8 / a.const.sidereal_day
 
@@ -28,8 +28,7 @@ for filename in sys.argv[2:]:
     print 'Reading', filename,
     npz = n.load(filename)
     lst_i = [n.argmin(n.abs(lsts - lst)) for lst in npz['lsts']]
-    lst_dwgt = n.array([lstnpz['wgt'][i] for i in lst_i])
-    lst_gw = lstgridwgts(n.array([lsts[i] for i in lst_i])-npz['lsts'], lstres)
+    lst_gw = lstgridwgts(npz['lsts'] - n.array([lsts[i] for i in lst_i]), lstres)
     print len(npz['times'])
     wgt = n.where(n.isnan(npz['chi2_lin']), 0, 1./npz['chi2_lin'])
     if MASKSUN:
@@ -38,71 +37,89 @@ for filename in sys.argv[2:]:
             sun.compute(aa)
             if sun.alt > 0: wgt[i] *= 0
     gsum,gwgt = 0, 0
+    info = dict(npz)
     #for sep in npz.files:
-    #for sep in ['sep%d'%i for i in range(10)]:
     for sep in [SEP]:
         if not sep.startswith('sep'): continue
         dat = n.where(wgt > 0, npz[sep], 0)
-        _dat,_wgt = [],[] # XXX could move wgt out
-        # XXX should instead interpolate lst data to measured lst
-        for i in range(len(lst_i)):
-            d1,d2,d3 = dat[max(i-1,0)], dat[i], dat[min(i+1,len(lst_i)-1)]
-            w1,w2,w3 = wgt[max(i-1,0)], wgt[i], wgt[min(i+1,len(lst_i)-1)]
+        lst_dat = []
+        # interpolate gridded lst data to actual lst of measured data
+        for i,L in enumerate(lst_i):
+            d1,d2,d3 = lstnpz[sep][max(L-1,0)], lstnpz[sep][L], lstnpz[sep][min(L+1,len(lsts)-1)]
             gw1,gw2,gw3 = lst_gw[i]
-            _dat.append(d1*w1*gw1+d2*w2*gw2+d3*w3*gw3)
-            _wgt.append(w1*gw1+w2*gw2+w3*gw3)
-        _dat = n.array(_dat)
-        _wgt= n.array(_wgt)
-        dat = _dat / n.where(_wgt > 0, _wgt, 1)
-        #_dat,_wgt = dat,wgt
+            lst_dat.append(d1*gw1+d2*gw2+d3*gw3)
+        lst_dat = n.array(lst_dat)
+        lst_dwgt = n.array([lstnpz['wgt'][i] for i in lst_i]) # thos one for first version of lstbin
+        #lst_dwgt = n.array([lstnpz['wgt_'+sep][i] for i in lst_i]) # this one for next version of lstbin
+        lst_dsum = lst_dat * lst_dwgt
         
-        lst_dsum = n.array([lstnpz[sep][i] for i in lst_i]) * lst_dwgt
-        g2 = dat * _wgt * n.conj(lst_dsum) * lst_dwgt
-        w2 = _wgt * n.conj(lst_dsum) * lst_dsum
-        #gsum += g2
-        #gwgt += w2
-        gsum += n.sum(g2, axis=-1)
-        gwgt += n.sum(w2, axis=-1)
-        if sep == SEP:
+        g2 = dat * wgt * n.conj(lst_dsum) * lst_dwgt
+        w2 = wgt * n.conj(lst_dsum) * lst_dsum
+        gsum = n.sum(g2, axis=-1) # average across the band
+        gwgt = n.sum(w2, axis=-1) # average across the band
+        sep_gt = gsum/gwgt
+        sep_gt.shape = (sep_gt.size, 1)
+        d = lst_dsum / n.where(lst_dwgt > 0, lst_dwgt, 1)
+        res = n.where(wgt > 0, dat/sep_gt - d, 0)
+        sig = n.sqrt(n.median(n.abs(res)**2))
+        sep_wgt = n.where(n.abs(res) > 3*sig, 0, 1)
+        info['gain_'+sep] = sep_gt.flatten()
+        info['wgt_'+sep] = sep_wgt
+        
+        #if sep == SEP:
+        if False:
             gt = n.where(gwgt > 0, gsum / gwgt, 0)
             gt.shape = (gt.size, 1)
-            d = lst_dsum / n.where(lst_dwgt > 0, lst_dwgt, 1)
-            res1.append(n.where(_wgt > 0, dat-d, 0))
-            res2.append(n.where(_wgt > 0, dat/gt - d, 0))
+            #res1.append(n.where(wgt > 0, dat-d, 0))
+            #res2.append(n.where(wgt > 0, dat/gt - d, 0))
+            #p.plot(npz['lsts'], dat[:,140], '+')
+            #p.plot(npz['lsts'], lst_dat[:,140], '.-')
+            #p.plot(npz['lsts'], dat[:,140]/gt[:,0], '.')
             #plt_y = dat[:,140]
             #plt_x = [npz['lsts'], lsts]
             #p.plot(plt_x[0], plt_y, '+')
             #p.plot(plt_x[1], plt_y / gt[:,0], '.')
-    gt = n.where(gwgt > 0, gsum / gwgt, 0)
-    gt.shape = (gt.size, 1)
-    g_vs_t.append(gt)
-    jds.append(npz['times'])
-    chi2.append(npz['chi2_lin'])
+            p.subplot(121); C.arp.waterfall(dat/gt, mode='lin', mx=1000, drng=1000)
+            p.colorbar(shrink=.5)
+            p.subplot(122); C.arp.waterfall(res_flg, mode='lin', mx=100, drng=100)
+            p.colorbar(shrink=.5)
+            p.show()
+    outfile = filename.split('.npz')[-2]+'_v2.npz'
+    print 'Writing', outfile
+    n.savez(outfile, **info)
+    #C.arp.waterfall(osum/owgt, mode='lin')
+    #p.colorbar()
+    #p.show()
+    #gt = n.where(gwgt > 0, gsum / gwgt, 0)
+    #gt.shape = (gt.size, 1)
+    #g_vs_t.append(gt)
+    #jds.append(npz['times'])
+    #chi2.append(npz['chi2_lin'])
 
 #p.show()
 
-g_vs_t = n.concatenate(g_vs_t, axis=0)
-jds = n.concatenate(jds, axis=0)
-chi2 = n.concatenate(chi2, axis=0)
-res1 = n.concatenate(res1, axis=0)
-res2 = n.concatenate(res2, axis=0)
-
-p.plot(jds, n.abs(g_vs_t[:,0]))
-p.plot(jds, n.angle(g_vs_t[:,0]))
-
-p.show()
-
-p.subplot(121)
-C.arp.waterfall(res1, mode='lin', mx=200, drng=200)
-p.colorbar(shrink=.5)
-
-p.subplot(122)
-C.arp.waterfall(res2, mode='lin', mx=200, drng=200)
-p.colorbar(shrink=.5)
-
-p.show()
-
-#import IPython; IPython.embed()
-import IPython.Shell; IPython.Shell.IPShellEmbed('')()
+#g_vs_t = n.concatenate(g_vs_t, axis=0)
+#jds = n.concatenate(jds, axis=0)
+#chi2 = n.concatenate(chi2, axis=0)
+#res1 = n.concatenate(res1, axis=0)
+#res2 = n.concatenate(res2, axis=0)
+#
+#p.plot(jds, n.abs(g_vs_t[:,0]))
+#p.plot(jds, n.angle(g_vs_t[:,0]))
+#
+#p.show()
+#
+#p.subplot(121)
+#C.arp.waterfall(res1, mode='lin', mx=200, drng=200)
+#p.colorbar(shrink=.5)
+#
+#p.subplot(122)
+#C.arp.waterfall(res2, mode='lin', mx=200, drng=200)
+#p.colorbar(shrink=.5)
+#
+#p.show()
+#
+##import IPython; IPython.embed()
+#import IPython.Shell; IPython.Shell.IPShellEmbed('')()
 
 
