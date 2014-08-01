ad48f1bec862c724038927056c4ae925dc39db94
mpresley42 <mpresley@princeton.edu>
2014-08-01 15:48:25 -0700

    Added functions from qgea to load Q and gsm files so that I wouldn't have to import qgea, which requires healpy, on nersc. Also added a function to compute MQdagNinv so that I would only compute it once for each array instead of for every array and frequency. Also made the plot look prettier.

--- mep/monte_carlo_analysis.py
+++ mep/monte_carlo_analysis.py
@@ -1,7 +1,6 @@
 import aipy as a, numpy as n, pylab as p
 from scipy.stats import norm
 import matplotlib as mpl
-import Q_gsm_error_analysis as qgea 
 
 """
 The monte carlo mpi code writes out a bunch of matrices. Each matrix has rows that 
@@ -15,6 +14,29 @@ Each row is a different set of visibilities, and each file has 10,000 rows.
 global data_loc; data_loc = '/global/scratch2/sd/mpresley/gs_data'
 global gsm_fits_file; gsm_fits_file = '/global/homes/m/mpresley/gs_mpi/general_files/fits_files/haslam408_extrap_fq_0.1_32.fits'
 
+def load_Q_file(gh='grid',del_bl=4.,num_bl=10,beam_sig=0.09,fq=0.05,lmax=10):
+    if gh=='grid':
+        Q_file = './Q_matrices/grid_del_bl_{0:.2f}_num_bl_{1}_beam_sig_{2:.2f}_Q_max_l_10.npz'.format(del_bl,num_bl,beam_sig)
+    elif gh=='hybrid':
+        Q_file = './Q_matrices/hybrid_del_bl_{0:.2f}_num_bl_{1}_Q_max_l_10.npz'.format(del_bl,num_bl)
+    
+    Qstuff = n.load(Q_file)
+    Q = Qstuff['Q']
+    Q = Q[:,0:(lmax+1)**2]
+    lms = Qstuff['lms']
+    lms = lms[0:(lmax+1)**2,:]
+    baselines = Qstuff['baselines']
+    return baselines,Q,lms 
+
+def total_noise_covar(ninst_sig,num_bl,fg_file):
+    Nfg_file = n.load(fg_file)
+    Nfg = Nfg_file['matrix']
+    #Nfg = Nfg[1:,1:]
+    #print 'fg ',Nfg.shape
+    Ninst = (ninst_sig**2)*n.identity(num_bl)
+    Ntot = Nfg + Ninst 
+    return Ntot
+
 def load_mc_data(mc_mat_loc):#del_bl=4.,num_bl=10,beam_sig=0.09,fq=0.05):
     #mc_mat_loc = '{0}/del_bl_{1:.2f}_num_bl_{2}_beam_sig_{3:.2f}'.format(mc_loc,del_bl,num_bl,beam_sig)
     for ii in range(1):
@@ -24,6 +46,14 @@ def load_mc_data(mc_mat_loc):#del_bl=4.,num_bl=10,beam_sig=0.09,fq=0.05):
         else:
             ys = n.vstack(ys,stuff['matrix'])
     return ys 
+
+def return_MQdagNinv(Q,N,num_remov=None):
+    Q = n.matrix(Q); N = n.matrix(N)
+    Ninv = uf.pseudo_inverse(N,num_remov=None) # XXX want to remove dynamically
+    info = n.dot(Q.H,n.dot(Ninv,Q))
+    M = uf.pseudo_inverse(info,num_remov=num_remov)
+    MQN = n.dot(M,n.dot(Q.H,Ninv))
+    return MQN 
 
 def forground_hist(del_bl=8.,num_bl=10,beam_sig=0.09,fq=0.1):
     save_tag = 'grid_del_bl_{0:.2f}_num_bl_{1}_beam_sig_{2:.2f}_fq_{3:.2f}'.format(del_bl,num_bl,beam_sig,fq)
@@ -45,8 +75,6 @@ def forground_hist(del_bl=8.,num_bl=10,beam_sig=0.09,fq=0.1):
     p.annotate('mu = {0:.2f}\nsigma = {1:.2f}'.format(mu,sigma), xy=(0.05, 0.95), xycoords='axes fraction')
     p.savefig('./figures/monte_carlo/true_sky_{0}.pdf'.format(save_tag))
     p.clf()
-
-
 
 def construct_gs_hist(del_bl=8.,num_bl=10,beam_sig=0.09,fq=0.1):
     save_tag = 'grid_del_bl_{0:.2f}_num_bl_{1}_beam_sig_{2:.2f}_fq_{3:.2f}'.format(del_bl,num_bl,beam_sig,fq)
@@ -81,32 +109,34 @@ def construct_gs_hist(del_bl=8.,num_bl=10,beam_sig=0.09,fq=0.1):
 def plot_spectrum(del_bl=8.,num_bl=10,beam_sig=0.09):
     save_tag_base = 'grid_del_bl_{0:.2f}_num_bl_{1}_beam_sig_{2:.2f}'.format(del_bl,num_bl,beam_sig)
     print save_tag_base
-    mu = n.array([]); sigma = n.array([]); fqs = n.array([])
+
+    baselines,Q,lms = load_Q_file(gh='grid',del_bl=del_bl,num_bl=num_bl,beam_sig=beam_sig,fq=fq,lmax=3)
+    N = total_noise_covar(0.1,baselines.shape[0],'{0}/gsm_matrices/{1}.npz'.format(data_loc,save_tag))
+    MQN = return_MQdagNinv(Q,N,num_remov=None)
+
+    mu = n.array([]); sigma = n.array([]) 
+    fqs = n.array([]); nums = n.array([])
+
     for mc_fold in os.listdir(data_loc):
         if save_tag_base in mc_fold:
                 ys = load_mc_data('{0}/monte_carlo/{1}'.format(data_loc,mc_fold))
                 fq = float(mc_fold.split('_')[-1])
                 alms_fg = qgea.generate_sky_model_alms(gsm_fits_file,lmax=3)
                 alms_fg = alms_fg[:,2]
-
-                baselines,Q,lms = qgea.load_Q_file(gh='grid',del_bl=del_bl,num_bl=num_bl,beam_sig=beam_sig,fq=fq,lmax=3)
-                N = qgea.total_noise_covar(0.1,baselines.shape[0],'{0}/gsm_matrices/{1}.npz'.format(data_loc,save_tag))
-
                 ahat00s = n.array([])
                 for ii in xrange(ys.shape[0]):
-                    _,ahat,_ = qgea.test_recover_alms(ys[ii,:],Q,N,alms_fg,num_remov=None)
+                    ahat = uf.vdot(MQN,ys[ii,:])
                     ahat00s = n.append(n.real(ahat[0]),ahat00s)
                 print ahat00s.shape
                 mu0,sigma0 = norm.fit(ahat00s)
                 n.append(mu,mu0); n.append(sigma,sigma0)
-                n.append(fqs,fq)
-    p.errorbar(fqs, mu, yerr=sigma, fmt='o',ecolor='Black')
+                n.append(fqs,fq); n.append(nums,ys.shape[0])
+    p.errorbar(fqs, mu, yerr=sigma, fmt='o',ecolor='Black',c=nums,cmap=mpl.cm.copper_r)
     p.title('Mean and sigma of recovered global signal for\n{0}'.format(save_tag_base))
     p.xlabel('Freq (GHz)')
     p.ylabel('Recovered a00')
     p.savefig('/global/homes/m/mpresley/gs_data/mc_spec_figs/{0}.pdf'.format(save_tag_base))
     p.clf()
-
 
 if __name__=='__main__':
     for del_bl in (4.,6.,8.):
