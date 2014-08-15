5bea6982d827270e3b7d7dc7deb3c9c39fd31d4f
mpresley42 <mpresley@princeton.edu>
2014-08-15 14:02:40 -0700

    Changed sandwich and generate_G to handle multiple frequency stuff.

--- mep/Nfg/sandwich.py
+++ mep/Nfg/sandwich.py
@@ -16,18 +16,24 @@ if __name__=='__main__':
     nside = int(sys.argv[1])
     npix = 12 * nside * nside
     KvectFname = sys.argv[2]
-    GmatrixFname = sys.argv[3]
-    outputFname = sys.argv[4]
+    DataFolder = sys.argv[3]
+    GmatrixFname = sys.argv[4]
+    outputFname = sys.argv[5]
 
-    Gmatrix = np.load(GmatrixFname)
+    Gmatrix = np.load('{0}/{1}.npz'.format(DataFolder,GmatrixFname))
+    print 'Gmatrix loaded...'
     Kvect = np.load(KvectFname)
+    print 'Kvect loaded...'
     Kmatrix = vect2sq(npix,Kvect)
 
     print "Doing first multiplication..."
+    print Gmatrix.shape
+    print Kmatrix.shape
     GKmatrix = np.einsum('ijm,jk',Gmatrix,Kmatrix)
     print "Doing second multiplication..."
+    print GKmatrix.shape
     GKGdagger = np.einsum('ijm,jk',GKmatrix,np.conj(Gmatrix.T))
 
-    np.save(outputFname,GKGdagger)
+#    np.save(outputFname,GKGdagger)
     for ii,fq in enumerate(fqs):
-        n.savez_compressed('{0}_fq_{1}'.format(outputFname,fq),matrix=GKGdagger[:,:,ii])
+        n.savez_compressed('{0}/{1}_fq_{2}'.format(DataFolder,outputFname,fq),matrix=GKGdagger[:,:,ii])
