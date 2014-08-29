2168847156bf49e5d4d4e59191f71a417fbb1220
Adrian Liu <acliu@berkeley.edu>
2014-08-29 14:48:02 -0700

    Tanh model in fisher code

--- mep/fisher_matrix.py
+++ mep/fisher_matrix.py
@@ -8,7 +8,7 @@ def gaussian_model((A,nu0,sigma),nuvec):
 
 def tanh_model((T21,zr,delta_z),nuvec):
     zvec = (1420./nuvec) - 1
-    return (T21/2.)*n.sqrt((1+zvec)/10.)*(n.tanh((zvec-zr)/delta_z)-1.)
+    return (T21/2.)*n.sqrt((1+zvec)/10.)*(n.tanh((zvec-zr)/delta_z)+1.)
 
 def gaussian_model_derivs((A,nu0,sigma),nuvec):
     Aderiv = -n.exp(-0.5*(nuvec-nu0)**2/sigma**2)
@@ -18,23 +18,26 @@ def gaussian_model_derivs((A,nu0,sigma),nuvec):
 
 def tanh_model_derivs((T21,zr,delta_z),nuvec):
     zvec = (1420./nuvec) - 1
-    T21deriv = 0.5*n.sqrt((1.+zvec)/10.)*(n.tanh((zvec-zr)/delta_z)-1.)
+    T21deriv = 0.5*n.sqrt((1.+zvec)/10.)*(n.tanh((zvec-zr)/delta_z)+1.)
     zrderiv = -(T21/2.)*n.sqrt((1.+zvec)/10.)/(delta_z*n.cosh((zvec-zr)/delta_z)**2)
     delta_z = -(T21/2.)*n.sqrt((1.+zvec)/10.)*(zvec-zr)/(delta_z*n.cosh((zvec-zr)/delta_z))**2
     return T21deriv,zrderiv,delta_z
 
-def compute_bias((A,nu0,sigma),nuvec,Cinv,expBias):
+def compute_bias((A,nu0,sigma),nuvec,Cinv,expBias,model):
     bias = n.zeros(3)
-    derivs = gaussian_model_derivs((A,nu0,sigma),nuvec)
-    Finv = n.linalg.inv(fisher_matrix((A,nu0,sigma),nuvec,Cinv))
+    command = "derivs = " + str(model) + "_model_derivs((A,nu0,sigma),nuvec)"
+    exec command
+    Finv = n.linalg.inv(fisher_matrix((A,nu0,sigma),nuvec,Cinv,model))
     for ii in range(3):
         bias[ii] = n.dot(derivs[ii],n.dot(Cinv,expBias))
     bias = n.dot(Finv,bias)
     return bias
 
-def fisher_matrix((A,nu0,sigma),nuvec,Cinv):
+def fisher_matrix((A,nu0,sigma),nuvec,Cinv,model):
     fisher = n.zeros((3,3))
-    derivs = gaussian_model_derivs((A,nu0,sigma),nuvec)
+    command = "derivs = " + str(model) + "_model_derivs((A,nu0,sigma),nuvec)"
+    exec command
+    #derivs = gaussian_model_derivs((A,nu0,sigma),nuvec)
     for ii in range(3):
         for jj in range(3):
             fisher[ii,jj] = n.dot(n.transpose(derivs[ii]),uf.vdot(Cinv,derivs[jj]))
@@ -52,14 +55,14 @@ def fisher_select_pair(F,si,sj):
     Fnew = n.linalg.inv(Finv) #uf.pseudo_inverse(Finv)
     return Fnew 
 
-def plot_pairwise_contours(theta,nuvec,Cinv,lvls=(-2.291,-6.158,-11.618)):
+def plot_pairwise_contours(theta,nuvec,Cinv,model,lvls=(-2.291,-6.158,-11.618)):
     """
     > theta is a (3,) vector that contains the model parameters
     > thetavecs is a (n,3) matrix that contains the values of the parameters 
         that will be plotted over
     """
     labels = ['A','nu0','sigma']
-    fisher = fisher_matrix(theta,nuvec,Cinv)
+    fisher = fisher_matrix(theta,nuvec,Cinv,model)
     Finv = n.linalg.inv(fisher)
     print "F-inverse"
     print Finv
@@ -91,12 +94,20 @@ def plot_pairwise_contours(theta,nuvec,Cinv,lvls=(-2.291,-6.158,-11.618)):
         p.clf()
 
 if __name__=='__main__':
-    theta = n.array([2,50.,1])
-    nuvec = n.arange(30,100)
+    theta = n.array([0.020,10.,0.5])
+    nuvec = n.arange(100,170)
+    numFreqs = nuvec.shape[0]
+    inverseC = n.loadtxt("Cinv.dat")
+    #print nuvec
+    #print tanh_model_derivs(theta,nuvec)
+    biasVec =  40.*n.sin(nuvec)
+    print biasVec
+    hulu = compute_bias(theta,nuvec,inverseC,biasVec,"tanh")
+    print hulu
     #thetavecs = n.vstack((n.linspace(1,20,num=50),n.linspace(-5,5,num=50),n.linspace(0.1,1,num=50)))
     #Cinv = n.dot(nuvec.reshape((nuvec.shape[0],1)),nuvec.reshape(1,nuvec.shape[0]))
-    Cinv = n.eye(70)#uf.invertible_matrix(70)
-    plot_pairwise_contours(theta,nuvec,Cinv)
+    #Cinv = n.eye(70)#uf.invertible_matrix(70)
+    #plot_pairwise_contours(theta,nuvec,Cinv,model)
 
 
 
