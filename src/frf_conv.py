a15c4c65be92e2193eb724310a5a999cee30b382
Aaron Parsons <aparsons@berkeley.edu>
2015-02-09 16:46:21 -0800

    Undo fix to frf_conv because it makes the module internally inconsistent

--- src/frf_conv.py
+++ src/frf_conv.py
@@ -98,8 +98,7 @@ def get_oldfilter(aa, (bli,blj), inttime, nints, freqs):
            fr_bins, windowed_bwfrs
      
 def mk_fng(bl, ex, ey, ez):
-    # negative to match conjugation convention of AIPY
-    return -2*n.pi/a.const.sidereal_day * (bl[0]*ex + bl[1]*ey * n.sqrt(1-ez**2))
+    return 2*n.pi/a.const.sidereal_day * (bl[0]*ex + bl[1]*ey * n.sqrt(1-ez**2))
 
 def get_optimal_kernel_at_ref(aa, ch, (bli, blj), binwidth=.00005):
     '''
