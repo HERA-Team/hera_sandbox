e441c17ebae09d3e04b319e0d02b2796ceea71db
Carina Cheng <cacheng@folio.sas.upenn.edu>
2015-04-10 18:45:03 -0400

    batch script for running vis_simulation_v4.py

--- ctc/code/batch_vissim.sh
+++ ctc/code/batch_vissim.sh
@@ -1,7 +1,46 @@
 #$ -S /bin/bash
-ARGS = `pull_args.py $*` #give a chunk of times
+#$ -V
+#$ -cwd 
+#$ -l h_vmem=12G
+#$ -l paper
+#$ -o /data2/home/cacheng/capo/ctc/code/vissim.out
+#$ -e /data2/home/cacheng/capo/ctc/code/vissim.err
 
-vis_simulation_v4.py --sdf 0.001 --sfreq 0.1 --nchan 10 --inttime 20000 --map pspec --mappath /Users/carinacheng/capo/ctc/images/pspecs/pspec100lmax100/ --filename test.uv -C psa898_v003 -a 0_16 $ARGS
+echo CODE START
 
+startjd=2454500
+endjd=2454501
+inttime=5000
+numchunks=4 #number of processors
+nchunks=`seq ${numchunks}`
+echo nchunks: ${nchunks}
+
+ARGS=`python -c "import numpy; import aipy; print ' '.join(map(str,numpy.arange(${startjd},${endjd},${inttime}/aipy.const.s_per_day)))"`
+echo times: ${ARGS}
+
+numtimes=`python -c "import numpy; import aipy; print len(numpy.arange(${startjd},${endjd},${inttime}/aipy.const.s_per_day))"`
+echo numtimes: ${numtimes}
+
+startindex=1
+
+chunksize=$((${numtimes}/${numchunks}+1))
+echo numchunks: ${numchunks}
+echo chunksize: ${chunksize}
+
+loopargs=`pull_args.py ${nchunks}`
+
+for chunk in ${loopargs}; do
+
+    #echo ${ARGS} | cut -d " " -f ${startindex}-$((${startindex}+${chunksize}-1)) #list of times broken into chunks
+    list="$(echo ${ARGS} | cut -d " " -f ${startindex}-$((${startindex}+${chunksize}-1)))" #list of times broken into chunks
+    echo ${list}
+    name=`echo ${list} | cut -d " " -f 1` 
+    echo ${name}
+    #vis_simulation_v4.py --sdf 0.001 --sfreq 0.1 --nchan 10 --inttime 20000 --map pspec --mappath /data2/home/cacheng/capo/ctc/images/pspecs/pspec203lmax200/ --filename test.uv -C psa898_v003 -a 0_16 ${list}
+
+    
+    startindex=$((${startindex}+${chunksize}));
+ 
+done
 
 
