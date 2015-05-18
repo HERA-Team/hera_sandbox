87b3b4d73e5f2a8118abe7774e4529866733ae96
Yunfan G. Zhang <yunfanz@berkeley.edu>
2015-05-18 14:21:58 -0700

    pulled out interpolate

--- ygz/scripts/Paring_freq.py
+++ ygz/scripts/Paring_freq.py
@@ -5,7 +5,7 @@ import time as sys_time
 
 
 sz = 200
-sp = 1./sz
+d = 1./sz
 img = a.img.Img(200,res=0.5)   #400 by 400 image, i.e. 200 boxes, with 4 pixels per box,freq space kmax=100, dk=0.5
 X,Y,Z = img.get_top(center=(200,200))
 shape0 = X.shape
@@ -18,7 +18,7 @@ dt = 0.001
 dt_fine = 0.0004971027374267578
 times_coarse = n.arange(2456249.20169,2456249.50791, dt)
 times_fine = n.arange(2456249.20169,2456249.50791, dt_fine)
-dist = 1.2                           #size of cells to store in dictionary.
+dist = 0.2                           #size of cells to store in dictionary.
 corr_tol = 5000.                    #cutoff of minimum correlation
 aa = a.cal.get_aa('psa6240_v003',n.array(list_freq))
 src = a.fit.RadioFixedBody(0, aa.lat, janskies=0., mfreq=.18, name='test')
@@ -29,17 +29,16 @@ bmp_list  = export_beam.beam_real(aa[0], ntop, shape0, 'x')
 
 for ni in range(len(list_freq)):
     bmp = bmp_list[ni]
-    freq, fbmamp = export_beam.beam_fourier(bmp, sp, 400)
-    bm_intpl = export_beam.beam_interpol(freq,fbmamp,'cubic')
+    freq, fbmamp = export_beam.beam_fourier(bmp, d, 400)
     print 'Time to initialize:', sys_time.clock(), 'seconds'
 
     d = select_pair.pair_coarse(aa, src,times_coarse,dist)  #coarsely determine crossings
     print 'Time after coarse selection:', sys_time.clock(), 'seconds'
     #pairs_sorted = select_pair.pair_sort(d,freq,fbmamp)        #sort crossings
     #clos_app = select_pair.get_closest(pairs_sorted)           #determine closest approach points
-    clos_app = select_pair.alter_clos(d,bm_intpl)            #determine closest approach points
+    clos_app = select_pair.alter_clos(d,freq,fbmamp)            #determine closest approach points
     print 'Found closest approach points after:', sys_time.clock(), 'seconds'
-    pairs_final = select_pair.pair_fin(clos_app,dt_fine,aa,src,freq, fbmamp,False,False,False,5000.)  #output final sorted pairs
+    pairs_final = select_pair.pair_fin(clos_app,dt_fine,aa,src,freq,fbmamp,False,False,False,5000.)  #output final sorted pairs
     print 'Total time:', sys_time.clock(), 'seconds'
 
     #write result to file and screen
