54f56cf0f00153f4f1e37d49e34445d38979ae03
Kara Kundert <karakundert@gmail.com>
2015-05-29 19:45:13 -0400

    added PAPER specs and multiple frequencies with flat sky

--- kmk/scripts/zeroResponse.py
+++ kmk/scripts/zeroResponse.py
@@ -1,38 +1,38 @@
 #! /usr/bin/env python
 import aipy as a
+import capo
 import numpy as np
 import matplotlib.pylab as pl
 
 # NOTE: optimal baseline length formula given in Presley et al 2015 eq. 9
-
-freq = np.array([.100]) # in GHz
-c = 3e10 # in cm/s
-wvlen = c / freq # observed wavelength
-b = 4e2 / a.const.len_ns # baseline between antennae, in nanoseconds
-b = np.array([b, 0, 0])
-
 # fill h with flat temperature across whole sky (DC signal)
 h = a.healpix.HealpixMap(nside=64)
 h.map = np.ones(shape = h.map.shape)
 h.map = h.map*4*np.pi/h.npix()
-x, y, z = h.px2crd(np.arange(h.npix())) #topocentric
+#temps = np.ones(3)
+temps = np.array([0.5, 0.6, 0.8]) # sky temperature
+x, y, z = xyz = h.px2crd(np.arange(h.npix())) #topocentric
 
-# primary beam of antenna
-beam = a.amp.Beam2DGaussian(freqs = freq, xwidth=np.pi, ywidth=np.pi)
-beam = beam.response(xyz=(x,y,z))[0,:]
+freq = np.array([.120, .150, .180]) # in GHz
+c = 3e10 # in cm/s
+wvlen = c / freq # observed wavelength
+
+# import array parameters
+aa = a.cal.get_aa('psa6622_v001', freq)
+
+beam = aa[0].bm_response(xyz, pol='x')**2
+beam = beam[0]
+
+bx, by, bz = aa.get_baseline(0, 103)
+print bx, by, bz
+
+vis = np.zeros(shape = temps.shape, dtype=np.complex) 
 
 # attenuate sky signal by primary beam
-obs = beam * h.map
-
-phs = np.exp(-2j*np.pi*freq*(b[0]*x + b[1]*y + b[2]*z))
-
-vis = np.sum(np.where(z>0,obs*phs,0))
+for i in range(len(temps)):
+    obs = temps[i] * beam * h.map
+    phs = np.exp(-2j*np.pi*freq[i]*(bx*x + by*y + bz*z))
+    vis[i] = np.sum(np.where(z>0,obs*phs,0))
+    i += 1
 
 print vis
-
-
-#ants = []
-#ants.append(a.phs.Antenna(0,0,0, beam, delay=0, offset=0))
-#ants.append(a.phs.Antenna(0,b,0, beam, delay=0, offset=0))
-
-#aa = a.ant.AntennaArray(ants=ants, location=("18:20:39", "-66:45:10"))
