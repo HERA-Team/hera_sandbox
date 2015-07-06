a255af7db0c2984632c1be6bcc0d9e73671f7646
Danny Jacobs <daniel.c.jacobs@asu.edu>
2015-07-06 12:59:53 -0400

    adding pol to omnical bin/p files

--- dcj/scripts/batch_omnical.sh
+++ dcj/scripts/batch_omnical.sh
@@ -8,6 +8,7 @@
 #$ -o grid_output
 CAL=psa6622_v001
 #NOTE: To be run in ~jacobsda/storage/psa128
+#NOTE: Use full path.
 #. /usr/global/paper/CanopyVirtualEnvs/shredddercanopyrc.sh
 #canopy-PAPER_Omni
 PATH=/home/jacobsda/src/anaconda/bin/:$PATH
@@ -17,10 +18,10 @@ for FILE in $FILES
 do
 FILEBASE=`python -c "import os; print os.path.basename('${FILE}')"`
 echo Processing $FILE
-echo omnical_PSA128.py -C ${CAL} -pyy -o /data4/paper/2014EoR/Analysis/ProcessedData/ -i \
-redundantinfo_first_cal_epoch1.bin -r calpar_first_cal_epoch1.p -u -d $FILEBASE -t O -s $FILE 
-time omnical_PSA128.py -C ${CAL} -pyy -o /data4/paper/2014EoR/Analysis/ProcessedData/epoch2/ -i \
-redundantinfo_first_cal_epoch2.bin -r calpar_first_cal_epoch2.p -u -d $FILEBASE -t O -s $FILE 
+echo omnical_PSA128.py -C ${CAL} -pyy -o /data4/paper/2014EoR/Analysis/ProcessedData/epoch3 -i \
+redundantinfo_first_cal_epoch3yy.bin -r calpar_first_cal_epoch3yy.p -u -d $FILEBASE -t O -s $FILE 
+time omnical_PSA128.py -C ${CAL} -pyy -o /data4/paper/2014EoR/Analysis/ProcessedData/epoch3/ -i \
+redundantinfo_first_cal_epoch3yy.bin -r calpar_first_cal_epoch3yy.p -u -d $FILEBASE -t O -s $FILE 
 
 done
 
