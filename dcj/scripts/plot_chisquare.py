08b451fffc8f0d54578b85a8540c96db810f7804
Danny Jacobs <daniel.c.jacobs@asu.edu>
2015-07-06 13:00:49 -0400

    refining chisquare plot

--- dcj/scripts/plot_chisquare.py
+++ dcj/scripts/plot_chisquare.py
@@ -14,24 +14,24 @@ for datafile in sys.argv[1:]:
     
     data.shape = (nt, (nf + nprefix))
     data = data[:, nprefix:]
-    print data.shape
     chisq.append(data)
     media.append(n.median(data,axis=0))
     means.append(n.mean(data,axis=0))
+    print '.',
 chisq = n.concatenate(chisq)
 media = n.vstack(media)
 means = n.vstack(means)
 print chisq.shape,media.shape
 imshow(n.log(chisq),aspect='auto',vmin=-6)
 colorbar()
-figure()
-imshow(n.log(media),aspect='auto',vmin=-6,interpolation='nearest',vmax=-3)
-title('chisq median per file')
-colorbar()
-figure()
-imshow(n.log(means),aspect='auto',vmin=-6,interpolation='nearest',vmax=-3)
-colorbar()
-figure()
-for i in range(100):
-    plot(n.log(means[i,:])+n.std(means)*i)
+#figure()
+#imshow(n.log(media),aspect='auto',vmin=-6,interpolation='nearest',vmax=-3)
+#title('chisq median per file')
+#colorbar()
+#figure()
+#imshow(n.log(means),aspect='auto',vmin=-6,interpolation='nearest',vmax=-3)
+#colorbar()
+#figure()
+#for i in range(100):
+#    plot(n.log(means[i,:])+n.std(means)*i)
 show()
