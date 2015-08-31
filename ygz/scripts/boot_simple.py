3711f472856edfaab8af8e54cf6982de10dfdbd6
Yunfan Zhang <yunfanzhang@airbears2-10-142-60-187.airbears2.1918.berkeley.edu>
2015-08-31 15:46:46 -0700

    checked boot_simple

---
+++ ygz/scripts/boot_simple.py
@@ -0,0 +1,35 @@
+__author__ = 'yunfanzhang'
+
+import numpy as n
+import random
+
+def cross_mult(data1, data2):
+    return n.multiply(n.conjugate(data1), data2)
+
+#Bootstrap over a 2d array
+def bootstrap(B, data):
+    boot = []
+    for b in range(B):
+        temps = n.array([])
+        for i in range(len(data[0])):    #for each channel
+            temps = n.append(temps,0.)
+            for j in range(len(data)):      #bootstrap over all the time samples
+                choi = random.choice(data)   #choi contains all channels of a time sample
+                temps[i] = temps[i] + choi[i]
+        boot.append(temps/float(len(data)))
+    boot = n.array(boot).transpose()
+    print boot.shape
+    bootmean,booterr = [],[]
+    for ch in range(len(boot)):
+        mean = n.sum(boot[ch])/float(B)
+        sig = n.sqrt(n.sum((boot[ch]-mean)**2)/(B-1.))
+        bootmean.append(mean)
+        booterr.append(2*sig)
+
+    return bootmean, booterr
+#p.hist(boot[5])
+#p.show()
+
+#simple test
+#x = n.array([[1, 2, 3], [4, 5, 6],[7,8,9],[10,11,12],[13,14,15]])
+#bm, be = boot_simple.bootstrap(100, x)
