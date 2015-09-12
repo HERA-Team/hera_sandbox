18e6ab67e04fe1677b5c4b4451c72dc2a8f5bd2f
Carina Cheng <cacheng@folio.sas.upenn.edu>
2015-09-11 21:23:50 -0400

    edited xtalk stuff

--- ctc/code/omni_apply.sh
+++ ctc/code/omni_apply.sh
@@ -10,6 +10,6 @@ ARGS=`pull_args.py $*`
 
 for f in ${ARGS}; do
     echo working on ${f}...
-    omni_apply.py --omnipath /data4/paper/2014EoR/Analysis/ProcessedData/epoch2/omni_v2/ ${f}
+    omni_apply.py --omnipath /data4/paper/2014EoR/Analysis/ProcessedData/epoch3/omni_v2/ --omniruntag x ${f}
 done
 
--- ctc/code/omni_run.sh
+++ ctc/code/omni_run.sh
@@ -10,6 +10,6 @@ ARGS=`pull_args.py $*`
 
 for f in ${ARGS}; do
     echo working on ${f}...
-    omni_run.py --calpar /data4/paper/2014EoR/Analysis/ProcessedData/epoch3/calpar_first_cal_epoch3xx.p --redinfo /data4/paper/2014EoR/Analysis/ProcessedData/epoch3/redundantinfo_first_cal_epoch3xx.bin --omnipath /data4/paper/2014EoR/Analysis/ProcessedData/epoch3/omni_v2/ ${f}
+    omni_run.py --calpar /data4/paper/2014EoR/Analysis/ProcessedData/epoch3/calpar_first_cal_epoch3xx.p --redinfo /data4/paper/2014EoR/Analysis/ProcessedData/epoch3/redundantinfo_first_cal_epoch3xx.bin --omnipath /data4/paper/2014EoR/Analysis/ProcessedData/epoch3/omni_v2/ --xtalk --omniruntag x ${f}
 done
 
