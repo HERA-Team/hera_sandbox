caf53ea90b1c68f51d4c3cccb05115de85227df8
Carina Cheng <cacheng@folio.sas.upenn.edu>
2015-09-17 15:23:06 -0400

    echos pull_args arguments

--- ctc/code/omni_run.sh
+++ ctc/code/omni_run.sh
@@ -6,10 +6,13 @@
 #$ -l h_vmem=12G
 #$ -l scratch=9G
 #$ -l paper
+#$ -N OMNI_RUN
 ARGS=`pull_args.py $*`
+
+echo ${ARGS}
 
 for f in ${ARGS}; do
     echo working on ${f}...
-    omni_run.py --calpar /data4/paper/2014EoR/Analysis/ProcessedData/epoch3/calpar_first_cal_epoch3xx.p --redinfo /data4/paper/2014EoR/Analysis/ProcessedData/epoch3/redundantinfo_first_cal_epoch3xx.bin --omnipath /data4/paper/2014EoR/Analysis/ProcessedData/epoch3/omni_v2/ --xtalk --omniruntag x ${f}
+    omni_run.py --calpar /data4/paper/2014EoR/Analysis/ProcessedData/epoch2/calpar_first_cal_epoch2xx.p --redinfo /data4/paper/2014EoR/Analysis/ProcessedData/epoch2/redundantinfo_first_cal_epoch2xx.bin --omnipath /data4/paper/2014EoR/Analysis/ProcessedData/epoch2/omni_v2_alllsts/ ${f}
 done
 
