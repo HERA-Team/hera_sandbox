61e74a4ee304e0c8447ef5b2d3941b4e6bdfb793
Danny Jacobs <daniel.c.jacobs@asu.edu>
2015-09-18 16:05:28 -0400

    Checking in a bunch of useful stuff.

---
+++ dcj/scripts/pull_antpols.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+import aipy as a, sys, optparse, os
+
+o = optparse.OptionParser()
+a.scripting.add_standard_options(o, ant=True, pol=True)
+opts,args = o.parse_args(sys.argv[1:])
+
+for filename in args:
+    outfile = os.path.basename(filename)+'A'
+    print filename, '->', outfile
+    uvi = a.miriad.UV(filename)
+    if os.path.exists(outfile):
+        print '    File exists... skipping.'
+        continue
+    a.scripting.uv_selector(uvi, ants=opts.ant)
+    if not opts.pol is None: a.scripting.uv_selector(uvi, pol_str=opts.pol)
+    uvo = a.miriad.UV(outfile,status='new')
+    uvo.init_from_uv(uvi)
+    uvo.pipe(uvi,append2hist='PULL ANTPOLS:'+' '.join(sys.argv)+'\n')
