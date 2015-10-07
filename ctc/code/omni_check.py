89be984e99094d4318157144d4aa631f9dafe2f9
Carina Cheng <cacheng@folio.sas.upenn.edu>
2015-10-06 20:50:57 -0400

    plots gains per antenna from omni_output.npz files

--- ctc/code/omni_check.py
+++ ctc/code/omni_check.py
@@ -12,7 +12,11 @@ import os, sys
 
 ### Options ###
 o = optparse.OptionParser()
-o.set_usage('omini_check.py [options] *omni_output.npz')
+o.set_usage('omni_check.py [options] *omni_output.npz')
+o.add_option('--chisq',dest='chisq',default=False,action="store_true",
+            help='Plot chisq.')
+o.add_option('--gains',dest='gains',default=False,action="store_true",
+            help='Plot gains of each antenna solved for.')
 o.set_description(__doc__)
 #o.add_option('-C',dest='cal',default='psa6622_v003',type='string',
 #            help='Path and name of calfile.')
@@ -23,7 +27,7 @@ pol = 'xx' #XXX shouldn't be hard-coded in the future
 
 
 ### Plot ChiSq ####
-if args[0][-3:] == 'npz':
+if opts.chisq == True:
     chisqs = []
     for i,file in enumerate(args):
         print 'Reading',file
@@ -41,4 +45,37 @@ if args[0][-3:] == 'npz':
     plt.colorbar()
     plt.show()
 
+
+### Plot Gains ###
+if opts.gains == True:
+    gains = {}
+    for i, file in enumerate(args): #loop over files
+        print 'Reading',file
+        file = numpy.load(file)
+        for key in file.keys(): #loop over antennas
+            if "gains" in key:
+                gain = file[key]
+                antnum = key.split(',')[2]
+                try: gains[antnum].append(gain) #XXX only works if files are same size in time and freq
+                except: gains[antnum] = [gain]
+    for key in gains.keys():
+        gains[key] = numpy.array(gains[key])
+        gains[key] = gains[key].reshape(gains[key].shape[0]*gains[key].shape[1],gains[key].shape[2])
+
+    subplotnum = 1
+    plotnum = 1
+    plt.figure(plotnum,figsize=(10,10))
+    for ant in gains.keys(): #loop over antennas
+        if subplotnum == 26:
+            #break #only generate one page of plots (faster for testing) 
+            plotnum += 1
+            plt.figure(plotnum,figsize=(10,10))
+            subplotnum = 1
+        plt.subplot(5,5,subplotnum)
+        plt.imshow(numpy.abs(gains[ant]),vmax=1.5,aspect='auto',interpolation='nearest')
+        plt.title(ant,fontsize=10)
+        plt.tick_params(axis='both',which='major',labelsize=6)
+        subplotnum += 1
+    plt.tight_layout()
+    plt.show()
 
