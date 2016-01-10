c1a32ecf06a3fcccd77a8828f70d9a5ac86a8ff3
Aaron Parsons <aparsons@berkeley.edu>
2016-01-10 15:34:59 -0800

    Made arp/scripts/omni_recal.py output results to npz.  Tweaked omni_apply.py to permit translating uv file name to appropriate npz file.

--- arp/scripts/omni_recal.py
+++ arp/scripts/omni_recal.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 import aipy as ap, numpy as np, capo
-import sys
+import sys, os
 import pylab as plt
 
 POL = 'x'
@@ -13,14 +13,18 @@ reds = info.get_reds()
 cal_ants = {}
 for filename in sys.argv[1:]:
     print 'Reading', filename
+    outfile = filename[:-len('.npz')] + '2.npz'
+    if os.path.exists(outfile):
+        print '    %s exists.  Skipping...' % outfile
+        continue
     meta,gains,vismdl,xtalk,jds,lsts,freqs = capo.omni.from_npz(filename)
     mdl_bls = vismdl[DPOL]
     good_ants = gains[POL].copy()
-    del(good_ants[10]) # XXX do this to test a known good antenna with a known solution
+    #del(good_ants[10]) # XXX do this to test a known good antenna with a known solution
     use_bls = {}
     for gp in reds:
         gp1 = [(i,j) for i,j in gp if good_ants.has_key(i) ^ good_ants.has_key(j)]
-        mdl = [bl for bl in gp if mdl_bls.has_key(bl)]
+        mdl = [bl for bl in gp if mdl_bls.has_key(bl)] # XXX I think info should be able to do this cleaner
         if len(mdl) != 1: continue
         for i,j in gp1: use_bls[(i,j)] = mdl[0]
     # Time to work with data
@@ -38,23 +42,8 @@ for filename in sys.argv[1:]:
         gmdl = gains[POL][j][ti].conj() * mdl.conj()
         gsum[i] = gsum.get(i,0) + np.where(f, 0, d * gmdl.conj())
         gwgt[i] = gwgt.get(i,0) + np.where(f, 0, np.abs(gmdl)**2)
-        #if i == 10:
-        #    dmdl = gains[POL][i][ti] * gmdl
-        #    plt.subplot(211)
-        #    plt.plot(np.abs(d))
-        #    plt.plot(np.abs(dmdl))
-        #    plt.subplot(212)
-        #    plt.plot(np.angle(d))
-        #    plt.plot(np.angle(dmdl))
-        #    plt.show()
     bad_ants = gsols[-1][0].keys()
-    #for i in gains[POL]:
-    #for i in [10]:
-    #    plt.semilogy(np.abs(np.average(gains[POL][i], axis=0)), label=str(i), color='k')
     for i in bad_ants:
-    #for i in [10]:
-        gains[POL][i] = np.array([gsum[i]/gwgt[i] for gsum,gwgt in gsols])
-        plt.semilogy(np.abs(np.average(gains[POL][i], axis=0)), label=str(i))#, color='r')
-    plt.legend(loc='best')
-    plt.show()
-    import IPython; IPython.embed()
+        gains[POL][i] = np.array([np.where(gwgt[i] > 0, gsum[i]/gwgt[i], 0) for gsum,gwgt in gsols])
+    # XXX haven't filled in xtalk at all
+    capo.omni.to_npz(outfile, meta, gains, vismdl, xtalk, jds, lsts, freqs)
