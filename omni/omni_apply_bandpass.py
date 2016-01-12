9f433ad14190a3ec4da02d1f1bacbaaeb525019a
Carina Cheng <cacheng@folio.sas.upenn.edu>
2016-01-11 20:01:54 -0500

    applies bandpass solutions from *ucalbandpass.npz to all files within that npz

--- omni/omni_apply_bandpass.py
+++ omni/omni_apply_bandpass.py
@@ -8,12 +8,9 @@ import optparse
 
 ### Options ###
 o = optparse.OptionParser()
-o.set_usage('omni_apply_bandpass.py *uvcRREO')
+o.set_usage('omni_apply_bandpass.py *uvcRRE.ucalbandpass.npz')
 o.set_description(__doc__)
 opts,args = o.parse_args(sys.argv[1:])
-
-#Load passband
-bp = numpy.load('bandpass.npz')['bandpass']
 
 def mfunc(uv,p,d):
     d /= bp
@@ -21,12 +18,17 @@ def mfunc(uv,p,d):
 
 #Apply passband
 for file in args:
-    print file, '->', file+'u'
-    if os.path.exists(file+'u'):
-        print '    File exists... skipping.'
-        continue
-    uvi = aipy.miriad.UV(file)
-    uvo = aipy.miriad.UV(file+'u', status='new')
-    uvo.init_from_uv(uvi)
-    uvo.pipe(uvi,mfunc=mfunc,append2hist='OMNI_APPLY_PASSBAND:'+' '.join(sys.argv)+'\n')
+    bp = numpy.load(file)['bandpass'] 
+    files = numpy.load(file)['files']
+    for fl in files:
+        ifile = fl.split('.npz')[0]+'M' #applies bandpass to model vis UV file
+        ofile = ifile+'u'
+        print ifile, '->', ofile
+        if os.path.exists(ofile):
+            print '    File exists... skipping.'
+            continue
+        uvi = aipy.miriad.UV(ifile)
+        uvo = aipy.miriad.UV(ofile, status='new')
+        uvo.init_from_uv(uvi)
+        uvo.pipe(uvi,mfunc=mfunc,append2hist='OMNI_APPLY_PASSBAND:'+' '.join(sys.argv)+'\n')
 
