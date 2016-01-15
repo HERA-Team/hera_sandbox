d702333a49f0a61f56183da86c240d25560356eb
Aaron Parsons <aparsons@berkeley.edu>
2016-01-14 17:04:27 -0800

    Wrapped input to omni.redcal

--- src/omni.py
+++ src/omni.py
@@ -95,7 +95,19 @@ def aa_to_info(aa, pols=['x'], **kwargs):
 
 def redcal(data, info, xtalk=None, gains=None, vis=None,removedegen=False, uselogcal=True, maxiter=50, conv=1e-3, stepsize=.3, computeUBLFit=True, trust_period=1):
     # XXX add layer to support new gains format
-    meta, gain, vis = omnical.calib.redcal(data, info, xtalk=xtalk, gains=gains, vis=vis, removedegen=removedegen, uselogcal=uselogcal, maxiter=maxiter, conv=conv, stepsize=stepsize, computeUBLFit=computeUBLFit, trust_period=trust_period)    
+    if gains:
+        _gains = {}
+        for pol in gains:
+            for i in gains[pol]:
+                ai = Antpol(i,pol,info.nant)
+                _gains[int(ai)] = gains[pol][i]
+    if vis:
+        _vis = {}
+        for pol in vis:
+            for i,j in vis[pol]:
+                ai,aj = Antpol(i,pol,info.nant), Antpol(j,pol,info.nant)
+                _vis[(int(ai),int(aj))] = vis[pol][(i,j)]
+    meta, gains, vis = omnical.calib.redcal(data, info, xtalk=xtalk, gains=_gains, vis=_vis, removedegen=removedegen, uselogcal=uselogcal, maxiter=maxiter, conv=conv, stepsize=stepsize, computeUBLFit=computeUBLFit, trust_period=trust_period)    
     # rewrap to new format
     def mk_ap(a): return Antpol(a, info.nant)
     for i,j in meta['res']:
@@ -110,52 +122,17 @@ def redcal(data, info, xtalk=None, gains=None, vis=None,removedegen=False, uselo
             ant = int(k.split('chisq')[1])
             meta['chisq'+str(mk_ap(ant))] = meta.pop(k)
         except(ValueError): pass
-    for i in gain.keys():
+    for i in gains.keys():
         ap = mk_ap(i)
-        if not gain.has_key(ap.pol()): gain[ap.pol()] = {}
-        gain[ap.pol()][ap.ant()] = gain.pop(i)
+        if not gains.has_key(ap.pol()): gains[ap.pol()] = {}
+        gains[ap.pol()][ap.ant()] = gains.pop(i)
     for i,j in vis.keys():
         api,apj = mk_ap(i),mk_ap(j)
         pol = api.pol() + apj.pol()
         bl = (api.ant(), apj.ant())
         if not vis.has_key(pol): vis[pol] = {}
         vis[pol][bl] = vis.pop((i,j))
-    return meta, gain, vis
-
-def redcal_polkeys(data, info, xtalk=None, gains=None, vis=None,removedegen=False, uselogcal=True, maxiter=50, conv=1e-3, stepsize=.3, computeUBLFit=True, trust_period=1):
-    _meta, _gain, _vis = omnical.calib.redcal(data, info, xtalk=None, gains=None, vis=None, removedegen=False, uselogcal=True, maxiter=50, conv=1e-3, stepsize=.3, computeUBLFit=True, trust_period=1)    
-    meta, gain, vis, res = {}, {}, {}, {}
-    mk_ap = lambda a: Antpol(a,NUMPOL[ant / info.nant], info.nant)
-    Npols = info.nant/max(_gain.keys())
-    for pi in range(Npols): #gains and chisqare per Antpol
-        gains[NUMPOL[pi]] = {} 
-        meta[NUMPOL[pi]] = {}
-        for pj in range(Npols): #res and vis per Antpol pair
-            vis[NUMPOL[pi]+NUMPOL[pj]] = {}
-            res[NUMPOL[pi]+NUMPOL[pj]] = {} 
-    for key in _meta.keys():
-        if key=='iter': #XXX what is iter there for?
-            meta[key] = _meta[key]
-            continue
-        if key=='chisq': #XXX what is this there for (no ant index)?
-            meta[key] = _meta[key]
-            continue
-        if key=='res':
-            for i,j in _meta[key].keys():
-                api,apj = mk_ap(i),mk_ap(j)
-                res[api.pol()+apj.pol()][(api.ant(),apj.ant())] = _meta[key][(i,j)]
-            meta['res'] = res
-            continue
-        ant = int(key.split('chisq')[1])
-        ap = mk_ap(ant)
-        meta[ap.pol()]['chisq'+str(ap.ant())] = _meta[key]
-    for ant in _gain.keys():
-        ap = mk_ap(ant)
-        gain[ap.pol()][ap.ant()] = _gain[key]
-    for i,j in _vis.keys(): #XXX this could happen in the "res" loop above
-        api,apj = mk_ap(i),mk_ap(j)
-        vis[api.pol()+apj.pol()][(api.ant(),apj.ant())] = _vis[(i,j)]
-    return meta, gain, vis
+    return meta, gains, vis
 
 def compute_xtalk(res, wgts):
     '''Estimate xtalk as time-average of omnical residuals.'''
