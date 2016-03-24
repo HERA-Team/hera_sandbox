73372ca6c51e7559e3066dca3f28209da1394466
Aaron Parsons <aparsons@berkeley.edu>
2016-03-24 11:21:45 -0700

    Moved plotting routines from arp to plot.  Fixed import error in red.py

--- src/arp.py
+++ src/arp.py
@@ -1,6 +1,6 @@
-import aipy as a, numpy as n, pylab as P
+import aipy as a, numpy as n, pylab as plt
 import sys, scipy
-from mpl_toolkits.basemap import Basemap
+from plot import data_mode, waterfall, plot_hmap_ortho
 
 def get_dict_of_uv_data(filenames, antstr, polstr, decimate=1, decphs=0, verbose=False, recast_as_array=True):
     info = {'lsts':[], 'times':[]}
@@ -81,28 +81,6 @@ def gen_ddr_filter(shape, dw, drw, ratio=.25, invert=False):
 def rms(d,wgt=None):
     if wgt == None: return n.sqrt(n.average(n.abs(d)**2))
     else: return n.sqrt(n.sum(n.abs(d)**2) / n.sum(n.abs(wgt)**2))
-
-def data_mode(data, mode='abs'):
-    if mode.startswith('phs'): data = n.angle(data)
-    elif mode.startswith('lin'):
-        data = n.absolute(data)
-        #data = n.masked_less_equal(data, 0)
-    elif mode.startswith('real'): data = data.real
-    elif mode.startswith('imag'): data = data.imag
-    elif mode.startswith('log'):
-        data = n.absolute(data)
-        data = n.log10(data)
-    else: raise ValueError('Unrecognized plot mode.')
-    return data
-
-def waterfall(d, mode='log', mx=None, drng=None, recenter=False, **kwargs):
-    if n.ma.isMaskedArray(d): d = d.filled(0)
-    if recenter: d = a.img.recenter(d, n.array(d.shape)/2)
-    d = data_mode(d, mode=mode)
-    if mx is None: mx = d.max()
-    if drng is None: drng = mx - d.min()
-    mn = mx - drng
-    return P.imshow(d, vmax=mx, vmin=mn, aspect='auto', interpolation='nearest', **kwargs)
 
 def redundant_bl_cal(d1, w1, d2, w2, fqs, use_offset=False, maxiter=10, window='blackman-harris',
         clean=1e-4, verbose=False, tau=0., off=0.):
@@ -192,36 +170,6 @@ def selfcal_diff(m_bl, r_ant, r_wgt=1e6):
     Pinv = n.linalg.pinv(P) # this succeeds where lstsq fails, for some reason
     C = n.dot(Pinv, M)
     return dict(zip(ants,C))
-
-def plot_hmap_ortho(h, cmap='jet', mode='log', mx=None, drng=None, 
-        res=0.25, verbose=False):
-    m = Basemap(projection='ortho',lat_0=90,lon_0=180,rsphere=1.)
-    if verbose:
-        print 'SCHEME:', h.scheme()
-        print 'NSIDE:', h.nside()
-    lons,lats,x,y = m.makegrid(360/res,180/res, returnxy=True)
-    lons = 360 - lons
-    lats *= a.img.deg2rad; lons *= a.img.deg2rad
-    y,x,z = a.coord.radec2eq(n.array([lons.flatten(), lats.flatten()]))
-    ax,ay,az = a.coord.latlong2xyz(n.array([0,0]))
-    data = h[x,y,z]
-    data.shape = lats.shape
-    data /= h[0,0,1]
-    #data = data**2 # only if a voltage beam
-    data = data_mode(data, mode)
-    m.drawmapboundary()
-    m.drawmeridians(n.arange(0, 360, 30))
-    m.drawparallels(n.arange(0, 90, 10))
-    if mx is None: mx = data.max()
-    if drng is None:
-        mn = data.min()
-    #    if min < (max - 10): min = max-10
-    else: mn = mx - drng
-    return m.imshow(data, vmax=mx, vmin=mn, cmap=cmap)
-    #step = (mx - mn) / 10
-    #levels = n.arange(mn-step, mx+step, step)
-    #map.contourf(cx,cy,data,levels,linewidth=0,cmap=cmap)
-    
 
 def sinuspike(d, fqs, f=None, clean=1e-3, maxiter=100, nsig=3, window='blackman-harris'):
     d = d * (fqs/.150)**2.5 # Flatten noise assuming synchrotron spectral index
