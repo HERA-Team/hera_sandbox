113634073ca244d05e162a4deaabbb3a52514095
Kara Kundert <karakundert@gmail.com>
2016-04-01 13:40:07 -0700

    initial version of multi-time sim using resampled topocentric GSM

--- kmk/scripts/zeroResponse.py
+++ kmk/scripts/zeroResponse.py
@@ -2,31 +2,24 @@
 import aipy as a
 import capo
 import numpy as np
-import matplotlib.pylab as pl
-import optparse, sys
 import os
+from GlobalSkyModel import GlobalSkyModel
 
-sim_dir = '/home/kara/capo/kmk/scripts/'
-data_dir = '/home/kara/capo/kmk/data/'
-data_file = 'zen.2456895.51490.xx.uvcRRE'
-calfile = 'test'
-gsm_dir = '/home/kara/capo/kmk/gsm/gsm_raw/'
-
-def bl2delay(array, bl):
+def bl2delay(array, ij):
     """ converts unique baseline numbers into nanosecond delays """
-    i, j = a.miriad.bl2ij(bl)
-    bx, by, bz = aa.get_baseline(i, j)
-    return np.sqrt(bx**2 + by**2 + bz**2)
+    i,j = ij
+    bl = array.get_baseline(i, j)
+    return np.sqrt(np.dot(bl, bl))
 
-def calcFreq(array, bl, ref_bl, ref_freq, min_freq, max_freq):
-    """ calculates nuCal frequencies based on an initial frequency, reference baseline, 
+def calcFreq(array, ij, ref_ij, ref_freq, min_freq, max_freq):
+    """ calculates nuCal frequencies (in GHz) based on an initial frequency, reference baseline, 
         and input baseline. must include min and max frequencies for array in order to
         ensure that calculated frequencies are legal. """
-    f = ref_freq * (ref_bl / baseline)
-    if max_freq >= f >= min_freq:
-        return f
-    else:
-        raise ValueError("invalid frequency in baseline %d,%d" % (i,j))
+    bl = bl2delay(array, ij)
+    ref_bl = bl2delay(array, ref_ij)
+    f = ref_freq * (ref_bl / bl)
+    assert(max_freq >= f >= min_freq)
+    return f
 
 def makeFlatMap(nside, freq, Tsky=1.0):
     """ fill sky map of given size and frequency with flat temperature across whole sky,
@@ -42,46 +35,6 @@ def hpm_TtoJy(hpm, freq):
     wvlen = a.const.c / freq
     hpm.map *= (4*np.pi/hpm.npix())*2*a.const.k/wvlen**2 # convert to Jy to make summable
     return hpm
-
-def makeGSM(path, filename, sfreq, sdf, num):
-    """ runs the MIT multi-frequency global sky model simulation at given frequencies 
-
-        required input variables:
-        path: provides pathname to gsmmf.sh -- need to be in this directory to run sim
-        filename: desired output filenames for GSM maps
-        sfreq: desired starting frequency for your GSM maps
-        sdf: desired frequency spacings of maps
-        num: desired number of maps produced 
-        
-        GSM simulator can be downloaded at 
-        <<http://space.mit.edu/~angelica/gsm/index.html>> """
-    os.chdir(path)
-    os.system('rm -rf args.dat')
-    f = open('args.dat', 'w')
-    s = "%s %f %f %d" % (filename, sfreq, sdf, num)
-    print s
-    f.write(s)
-    f.close()
-    os.system(path+'gsmmf.sh')
-
-def makeGSMMap(array, nside, filename, freq, path='/home/kara/capo/kmk/gsm/gsm_raw/'):
-    """ create a Healpix map of a given size filled with a simulated global sky model
-        at a given frequency """
-    makeGSM(path=path, filename=filename, sfreq=freq, sdf=10, num=1)
-    g = a.healpix.HealpixMap(nside=512)
-    gsm = a.healpix.HealpixMap(nside=nside)
-    print "GSM map size = " + str(gsm.map.shape)
-    d = np.loadtxt(path + filename + str(1001) + '.dat')
-    g.map = d
-    g = hpm_TtoJy(g, freq)
-    gsm.from_hpm(g) # hack to lower resolution to prevent memory overload
-    # convert to topocentric coordinates
-    ga2eq = a.coord.convert_m('eq', 'ga', oepoch=array.epoch) #conversion matrix
-    eq2top = a.coord.eq2top_m(array.sidereal_time(),array.lat) #conversion matrix
-    ga2eq2top = np.dot(eq2top, ga2eq)
-    i, j, k = ijk = np.dot(ga2eq2top,gsm.px2crd(np.arange(gsm.npix()))) #topocentric
-    return gsm
-
 
 def extractData(uv):
     """ create arrays for visibilities for each baseline in uv data file for 
@@ -109,55 +62,85 @@ def extractData(uv):
     print "data collected"
     return d
 
-def calcVis(hpm, beam, bl, coord, freq):
+def calcVis(aa, hpm, ij, beam, freq, txyz):
+    # TODO: import array + GSMMap, calculate topocentric coordinates on the 
+    # fly, generate PB on the fly, include time
     """ simulate sky visibilities for a given baseline and primary beam, 
         provided with a sky map at a known frequency and its coordinate system """
-    x,y,z = coord
-    i, j = a.miriad.bl2ij(bl)
+    tx,ty,tz = txyz
+    i,j = ij
     print i, j
-    bx, by, bz = aa.get_baseline(i, j)
+    bxyz = bx, by, bz = aa.get_baseline(i, j, src='z')
     # attenuate sky signal and visibility by primary beam
-    obs_sky = hpm.map
-    #obs_sky = beam * hpm.map
-    phs = np.exp(np.complex128(-2j*np.pi*freq*(bx*x + by*y + bz*z)))
-    vis = np.sum(np.where(z>0, obs_sky*phs, 0))
+    obs_sky = beam * hpm.map
+    phs = np.exp(np.complex128(-2j*np.pi*freq*np.dot(bxyz, txyz)))
+    vis = np.sum(np.where(tz>0, obs_sky*phs, 0))
     return vis
 
-# select 150 MHz and 160 MHz for u-mode calibration test
-freq = 0.100
+if __name__ == '__main__':
+    import matplotlib.pylab as pl
+    import optparse, sys
 
-flatSky = makeFlatMap(nside=64, Tsky=1.0, freq=freq)
-xyz = flatSky.px2crd(np.arange(flatSky.npix())) #topocentric
+    o = optparse.OptionParser()
+    o.add_option('--sim_dir', default='/home/kara/capo/kmk/scripts/')
+    o.add_option('--data_dir', default='/home/kara/capo/kmk/data/')
+    o.add_option('--data_file', default='zen.2456895.51490.xx.uvcRRE')
+    o.add_option('--calfile', default='test')
+    o.add_option('--gsm_dir', default='/home/kara/capo/kmk/gsm/gsm_raw/')
 
-# create array of baselines
-test_ants = '(64)_(29,24,28,55,34,27,51,57)'
-parsed_ants = a.scripting.parse_ants(test_ants,8)
-bl = []
-for i in xrange(len(parsed_ants)):
-    bl.append(parsed_ants[i][0])
-bl = np.array(bl)
+    opts,args = o.parse_args(sys.argv[1:])
+    sim_dir = opts.sim_dir
+    data_dir = opts.data_dir
+    data_file = opts.data_file
+    calfile = opts.calfile
+    gsm_dir = opts.gsm_dir
 
-aa = a.cal.get_aa(calfile, np.array([freq]))
-ref_bl = bl2delay(aa, bl.max())
-freqs = []
-for i in xrange(len(bl)):
-    baseline = bl2delay(array=aa, bl=bl[i])
-    freqs.append(calcFreq(array=aa, bl=baseline, ref_bl=ref_bl, ref_freq=freq, min_freq=0.100, max_freq=0.200))
-freqs = np.array(freqs)
+    # select 150 MHz and 160 MHz for u-mode calibration test
+    freq = 0.100
+    aa = a.cal.get_aa(calfile, np.array([freq]))
 
-# import array parameters
-aa = a.cal.get_aa(calfile, freqs)
-aa_freqs = aa.get_freqs()
-beam = aa[0].bm_response(xyz, pol = 'x')**2
-print "array parameters imported"
+    flatSky = makeFlatMap(nside=64, Tsky=1.0, freq=freq)
 
-sim_data = []
-for i in xrange(len(bl)):
-    gsmMap = makeGSMMap(path=gsm_dir, filename='gsm', nside=64, freq=freqs[i], array=aa)
-    gsm_vis = calcVis(hpm=gsmMap, beam=beam[i], bl=bl[i], coord=xyz, freq=freqs[i])
-    vis_data = [a.miriad.bl2ij(bl[i]), freqs[i], gsm_vis]
-    sim_data.append(vis_data)
+    # create array of baselines
+    test_ants = '(64)_(29,24,28,55,34,27,51,57)'
+    parsed_ants = a.scripting.parse_ants(test_ants,8)
+    ij = []
+    for i in xrange(len(parsed_ants)):
+        ij.append(a.miriad.bl2ij(parsed_ants[i][0]))
+    ij = np.array(ij)
 
-np.array(sim_data)
-np.savez(sim_dir+'sim_output',sim_data)
-print sim_data
+    freqs = []
+    for i in xrange(len(ij)):
+        freqs.append(calcFreq(array=aa, ij=ij[i], ref_ij=ij[len(ij)-1], ref_freq=freq, min_freq=0.100, max_freq=0.200))
+    freqs = np.array(freqs)
+
+    # import array parameters
+    aa = a.cal.get_aa(calfile, freqs) # equatorial
+    aa_freqs = aa.get_freqs()
+    print "array parameters imported"
+
+    t = np.array([-3.0,-2.0,-1.0,0.0,1.0,2.0,3.0])
+    sim_data = []
+    for i in xrange(len(ij)):
+        gsmMap = a.healpix.HealpixMap(nside=64)
+        gsmMap.map = GlobalSkyModel(freq=1000*freqs[i], GSMlocation=gsm_dir, GSMNSIDE=64).map # galactic
+        print "GSM made at freq %f" % freqs[i]
+        # resample GSM into topocentric coords for each time step
+        for n in xrange(len(t)): 
+            gxyz = gx,gy,gz, = gsmMap.px2crd(np.arange(gsmMap.npix())) # galactic
+            ga2eq = a.coord.convert_m(isys='eq', osys='ga', oepoch=aa.epoch) 
+            # conversion matrix so the isys and osys are reversed
+            exyz = ex,ey,ez = np.dot(ga2eq, gsmMap.px2crd(np.arange(gsmMap.npix()))) # equatorial
+            txyz = tx,ty,tz = np.dot(a.coord.eq2top_m(t[n], aa.lat), exyz) # topocentric
+            # generate proper PB for each time step
+            beam = aa[0].bm_response(txyz, pol='x')**2 # topocentric
+            #gsmMap.to_fits("gal_coord_map.fits")
+            gsmMap.map = gsmMap[tx,ty,tz] # topocentric
+            #gsmMap.to_fits("top_coord_map.fits")
+            gsm_vis = calcVis(aa=aa, hpm=gsmMap, ij=ij[i], beam=beam, freq=freqs[i], txyz=txyz)
+            vis_data = [ij[i], t[n], freqs[i], gsm_vis]
+            sim_data.append(vis_data)
+
+    np.array(sim_data)
+    np.savez(sim_dir+'beam_sim_output',sim_data)
+    print sim_data
