c24bdbaed58531ef837d8ed08977f27cd80e7a75
Carina Cheng <cacheng@folio.sas.upenn.edu>
2016-04-14 15:27:17 -0400

    moved loops around so fill_between works in the loops

--- pspec_pipeline/plot_sigloss.py
+++ pspec_pipeline/plot_sigloss.py
@@ -3,25 +3,21 @@ import numpy as n, pylab as p
 from matplotlib import gridspec
 import glob
 
-fig = p.figure(1, figsize=(7,7))
-gs = gridspec.GridSpec(2,2, width_ratios=[1,.4], height_ratios=[.4,1])
-fig.subplots_adjust(left=.15, top=.95, bottom=.15, wspace=.15, hspace=.15, right=0.95)
 
-#fig2 = p.figure(2)
+### GETTING SIGLOSS DATA ###
+
 pCs,pIs = [],[]
 pCs_err,pIs_err = [],[]
 for inject in glob.glob('inject_*'):
     print 'Reading', inject
-#for inject in glob.glob('optimal/inject_*'):
-    pspecs = glob.glob(inject + '/pspec_bootsigloss*.npz')
+    pspecs = glob.glob(inject + '/pspec_boot*.npz') 
     inject = float(inject.split('_')[-1])
     pC_avg, pI_avg = [], []
     pC_spec, pI_spec = [], []
     for pspec in pspecs:
         npz = n.load(pspec)
-        #pC,pI = npz['pC'], npz['pI']
-        pC,pI = npz['pk_vs_t'], npz['nocov_vs_t']
-        pC_avg.append(n.average(pC.real))
+        pC,pI = npz['pk_vs_t'], npz['nocov_vs_t'] #(#chan, #times)
+        pC_avg.append(n.average(pC.real)) #avg over freq and timr
         pI_avg.append(n.average(pI.real))
         #pC_spec.append(n.average(pC.real, axis=1))
         #pI_spec.append(n.average(pI.real, axis=1))
@@ -36,11 +32,23 @@ for inject in glob.glob('inject_*'):
     #p.figure(1)
     #p.subplot(211); p.loglog([pI_avg], [pC_avg], 'k.')
     #p.subplot(212); p.loglog([pC_avg], [pI_avg/pC_avg], 'k.')
-#p.show()
 pIs,pCs = n.array(pIs), n.array(pCs)
 pIs_err,pCs_err = n.array(pIs_err), n.array(pCs_err)
 
+
+### GETTING PSPEC DATA ###
+
+npz = n.load('pspec.npz')
+kpls,pks,errs = npz['kpl'], npz['pk'], npz['err']
+
+
 ### PLOTTING ###
+
+fig = p.figure(1, figsize=(7,7))
+gs = gridspec.GridSpec(2,2, width_ratios=[1,.4], height_ratios=[.4,1])
+fig.subplots_adjust(left=.15, top=.95, bottom=.15, wspace=.15, hspace=.15, right=0.95)
+
+#Plot 2
 p.figure(1)
 pklo,pkhi = 1e3,1e13
 p.subplot(gs[2])
@@ -52,21 +60,13 @@ p.ylim(pklo, pkhi)
 p.xlabel(r'$P_{\rm in}(k)\ [{\rm mK}^2\ (h^{-1}\ {\rm Mpc})^3]$', fontsize=14)
 p.ylabel(r'$P_{\rm out}(k)\ [{\rm mK}^2\ (h^{-1}\ {\rm Mpc})^3]$', fontsize=14)
 p.grid()
-#p.show()
-
-npz = n.load('pspec.npz')
-#print npz.files
-"""
-kpls,pks,errs = npz['kpl'], npz['pk'], npz['err']
 for kpl,pk,err in zip(kpls,pks,errs):
     #p.([pklo,pkhi], [pk,pk], 'r')
     pkup = max(pk+err,1e-6)
     pkdn = max(pk-err,1e-6)
-    p.subplot(gs[2])
     p.fill_between([pklo,pkhi], [pkdn,pkdn], [pkup,pkup], facecolor='gray', edgecolor='gray')
-    p.subplot(gs[3])
-    p.fill_between([1e-3,20], [pkdn,pkdn], [pkup,pkup], facecolor='gray', edgecolor='gray')
-"""
+
+#Plot 3    
 ax3 = p.subplot(gs[3])
 p.setp(ax3.get_yticklabels(), visible=False)
 #p.loglog(n.clip(pIs/pCs - 1, 1e-3,n.Inf), pCs, 'k.')
@@ -81,7 +81,12 @@ p.xlim(1e-3,20)
 #p.xlim(1,10)
 p.grid()
 p.xlabel(r'$P_{\rm in}/P_{\rm out}-1$', fontsize=14)
+for kpl,pk,err in zip(kpls,pks,errs):
+    pkup = max(pk+err,1e-6)
+    pkdn = max(pk-err,1e-6)
+    p.fill_between([1e-3,20], [pkdn,pkdn], [pkup,pkup], facecolor='gray', edgecolor='gray')
 
+#Plot 1
 ax0 = p.subplot(gs[0])
 p.setp(ax0.get_xticklabels(), visible=False)
 p.errorbar(pIs, pCs/pIs, xerr=2*pIs_err, yerr=2*pCs_err/pIs, fmt='k.', capsize=0)
