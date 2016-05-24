56c9930889db32fc67e9fdd332e0db0f38b3d788
Carina Cheng <cacheng@folio.sas.upenn.edu>
2016-05-24 15:20:10 -0400

    works whether run in or out of directory with files

--- ctc/scripts/batch_hera_subarrays.sh
+++ ctc/scripts/batch_hera_subarrays.sh
@@ -15,44 +15,46 @@ PAPER_HEX=0,2,14,17,21,40,44,45,54,62,68,69,84,85,86,100,101,102,113
 IMG_ARR=1,3,4,13,15,16,23,26,37,38,41,42,46,47,49,50,56,57,58,59,61,63,66,67,70,71,73,74,82,83,87,90,98,99,103,106,114,115,116,117,118,119,120,121,122,123,124,125,126,127
 
 for FILE in ${FILES}; do 
-    NAME=${FILE##*/}
+    PATHY=$(dirname $FILE) #path to datafile
+    NAME=$(basename $FILE uv) #name of file without 'uv' at the end
+    BASE=${PATHY}/${NAME} 
     POL=${NAME:18:2}
     echo 'working on ' ${FILE}
-    
+ 
     if [[ ! -e ${NAME:0:21}PP.uv ]]; then
         echo pull_antpols.py -p ${POL} -a "($POL_ARR)_($POL_ARR)" ${FILE}
         pull_antpols.py -p ${POL} -a "($POL_ARR)_($POL_ARR)" ${FILE}
-        echo ${FILE}A -\> ${NAME:0:21}PP.uv
-        mv ${FILE}A ${NAME:0:21}PP.uv
+        echo ${FILE}A -\> ${BASE}PP.uv
+        mv ${FILE}A ${BASE}PP.uv
     else
-        echo ${NAME:0:21}PP.uv exists... skipping... 
+        echo ${BASE}PP.uv exists... skipping... 
     fi 
    
-    if [[ ! -e ${NAME:0:21}HH.uv ]]; then 
+    if [[ ! -e ${BASE}HH.uv ]]; then 
         echo pull_antpols.py -p ${POL} -a "($HERA_HEX)_($HERA_HEX)" ${FILE}
         pull_antpols.py -p ${POL} -a "($HERA_HEX)_($HERA_HEX)" ${FILE}
-        echo ${FILE}A -\> ${NAME:0:21}HH.uv
-        mv ${FILE}A ${NAME:0:21}HH.uv
+        echo ${FILE}A -\> ${BASE}HH.uv
+        mv ${FILE}A ${BASE}HH.uv
     else
-        echo ${NAME:0:21}HH.uv exists... skipping...
+        echo ${BASE}HH.uv exists... skipping...
     fi
 
-    if [[ ! -e ${NAME:0:21}PH.uv ]]; then
+    if [[ ! -e ${BASE}PH.uv ]]; then
         echo pull_antpols.py -p ${POL} -a "($PAPER_HEX)_($PAPER_HEX)" ${FILE}
         pull_antpols.py -p ${POL} -a "($PAPER_HEX)_($PAPER_HEX)" ${FILE} 
-        echo ${FILE}A -\> ${NAME:0:21}PH.uv
-        mv ${FILE}A ${NAME:0:21}PH.uv
+        echo ${FILE}A -\> ${BASE}PH.uv
+        mv ${FILE}A ${BASE}PH.uv
     else
-        echo ${NAME:0:21}PH.uv exists... skipping...
+        echo ${BASE}PH.uv exists... skipping...
     fi
 
-    if [[ ! -e ${NAME:0:21}PI.uv ]]; then
+    if [[ ! -e ${BASE}PI.uv ]]; then
         echo pull_antpols.py -p ${POL} -a "($IMG_ARR)_($IMG_ARR)" ${FILE}
         pull_antpols.py -p ${POL} -a "($IMG_ARR)_($IMG_ARR)" ${FILE}
-        echo ${FILE}A -\> ${NAME:0:21}PI.uv
-        mv ${FILE}A ${NAME:0:21}PI.uv
+        echo ${FILE}A -\> ${BASE}PI.uv
+        mv ${FILE}A ${BASE}PI.uv
     else
-        echo ${NAME:0:21}PI.uv exists... skipping...
+        echo ${BASE}PI.uv exists... skipping...
     fi
 
 done
