d1b2d40c26d2d234442833735bd26bfe126746c1
SaulAryehKohn <saul.aryeh.kohn@gmail.com>
2016-06-24 15:01:15 -0400

    less hardcoding: can now specify percentile cut on deviant antennae delays

--- omni/analyze_firstcal.py
+++ omni/analyze_firstcal.py
@@ -8,6 +8,7 @@ from matplotlib import pyplot as plt
 #plt.use('Agg')
 
 o = optparse.OptionParser()
+o.add_option('--pcnt',default=93,help='Percentile cut to make on standard deviants per antenna delay. Default is worst 7% of antennae.')
 o.add_option('--statfile',help='Full path and name of file to output mean and s.d. of delay solutions per antenna')
 o.add_option('--badantsin',help='Full path and name of badantfile for previous analyze_firstcal run')
 o.add_option('--badantsout',help='Full path and name of file to output list of bad antennae to')
@@ -32,7 +33,9 @@ for npzfile in args:
     for k in d.keys():
         if k.isdigit():
             try: DD[k].append(d[k+'d'][0])
-            except KeyError: print 'Key error %s ???'%k
+            except KeyError: 
+                print 'Key error %s'%k
+                continue
 
 stds = []
 #save antenna stats
@@ -42,7 +45,7 @@ for k in DD.keys():
     if not opts.statfile is None: print >> fstat, '%s %f %f'%(k, np.mean(DD[k]), np.std(DD[k]))
 if not opts.statfile is None: fstat.close()
 
-w = np.where(stds>=np.percentile(stds,93))[0] #XXX HARDCODED PERCENTILE CUT. AROOGAH
+w = np.where(stds>=np.percentile(stds,float(opts.pcnt)))[0] #XXX best stat to cut on?
 badants = list(np.array(DD.keys())[list(w)])
 bastring=','.join(sorted(badants))
 
