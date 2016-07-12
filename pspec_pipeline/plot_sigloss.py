3762ab8a481549683cdbb25818afde8950639c35
Carina Cheng <cacheng@folio.sas.upenn.edu>
2016-07-12 16:56:30 -0400

    plots P(k) points and grey bar based on pCv values. Does not handle errors yet.

--- pspec_pipeline/plot_sigloss.py
+++ pspec_pipeline/plot_sigloss.py
@@ -8,47 +8,49 @@ import glob
 
 ### GETTING SIGLOSS DATA ###
 
-pCs,pIs = [],[]
+pCs,pIs,pCvs = [],[],[]
 pCs_err,pIs_err = [],[]
 for inject in glob.glob('inject_*'):
     print 'Reading', inject
     pspecs = glob.glob(inject + '/pspec_boot*.npz') 
     inject = float(inject.split('_')[-1])
-    pC_avg, pI_avg = [], []
+    pC_avg, pI_avg, pCv_avg = [], [], []
     pC_spec, pI_spec = [], []
     for pspec in pspecs:
         npz = n.load(pspec)
-        pC,pI = npz['pk_vs_t'], npz['nocov_vs_t'] #(#chan, #times)
-        pC_avg.append(n.average(pC.real)) #avg over freq and timr
+        pC,pI,pCv = npz['pk_vs_t'], npz['nocov_vs_t'], npz['pCv'] #(#chan, #times)
+        kpls = n.array(npz['kpl'])
+        pC_avg.append(n.average(pC.real)) #avg over freq and time
         pI_avg.append(n.average(pI.real))
+        pCv_avg.append(n.average(pCv.real,axis=1)) #spectrum
         #pC_spec.append(n.average(pC.real, axis=1))
         #pI_spec.append(n.average(pI.real, axis=1))
         #pC_spec.append(pC.real)
         #pI_spec.append(pI.real)
-    pCs.append(n.average(pC_avg))
+    pCs.append(n.average(pC_avg)) #should bootstrap all these pC_avg's (20 of them) to get error bars... look at their distribution
     pCs_err.append(n.std(pC_avg)/n.sqrt(len(pspecs)))
     pIs.append(n.average(pI_avg))
     pIs_err.append(n.std(pI_avg)/n.sqrt(len(pspecs)))
+    pCvs.append(n.average(pCv_avg,axis=0)) #spectrum
     #pC_spec = n.average(pC_spec, axis=0)
     #print inject, pspec, pC_avg, pI_avg
     #p.figure(1)
     #p.subplot(211); p.loglog([pI_avg], [pC_avg], 'k.')
     #p.subplot(212); p.loglog([pC_avg], [pI_avg/pC_avg], 'k.')
-pIs,pCs = n.array(pIs), n.array(pCs)
+pIs,pCs,pCvs = n.array(pIs), n.array(pCs), n.array(n.average(pCvs,axis=0)) #avg over inject #s
 pIs_err,pCs_err = n.array(pIs_err), n.array(pCs_err)
-
 
 ###Build an interpolator to find sigloss factors###
 sig_factor_interp = interp1d(pIs, pIs/pCs,kind='linear',bounds_error=False,fill_value=0)
 
 ### GETTING PSPEC DATA ###
+# XXX only used to get 'freq' variable
 
 #npz = n.load('/data4/paper/2013EoR/Analysis/ProcessedData/epoch2/omni_v2_xtalk/lstbin_manybls/PS_frfnew/pspec_pk_k3pk.npz')
-#npz = n.load('/data4/paper/2013EoR/Analysis/ProcessedData/epoch2/omni_v2_xtalk/lstbin_manybls/PS_noise/pspec_pk_k3pk.npz') #purely noise 
-npz = n.load('/home/cacheng/capo/ctc/matt_data/noise_diffbls/pspec_pk_k3pk.npz') #matt's data
+npz = n.load('/data4/paper/2013EoR/Analysis/ProcessedData/epoch2/omni_v2_xtalk/lstbin_manybls/PS_noise/diff_bls/pspec_pk_k3pk.npz') #purely noise 
+#npz = n.load('/home/cacheng/capo/ctc/matt_data/noise_diffbls/pspec_pk_k3pk.npz') #matt's data
 
-kpls,pks,errs = npz['kpl'], npz['pk'], npz['err']
-
+#kpls,pks,errs = npz['kpl'], npz['pk'], npz['err']
 freq = npz['freq']
 z_bin = f2z(freq)
 
@@ -60,7 +62,7 @@ fig.subplots_adjust(left=.15, top=.95, bottom=.15, wspace=.35, hspace=.15, right
 
 #Plot 2
 p.figure(1)
-pklo,pkhi = 1e1,1e12
+pklo,pkhi = 1e-6,1e10
 #pklo,pkhi = 1e-10,1e5 #for noise only
 ax2 = p.subplot(gs[4]) #used to be 2
 #p.loglog(pIs, pCs, 'k.')
@@ -72,12 +74,17 @@ p.ylim(pklo, pkhi)
 p.xlabel(r'$P_{\rm in}(k)\ [{\rm mK}^2\ (h^{-1}\ {\rm Mpc})^3]$', fontsize=14)
 p.ylabel(r'$P_{\rm out}(k)\ [{\rm mK}^2\ (h^{-1}\ {\rm Mpc})^3]$', fontsize=14)
 p.grid()
+pkup = max(n.abs(pCvs))
+pkdn = min(n.abs(pCvs))
+p.fill_between([pklo,pkhi],[pkdn,pkdn],[pkup,pkup], facecolor='gray', edgecolor='gray')
+"""
 for kpl,pk,err in zip(kpls,pks,errs):
     #p.([pklo,pkhi], [pk,pk], 'r')
     pkup = max(pk+err,1e-6)
     pkdn = max(pk-err,1e-6)
     print pkdn,pkup
     p.fill_between([pklo,pkhi], [pkdn,pkdn], [pkup,pkup], facecolor='gray', edgecolor='gray')
+"""
 
 #Plot 3    
 ax3 = p.subplot(gs[5]) #used to be 3
@@ -90,18 +97,21 @@ print "pC: ", pCs
 ax3.set_xscale('log')
 ax3.set_yscale('log')
 p.ylim(pklo, pkhi)
-p.xlim(1e-2,1e4)
+p.xlim(1e-1,1e5)
 #p.xlim(1,10)
 p.grid()
 p.xlabel(r'$P_{\rm in}/P_{\rm out}-1$', fontsize=14)
-
-sig_factors=[]
+p.fill_between([1e-3,1e8], [pkdn,pkdn], [pkup,pkup], facecolor='gray', edgecolor='gray')
+sig_factors = []
+sig_factors.append(sig_factor_interp(pkup))
+"""
 for kpl,pk,err in zip(kpls,pks,errs):
     pkup = max(pk+err,1e-6)
     pkdn = max(pk-err,1e-6)
     p.fill_between([1e-3,1e8], [pkdn,pkdn], [pkup,pkup], facecolor='gray', edgecolor='gray')
     if pkup > 1e-6 and kpl > .17:
         sig_factors.append( sig_factor_interp(pkup))
+"""
 
 #Plot 1
 ax0 = p.subplot(gs[1]) #used to be 0
@@ -119,11 +129,12 @@ p.savefig('sigloss.png',format='png')
 
 #P(k) plot on left side
 ax4 = p.subplot(gs[3])
-p.setp(ax4.get_yticklabels())#, visible=False)
+p.setp(ax4.get_yticklabels(), visible=True)
 ax4.set_xscale('log')
 ax4.set_yscale('log')
 p.ylim(pklo, pkhi)
-p.errorbar(kpls, n.abs(pks), yerr=errs, fmt='k.', capsize=0)
+p.plot(kpls,n.abs(pCvs),'k.')
+#p.errorbar(kpls, n.abs(pCvs), yerr=2*pCvs_err, fmt='k.', capsize=0)
 p.grid()
 
 
