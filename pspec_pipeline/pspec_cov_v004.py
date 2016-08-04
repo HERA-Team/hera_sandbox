dbdcd59f66ff4e5450d75959942d3c40d5121abe
Carina Cheng <cacheng@folio.sas.upenn.edu>
2016-08-04 15:10:34 -0400

    populates 'even' and 'odd' datasets with all files listed in argument if there is no 'even' or 'odd' in the path. This way the code can run on single days and doesn't necessarily have to be LST-binned

--- pspec_pipeline/pspec_cov_v004.py
+++ pspec_pipeline/pspec_cov_v004.py
@@ -68,10 +68,12 @@ def get_Q(mode, n_k): #encodes the fourier transform from freq to delay
         return Q
 
 #Read even&odd data
-dsets = {
+if 'even' in args[0] or 'odd' in args[0]:
+    dsets = {
     'even': [x for x in args if 'even' in x],
     'odd' : [x for x in args if 'odd' in x]
-}
+    }
+else: dsets = {'even': args, 'odd': args}
 print dsets
 
 #Get uv file info
