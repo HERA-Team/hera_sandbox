49fe04cc25e908e369c0c6513535dccee418e6ee
Matthew Kolopanis <matthew.kolopanis@gmail.com>
2016-08-17 16:52:48 -0700

    sigloss limits now computes limits for various confidence levels and saves pspec_pk_k3pk.npz files for pC and pI

--- pspec_pipeline/sigloss_limits.py
+++ pspec_pipeline/sigloss_limits.py
@@ -28,13 +28,41 @@ def injpath_to_injlevel(filepath):
     injdir = os.path.split(os.path.dirname(filepath))[-1]
     return float(injdir.split('_')[-1])
 
+def fit_prob_to_tanh(probs, pIs):
+        #tanh fit is VERY sensitive to initial conditions
+        p0 = np.zeros(4)
+        mean_ind = np.argmin( abs( probs - np.mean(probs) )  )
+        p0[0] = pIs[mean_ind] # set mean and width to entral value of pspecs
+        p0[1] = pIs[mean_ind]# set mean and width to central value of pspecs
+        p0[2] = .25 + np.min(probs) #offest of guess is min value + .25
+        p0[3] = (np.max(probs) - np.min(probs ) )*3/4.#scale is  3/4 differnce in probabilities
+
+        parms, cov = curve_fit(tanh,pIs,probs,p0=p0,maxfev=50000)
+        return parms
+
+def get_pk_k3pk(prob,ks, parms):
+        nk = len(ks)
+        upper_limits=[]
+        upper_limits.append([ atanh(prob,*ps) for ps in parms])
+        upper_limits = np.array(upper_limits).squeeze()
+        k3pk = np.tile(1e-5,nk)
+        k3err = upper_limits * k**3/(2*np.pi**2)
+        Pk = np.tile(1e-5, nk)
+        pkerr = upper_limits
+        return Pk, pkerr, k3pk, k3err
+
 def tanh(x, m, s, c = 1.0, a=1.0): return c  + a*np.tanh( (x-m)/(2*s))
-def atanh(p,m,s,c,a): return 2*s*np.arctanh( (p - c )/a) + m
+def atanh(p,m,s,c,a): return 2*s*np.arctanh( (p - c )/a ) + m
 
 #read in the bootstrapped pspecs
 pspec_channels = ['pCv_fold','pCv_fold_err', #weighted data pspec
+                    'pIv_fold','pIv_fold_err', #unweighted data pspec
                     'pC_fold','pC_fold_err', #weighted data+inj pspec
-                    'pI_fold','pI_fold_err'] #unweighted inj pspec
+                    'pI_fold','pI_fold_err', #unweighted inj pspec
+                    'pCv','pCv_err', #weighted data pspec
+                    'pIv','pIv_err', #unweighted data pspec
+                    'pC','pC_err', #weighted data+inj pspec
+                    'pI','pI_err'] #unweighted inj pspec pos and neg kpls
 pspecs = {}
 #sort the input files. makes things easier later
 injlevels = [injpath_to_injlevel(filename) for filename in args.pspec_files]
@@ -53,52 +81,45 @@ for pspec_channel in pspec_channels:
     pspecs[pspec_channel] = np.array(pspecs[pspec_channel])
 Ninj,Nk = pspecs[pspec_channel].shape
 k = F['k']
-
+kpls= F['kpl_fold']
+Nk = len(k)
+freq = F['freq']
 print "found injects:",Ninj
 print "found k bins:",Nk
 
+for ptype in ['pI', 'pC']:
+        #limit option #1. the net probability that pC is above pcV
+    probs = np.zeros((Ninj,Nk))
+    tanh_parms = []
+    for inj in xrange(Ninj):
+        for k_ind in xrange(Nk):
+            lossy_limit = pspecs[ptype+'v_fold'][0,k_ind]+pspecs[ptype+'v_fold_err'][0,k_ind]
+            if k_ind==5:
+                print "lossy_limit: ", lossy_limit
+                print ptype+" lower limit",pspecs[ptype+'_fold'][inj,k_ind]-pspecs[ptype+'_fold_err'][inj,k_ind]
+            probs[inj,k_ind] = G_mc(lossy_limit, #limit
+                            pspecs[ptype+'_fold'][inj,k_ind],
+                            pspecs[ptype+'_fold_err'][inj,k_ind])
 
-#limit option #1. the net probability that pC is above pcV
-probs = np.zeros((Ninj,Nk))
-for inj in xrange(Ninj):
+        tanh_parms.append( fit_prob_to_tanh(probs[:,k_ind],
+            pspecs['pI_fold'][:,k_ind]))
+
+
+    figure()
     for k_ind in xrange(Nk):
-        lossy_limit = pspecs['pCv_fold'][0,k_ind]+pspecs['pCv_fold_err'][0,k_ind]
-        if k_ind==5:
-            print "lossy_limit: ", lossy_limit
-            print "pC lower limit",pspecs['pC_fold'][inj,k_ind]-pspecs['pC_fold_err'][inj,k_ind]
-        probs[inj,k_ind] = G_mc(lossy_limit, #limit
-                        pspecs['pC_fold'][inj,k_ind],
-                        pspecs['pC_fold_err'][inj,k_ind])
+        plt.semilogx(pspecs['pI_fold'][:,k_ind],probs[:,k_ind],'-',label=k[k_ind])
+        # plt.semilogx(pspecs['pI_fold'][:,k_ind], tanh(pspecs['pI_fold'][:,k_ind], *tanh_parms[k_ind]),'-' )
+    grid()
+    legend(loc='best')
+    xlabel('$P_{inj}$')
+    ylabel('Probability to find $P_{inj}$')
+    title(ptype)
+    show()
+    prob_limits = [.68,.85,.9,.95,.99]
 
-prob_limits = [.68,.85,.9,.95,.99]
-tanh_parms = []
-upper_limits = []
-for k_ind in xrange(Nk):
-    #tanh fit is VERY sensitive to initial conditions
-    p0 = np.zeros(4)
-    mean_ind = np.argmin( abs( probs[:,k_ind] - np.mean(probs[:,k_ind]) )  )
-    p0[0] = pspecs['pI_fold'][mean_ind,k_ind] # set mean and width to central value of pspecs
-    p0[1] = pspecs['pI_fold'][mean_ind,k_ind]# set mean and width to central value of pspecs
-    p0[2] = .25+ np.min(probs[:,k_ind]) #offest of guess is min value
-    p0[3] = (np.max(probs[:,k_ind]) - np.min(probs[:,k_ind]) )*3/4.#scale is differnce in probabilities
+    for per in prob_limits:
+        pk,pkerr,k3pk,k3err = get_pk_k3pk(per,k,tanh_parms)
 
-    parms, cov = curve_fit(tanh, pspecs['pI_fold'][:,k_ind],probs[:,k_ind],p0=p0,maxfev=10000)
-    tanh_parms.append(parms)
-    upper_limits.append(atanh(prob_limits,*parms))
-
-    if k_ind ==5:
-        print "Upper limts at k={0:.4f} hMpc^-1".format(k[k_ind])
-        print "Confidence interval", "Upper Limit"
-        for _pl, _ul in zip(prob_limits,upper_limits[k_ind]):
-            print " {0} \t {1:.4e}".format(_pl,_ul)
-
-# colormap = plt.cm.gist_ncar
-# plt.gca().set_color_cycle([colormap(i) for i in np.linspace(0, 0.9, Nk+1)])
-figure()
-for k_ind in xrange(Nk):
-    plt.semilogx(pspecs['pI_fold'][:,k_ind],probs[:,k_ind],'x',label=k[k_ind])
-    plt.semilogx(pspecs['pI_fold'][:,k_ind], tanh(pspecs['pI_fold'][:,k_ind], *tanh_parms[k_ind]),'-' )
-grid()
-legend(loc='best')
-
-show()
+        np.savez('pspec_limits_k3pk_'+ptype+'_{0:02d}'.format(int(per*100))+'.npz',
+            freq=freq, k=k, k3pk=k3pk, k3err=k3err,
+            pk=pk, err=pkerr, kpl = kpls)
