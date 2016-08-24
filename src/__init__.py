71049d7175477a2aa0de2735608ded454cb93ac2
Yunfan Gerry Zhang <yunfanzh@folio.sas.upenn.edu>
2016-08-24 15:07:27 -0400

    resolve

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
 import arp, jcp, dfm, dcj, zsa, ctc
-import gb_weather, pfb, pspec, dspec, red, fringe, frf_conv, miriad
+import gb_weather, pfb, pspec, dspec, red, fringe, frf_conv, miriad,omni
 import arp, jcp, dfm, dcj, zsa, ctc#, wyl
