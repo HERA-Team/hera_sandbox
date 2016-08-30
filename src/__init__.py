c39fd6ba49d3afb3e8e2c925abb9abef04a14bfb
Yunfan G. Zhang <yunfanz@berkeley.edu>
2016-08-30 16:51:45 -0700

    capo without wyl

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 import pfb, pspec, dspec, red, fringe, frf_conv, miriad
 import oqe
 import warnings
-try: import omni, uCal, wyl
+try: import omni, uCal#, wyl
 except(ImportError,NameError):
     warnings.warn("Warning: omnical not installed, not importing capo.omni")
 import arp, jcp, dfm, dcj, zsa, ctc#, wyl
