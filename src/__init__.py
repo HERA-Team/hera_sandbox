ec34afb3ad4ee55197e16e1407b78877a093f88f
Yunfan Gerry Zhang <yunfanz@berkeley.edu>
2016-08-31 13:38:16 -0400

    resolve conflict

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,8 @@
 import pfb, pspec, dspec, red, fringe, frf_conv, miriad
 import oqe
 import warnings
-try: import omni, uCal, wyl
-except(ImportError,NameError):
-    warnings.warn("Warning: omnical not installed, not importing capo.omni")
+#try: import omni, uCal, wyl
+#except(ImportError,NameError):
+#    warnings.warn("Warning: omnical not installed, not importing capo.omni")
+import omni, uCal
 import arp, jcp, dfm, dcj, zsa, ctc#, wyl
