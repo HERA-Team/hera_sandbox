562bc6bfe24d444179b81a88d041d3f876b53138
Zaki Ali <zakiali88@gmail.com>
2016-09-08 17:24:14 -0400

    fix keys for delays in rewrite_fc_npz.py
    also, fix writeout dictionary.

--- zsa/scripts/rewrite_fc_npz.py
+++ zsa/scripts/rewrite_fc_npz.py
@@ -16,8 +16,9 @@ for f in args:
     elif opts.pol in pols: pol = opts.pol
     else:
         raise RuntimeError('No polarization string provided.')
+    data = {}
     for k in d.keys(): 
-        data = {}
-        if k.isdigit(): data[k+'xx'] = d[k]
+        if k.isdigit(): data[k+'x'] = d[k]
+        elif k.endswith('d'): data['d'+k[:-1]] = d[k]
         else: data[k] = d[k]
     np.savez(f, **data)
