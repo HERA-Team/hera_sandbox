0b0439a9f296e71f5c5b5fc1341776df956c6b0e
Zaki Ali <zakiali88@gmail.com>
2016-09-08 12:32:06 -0700

    merged in rewrite_fc_npz.py from fc_time branch

---
+++ zsa/scripts/rewrite_fc_npz.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+'''Rewrite firstcal npz files to be consistent with omnical.'''
+import numpy as np
+import sys,optparse
+
+o = optparse.OptionParser()
+o.parse_args('--pol', '-p',  action='store', 
+    'Polarization string')
+opts,args = o.parse_args(sys.argv[1:])
+
+pols = ['xx', 'yy', 'xy', 'yx']
+
+for f in args:
+    d = np.load(f)
+    pol_test = f.split('.')[3]
+    if pol_test in pols: pol = pol_test #test to see if polarization is 
+    elif opts.pol in pols: pol = opts.pol
+    else:
+        raise RuntimeError('No polarization string provided.')
+    for k in d.keys: 
+        data = {}
+        if k.isdigit(): data[k+'xx'] = d[k]
+        else: data[k] = d[k]
+    np.savez(f, **data)
