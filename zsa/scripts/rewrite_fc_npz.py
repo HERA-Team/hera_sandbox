1d8f3bd0c3a711ffe230c0ec19fca0274e319455
Zaki Ali <zakiali88@gmail.com>
2016-09-08 13:14:01 -0700

    more syntax bugs

--- zsa/scripts/rewrite_fc_npz.py
+++ zsa/scripts/rewrite_fc_npz.py
@@ -4,7 +4,7 @@ import numpy as np
 import sys,optparse
 
 o = optparse.OptionParser()
-o.parse_args('--pol', '-p',  action='store', help='Polarization string')
+o.add_option('--pol', '-p',  action='store', help='Polarization string')
 opts,args = o.parse_args(sys.argv[1:])
 
 pols = ['xx', 'yy', 'xy', 'yx']
