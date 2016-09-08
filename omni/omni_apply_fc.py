0b495aeeb05062abe997836690f1cbc0dba662ce
Zaki Ali <zakiali88@gmail.com>
2016-09-08 13:05:20 -0700

    updated omni_apply_fc.py to be nice with omnical file format.
    note that you will need to run rewrite_fc_npz.py

--- omni/omni_apply_fc.py
+++ omni/omni_apply_fc.py
@@ -13,7 +13,7 @@ o.add_option('--xtalk',dest='xtalk',default=False,action='store_true',
 o.add_option('--omnipath',dest='omnipath',default='%s.npz',type='string',
             help='Format string (e.g. "path/%s.npz", where you actually type the "%s") which converts the input file name to the omnical npz path/file.')
 o.add_option('--firstcal', dest='firstcal', type='string',
-            help='Firstcal calfile.')
+            help='Path to firstcal files.')
 opts,args = o.parse_args(sys.argv[1:])
 
 
@@ -27,47 +27,44 @@ for filename in args:
         fn[3] = p
         files[filename][p] = '.'.join(fn)
 
-firstfile = numpy.load(opts.firstcal)
-gains = {'x':{}}
-for ant in firstfile.keys():
-    if ant.isdigit():
-        gains['x'][int(ant)] = firstfile[ant]
+fcpath = opts.firstcal
 
 ### Read Data and Solutions ###
 for f,filename in enumerate(args):
-        newfile = files[filename][p].split('/')[-1]+'F' #saves in cwd
-#        omnifile = opts.omnipath % '.'.join(filename.split('/')[-1].split('.')[0:3])
-        if os.path.exists(newfile):
-            print '    %s exists.  Skipping...' % newfile
-            continue
-        times = []
+        if len(pols)>1: npzb=3
+        else: npzb=4
+        omnifile = opts.omnipath % '.'join(filename.split('/')[-1].split('.')[0:npzb])
+        print ' firstcal npz:', omnifile
+        exit()
+        _,gains,_,_ = capo.omni.from_npz(omnifile) #loads the firstcal file gains
+        for p in pols:
+            print 'Reading', files[filename][p]
+            newfile = files[filename][p].split('/')[-1]+'F' #saves in cwd
+            if os.path.exists(newfile):
+                print '    %s exists.  Skipping...' % newfile
+                continue
+            times = []
     
-        def mfunc(uv,p,d,f): #loops over time and baseline
-            global times #global list
-            _,t,(a1,a2) = p
-            p1,p2 = pol = aipy.miriad.pol2str[uv['pol']]
-            if len(times) == 0 or times[-1] != t: times.append(t) #fill times list
-            if opts.xtalk: #subtract xtalk
-                try: d -= xtalk[pol][(a1,a2)]
-                except(KeyError):
-                    try: d -= xtalk[pol][(a2,a1)].conj()
-                    except(KeyError): pass
-            ti = len(times) - 1 #time index
-            try: d*=numpy.conj(gains[p1][a1])
-            except(KeyError): pass
-            try: d*=numpy.conj(gains[p1][a2].conj())
-            except(KeyError): pass
-            #try: d /= gains[p1][a1][ti] #apply gains
-            #except(KeyError): pass
-            #try: d /= gains[p2][a2][ti].conj() 
-            #except(KeyError): pass
-            return p, numpy.where(f,0,d), f
+            def mfunc(uv,p,d,f): #loops over time and baseline
+                global times #global list
+                _,t,(a1,a2) = p
+                p1,p2 = pol = aipy.miriad.pol2str[uv['pol']]
+                if len(times) == 0 or times[-1] != t: times.append(t) #fill times list
+                ti = len(times) - 1 #time index
+                try: d*=numpy.conj(gains[p1][a1])
+                except(KeyError): pass
+                try: d*=numpy.conj(gains[p1][a2].conj())
+                except(KeyError): pass
+                #try: d /= gains[p1][a1][ti] #apply gains
+                #except(KeyError): pass
+                #try: d /= gains[p2][a2][ti].conj() 
+                #except(KeyError): pass
+                return p, numpy.where(f,0,d), f
     
-        if opts.xtalk: print '    Calibrating and subtracting xtalk'
-        else: print '    Calibrating'
-        uvi = aipy.miriad.UV(files[filename][p])
-        uvo = aipy.miriad.UV(newfile,status='new')
-        uvo.init_from_uv(uvi)
-        print '    Saving', newfile
-        uvo.pipe(uvi, mfunc=mfunc, raw=True, append2hist='OMNICAL: ' + ' '.join(sys.argv) + '\n')
-        
+            if opts.xtalk: print '    Calibrating and subtracting xtalk'
+            else: print '    Calibrating'
+            uvi = aipy.miriad.UV(files[filename][p])
+            uvo = aipy.miriad.UV(newfile,status='new')
+            uvo.init_from_uv(uvi)
+            print '    Saving', newfile
+            uvo.pipe(uvi, mfunc=mfunc, raw=True, append2hist='FIRSTCAL: ' + ' '.join(sys.argv) + '\n')
