c2f1591b03df823e8b78b062f52a237885517bb7
Zaki Ali <zakiali88@gmail.com>
2016-10-13 15:15:21 -0700

    added in function to run a notebook, inline and save output.

--- src/zsa.py
+++ src/zsa.py
@@ -189,3 +189,24 @@ def order_data(dd, info):
                 if not d.has_key(bl[::-1]): d[bl[::-1]] = {}
                 d[bl[::-1]][pol] = n.conj(dd[bl][pol])
     return d
+
+def run_nb(workdir, fileroot, notebook):
+
+    from shutil import copy
+    from rtpipe import get_notebook
+    from subprocess import call
+
+    os.environ['fileroot'] = fileroot
+    if agdir: #git directory
+        os.environ['agdir'] = agdir
+
+    os.chdir(workdir)
+    copy(nb, '{0}/{1}.ipynb'.format(workdir, fileroot))
+
+    cmd = 'jupyter nbconvert {0}.ipynb --inplace --execute --to notebook --allow-errors --ExecutePreprocessor.timeout=3600'.format(fileroot).split(' ')
+    status = call(cmd)
+
+    cmd = 'jupyter trust {0}.ipynb'.format(fileroot).split(' ')
+    status = call(cmd)
+
+    return True
