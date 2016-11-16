b90d9dd5ffa0be42423b05313b4037727f3fc143
Yunfan Zhang <yunfanz@airbears2-10-142-187-183.airbears2.1918.berkeley.edu>
2016-11-15 17:09:55 -0800

    sync

--- ygz/simu/baseline_ton_para.py
+++ ygz/simu/baseline_ton_para.py
@@ -3,13 +3,14 @@ import aipy as a, numpy as n, capo as C, pylab as p
 from joblib import Parallel, delayed
 import sys
 import multiprocessing
+from baseline_toff_para import find_corr, prepare
 num_cores = multiprocessing.cpu_count()
 
 #@p.ion()
 #fqs = n.linspace(.1,.2,203)
 fq = .15
 bl1, bl2 = (0,103),(0,103)
-N = 10000   #number of universes to average over
+N = 24000   #number of universes to average over
 
 VIS = False
 REDNORM = 1.
@@ -35,65 +36,13 @@ plt = None
 TT = n.arange(2455700.499,2455700.501,0.0001)
 NORM = float(TT.size)/1000.
 #for i in xrange(N):
-def find_corr(i):
-    print i
-    sky = n.random.normal(size=h.map.size)
-    h.map = sky # assume sky is in eq coord
-    #import IPython; IPython.embed()
-    vis1,vis2 = [],[]
-    aa.set_jultime(2455700.5)
-    bl1x,bl1y,bl1z = aa.get_baseline(bl1[0],bl1[1],'z')
-    bl2x,bl2y,bl2z = aa.get_baseline(bl2[0],bl2[1],'z')
-    for jd in TT:
-        # convert tx,ty,tz to ex,ey,ez (time dependent)
-        aa.set_jultime(jd)
-        m = aa.eq2top_m
-        tx,ty,tz = n.dot(m, eq)
-        bl1_prj = tx*bl1x + ty*bl1y + tz*bl1z
-        bl2_prj = tx*bl2x + ty*bl2y + tz*bl2z
-        fng1 = n.exp(-2j*n.pi*bl1_prj*fq)
-        fng2 = n.exp(-2j*n.pi*bl2_prj*fq)
-        #fng1=1
-        #fng2=1
-        #bm = n.ones_like(tx)
-        bm = aa[0].bm_response((tx,ty,tz),pol='I')[0]**2#/n.abs(tz)  #baseline beam is antenna beam squared.
-        bm = n.where(tz > 0.001, bm, 0)   
-        # bm /= bm.sum()   #avoiding the monopole
-        bm_fng1 = bm * fng1
-        bm_fng2 = bm * fng2
-        #m = n.linalg.inv(aa._eq2now)
-        #ex,ey,ez = n.dot(m, top)        
-        sky_prj = h.map
-        #sky_prj = n.ones_like(h.map)
-        ###################
-        if VIS:
-            ex2,ey2,ez2 = n.dot(m, top2)
-            sky_prj2 = h[ex2,ey2,ez2]
-            sky_prj2.shape = SH
-            if plt is None: plt = p.imshow(sky_prj2, vmax=2, vmin=-2,origin='lower')
-            else:
-               plt.set_data(sky_prj2)
-               p.draw()
-        ####################
-        vis1.append(n.sum(sky_prj * bm_fng1))
-        vis2.append(n.sum(sky_prj * bm_fng2))
-        #vis1.append(n.sum(sky_prj))
-        #vis2.append(n.sum(sky_prj))
-        #note top is a dome already, so no need for the jacobian factor
-    #just built data for the tw
-    # da2
-    vis1,vis2 = n.array(vis1), n.array(vis2)
 
-    #vis1 = n.hamming(vis1.size)*vis1
-    #vis2 = n.hamming(vis1.size)*vis2
 
-    _vis1,_vis2 = n.fft.fft(vis1), n.fft.fft(vis2)
-    #print _vis1.shape
-    #each universe has one corr
-    tempcorr = n.fft.ifftshift(n.fft.ifft(_vis2*n.conj(_vis1)))
-    #p.plot(n.abs(corr[i]))
-    return tempcorr
-corr = Parallel(n_jobs=16)(delayed(find_corr)(i) for i in xrange(N))
+print 'preparing bfs'
+bfs = prepare(TT)
+print 'done'
+
+corr = Parallel(n_jobs=2)(delayed(find_corr)(i, bfs) for i in xrange(N))
 corr = n.array(corr)
 print 'shape of corr:',corr.shape
 #import IPython; IPython.embed()
