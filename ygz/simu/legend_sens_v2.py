e724e4abce3da76357cfca04a03a01e510bc1271
Yunfan Zhang <yunfanz@airbears2-10-142-185-160.airbears2.1918.berkeley.edu>
2017-03-17 00:05:21 -0700

    pulling

--- ygz/simu/legend_sens_v2.py
+++ ygz/simu/legend_sens_v2.py
@@ -6,7 +6,7 @@ import pandas as pd
 from itertools import cycle
 from matplotlib import colors as mcolors
 import seaborn as sns 
-sns.set_context("paper", font_scale=2)
+sns.set_context("paper", font_scale=2.5)
 
 FILE = 'corr_res.csv'
 #FILE = 'HERA_350_core_pm300.csv'
@@ -38,12 +38,12 @@ for i in xrange(Npts):
 	ax.scatter(np.abs(df['dT'][i]), df['peak'][i]/peakmax, label=label, s=65, marker=marker, c=Cz[i])
 plt.setp(ax.get_xticklabels(), visible=False)
 if LEGEND:
-	legend = ax.legend(ncol=4, scatterpoints=1, frameon=False)
+	legend = ax.legend(ncol=4, scatterpoints=1, frameon=False,fontsize = 'x-small')
 	legend.get_frame().set_facecolor('none')
 
 #ax.grid()
 #ax.set_xlabel('Time Delay [Sidereal Day]')
-ax.set_ylabel('Correlation [Normalized to 1]')
+ax.set_ylabel('Correlation')
 ax.set_xlim([-0.01, 0.21])
 
 mult1010 = float(np.amax(df['mult']))
@@ -54,12 +54,30 @@ for i in xrange(Npts):
 	ax2.scatter(np.abs(df['dT'][i]), (df['mult']*df['peak']/mult1010)[i]/peakmax, s=65, c=Cz[i], label=label, marker=marker)
 #plt.legend()
 if LEGEND:
-	legend = ax2.legend(ncol=4, scatterpoints=1, frameon=False)
+	legend = ax2.legend(ncol=4, scatterpoints=1, frameon=False, fontsize = 'x-small')
 	legend.get_frame().set_facecolor('none')
 
 #ax2.grid()
 ax2.set_xlabel('Time Delay [Sidereal Day]')
-ax2.set_ylabel('Weighted Correlation [Normalized to 1]')
+ax2.set_ylabel('Weighted Correlation')
 #plt.tight_layout()
 f.subplots_adjust(hspace=0.05)
 plt.show()
+
+
+print "========= Statistics of sensitibity contribution =========="
+df['Theta'] = df['peak']*df['mult']
+df['Theta'] /= np.amax(df['Theta'])
+def get_imp(df, Theta_min=0.4):
+	dft = df.where(df['Theta']>Theta_min)
+	dfeq = dft.where(dft['sep']==dft['sep2'])
+	dfnq = dft.where(dft['sep']!=dft['sep2'])
+	
+	totalsumsq = np.sum(dft['Theta']**2)
+	eqsumsq = np.sum(dfeq['Theta']**2)
+	totalsens = np.sum(dft['Theta'])/totalsumsq
+	eqsens = np.sum(dfeq['Theta'])/eqsumsq
+	improve = (totalsens-eqsens)/eqsens
+	return improve
+
+
