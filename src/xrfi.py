c90517cd82225295ca818c4470b8a4bee4af36ed
Josh Dillon <jsdiillon@mit.edu>
2017-04-26 17:34:16 -0700

    PAPER firstcal/omnical investigation
    This was investigating the issue with firstcal being run only on EW
    baselines (causing a degeneracy issue in omnical) and also a single
    crazy baseline (29,81)

--- src/xrfi.py
+++ src/xrfi.py
@@ -1,12 +1,8 @@
 '''Module for all things Radio Frequency Interference Flagging'''
 import numpy as np
 
-def medmin(chisq):
-    #return np.median(np.min(chisq,axis=0))
-    mn = np.min(chisq,axis=0)
-    return 2*np.median(mn) - np.min(mn)
 
-def omni_chisq_to_flags(chisq, K=8, sigma=6, sigl=2):
+def omni_chisq_to_flags(chisq, K=8, sigma=6, sigl=2, smooth=True):
     '''Returns a mask of RFI given omnical's chisq statistic'''
     w_sm = np.empty_like(chisq)
     sig = np.empty_like(chisq)
@@ -15,8 +11,7 @@ def omni_chisq_to_flags(chisq, K=8, sigma=6, sigl=2):
         for j in xrange(chisq.shape[1]):
             i0,j0 = max(0,i-K), max(0,j-K)
             i1,j1 = min(chisq.shape[0], i+K), min(chisq.shape[1], j+K)
-            #w_sm[i,j] = np.median(chisq[i0:i1,j0:j1])
-            w_sm[i,j] = medmin(chisq[i0:i1,j0:j1])
+            w_sm[i,j] = np.median(chisq[i0:i1,j0:j1])
     #the residual from smooth component
     w_rs = chisq - w_sm 
     w_sq = np.abs(w_rs)**2
@@ -25,8 +20,7 @@ def omni_chisq_to_flags(chisq, K=8, sigma=6, sigl=2):
         for j in xrange(chisq.shape[1]):
             i0,j0 = max(0,i-K), max(0,j-K)
             i1,j1 = min(chisq.shape[0], i+K), min(chisq.shape[1], j+K)
-            #sig[i,j] = np.sqrt(np.median(w_sq[i0:i1,j0:j1]))
-            sig[i,j] = np.sqrt(medmin(w_sq[i0:i1,j0:j1]))
+            sig[i,j] = np.sqrt(np.median(w_sq[i0:i1,j0:j1]))
     #Number of sigma above the residual unsmooth part is.
     f1 = w_rs / sig
     #mask off any points above 'sig' sigma and nan's.
