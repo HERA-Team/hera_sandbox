c4696030e81e6f7ed9b55ac844337e8437a1c601
Carina Cheng <cacheng@folio2.sas.upenn.edu>
2017-04-27 16:17:52 -0400

    status of psa128 batch scripts after calibrating season 1, omni_v5_xtalk

--- ctc/scripts/batch_postcalibrate_128.sh
+++ ctc/scripts/batch_postcalibrate_128.sh
@@ -13,7 +13,7 @@ echo ${ARGS}
 
 for f in ${ARGS}; do
     ~/capo/omni/omni_xrfi.py --boxside=16 ${f:65:20}.npz 
-    ~/capo/omni/omni_xrfi_apply.py --omnipath /data4/paper/2014EoR/Analysis/ProcessedData/epoch4/omni_v4_xtalk/%s.chisqflag.npz ${f}
+    ~/capo/omni/omni_xrfi_apply.py --omnipath /data4/paper/2013EoR/Analysis/ProcessedData/epoch1/omni_v5_xtalk/%s.chisqflag.npz ${f}
     xrfi_simple.py -n 1000 -c 101,102,148,149,150,151,152,153,154,169 ${f}R
     ~/capo/ctc/scripts/pspec_prep.py -C ${CALFILE} -a cross --window=blackman-harris --nogain --nophs --clean=1e-9 --horizon=15 ${f}RR
     xrfi_simple.py -n 4 ${f}RRB 
