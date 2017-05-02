Fixed bug in weighting for linsolve, added new test that exposed it.

src/linsolve.py
@@ -90,7 +90,7 @@ class LinearEquation:
         if type(val) is str:
             n = ast.parse(val, mode='eval')
             val = ast_getterms(n)
-        self.wgt = kwargs.pop('wgt',1.)
+        self.wgts = kwargs.pop('wgts',1.)
         self.process_terms(val, **kwargs)
     def process_terms(self, terms, **kwargs):
         '''Classify terms from parsed str as Constant or Parameter.'''
@@ -120,10 +120,10 @@ class LinearEquation:
             for ti in t[:-1]:
                 assert(type(ti) is not str or self.consts.has_key(get_name(ti)))
         return terms
-    def eval_consts(self, const_list, wgt=1.):
+    def eval_consts(self, const_list, wgts=1.):
         '''Multiply out constants (and wgts) for placing in matrix.'''
         const_list = [self.consts[get_name(c)].get_val(c) for c in const_list]
-        return wgt * reduce(lambda x,y: x*y, const_list, 1.)
+        return wgts * reduce(lambda x,y: x*y, const_list, 1.)
     def put_matrix(self, m, eqnum, prm_order, complex=True):
         '''Place this equation in line eqnum of pre-made (# eqs,# prms) matrix m.'''
         xs,ys,vals = self.sparse_form(eqnum, prm_order, complex=complex)
@@ -134,7 +134,7 @@ class LinearEquation:
         xs, ys, vals = [], [], []
         for term in self.terms:
             p = self.prms[get_name(term[-1])]
-            f = self.eval_consts(term[:-1], self.wgt)
+            f = self.eval_consts(term[:-1], self.wgts)
             try: x,y,val = p.sparse_form(term[-1], eqnum, prm_order, f.flatten(), complex)
             except(AttributeError): # happens if f is a scalar
                 x,y,val = p.sparse_form(term[-1], eqnum, prm_order, f, complex)
@@ -147,7 +147,6 @@ class LinearSolver:
         self.data = data
         for k in wgts: assert(np.iscomplexobj(wgts[k]) == False) # tricky errors happen if wgts are complex
         self.wgts = wgts
-        for k in wgts: assert(np.iscomplexobj(wgts[k]) == False) # tricky errors happen if wgts are complex
         self.keys = data.keys()
         self.eqs = [LinearEquation(k,wgts=self.wgts.get(k,1.), **kwargs) for k in self.keys]
         # XXX add ability to have more than one measurment for a key=equation