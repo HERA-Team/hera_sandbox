a97fdddaa9149821ccaeb78cda6dfe7db2e8965f
Adam Beardsley <adam.p.beardsley@gmail.com>
2017-09-03 09:30:35 +0200

    script for diagnosising via autos

---
+++ apb/scripts/auto_diagnosis.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+import os
+import argparse
+import pyuvdata
+import numpy as np
+import pyuvdata.utils as uvutils
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser(description='Plot auto locations and magnitudes')
+parser.add_argument('file', metavar='file', type=str, nargs='+',
+                    help='File to be processed.')
+
+args = parser.parse_args()
+
+uv = pyuvdata.UVData()
+uv.read_miriad(args.file)
+antpos = uv.antenna_positions + uv.telescope_location
+antpos = uvutils.ENU_from_ECEF(antpos.T, *uv.telescope_location_lat_lon_alt).T
+
+amps = np.zeros(uv.Nants_telescope)
+for ant in range(uv.Nants_telescope):
+    print(ant)
+    d = uv.get_data((uv.antenna_numbers[ant], uv.antenna_numbers[ant]))
+    amps[ant] = np.mean(np.abs(d))
+
+plt.scatter(antpos[:, 0], antpos[:, 1], c=amps)
+plt.show()
