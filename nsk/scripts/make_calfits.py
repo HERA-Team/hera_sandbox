d0280edc06bebb537ebdfa2d28294af9c697a62d
Nicholas Kern <nkern@berkeley.edu>
2017-10-13 14:11:41 -0700

    added make_calfits to nsk/

---
+++ nsk/scripts/make_calfits.py
@@ -0,0 +1,69 @@
+from pyuvdata import UVCal
+import numpy as np
+
+def make_calfits(fname, data_array, freq_array, time_array, jones_array,  ants,
+                 channel_width=0.0, gain_convention='divide', history='', telescope_name='HERA',
+                 x_orientation='east', integration_time=10.0):
+    """
+    make a calfits file from data_array etc. objects
+   
+    fname : str, filename
+
+    data_array : ndarray, shape=(Nants, Nfreqs, Ntimes, Npols)
+
+    freq_array : ndarray, shape=Nfreqs
+
+    time_array : ndarray, shape=Ntimes
+
+    jones_array : ndarray, shape=Npols
+
+    ants : ndarray, shape=Nants
+    """
+    # specify ant params
+    Nants_data = len(ants)
+    Nants_telescope = len(ants)
+    ant_array = np.array(ants, np.int)
+    antenna_names = np.array(ants, np.str)
+    antenna_numbers = np.array(ants, np.int)
+
+    # frequency params
+    Nfreqs = len(freq_array)
+    freq_range = np.array([freq_array[0], freq_array[-1]])
+    freq_array = freq_array.reshape(1, -1) 
+
+    # pol params
+    Njones = len(jones_array)
+
+    # time params
+    Ntimes = len(time_array)
+    time_range = np.array([time_array[0], time_array[-1]])
+
+    # spw params
+    Nspws = 1
+    spw_array = np.array([0])
+    data_array = data_array.reshape(Nants_data, 1, Nfreqs, Ntimes, Njones)
+
+    # data params
+    if data_array.shape[2] > 1:
+        gain_array = data_array
+        delay_array = None
+        cal_type = 'gain'
+    else:
+        gain_array = None
+        delay_array = data_array
+        cal_type = 'delay'
+
+    flag_array = np.array(np.zeros_like(data_array), np.bool)
+    quality_array = np.zeros_like(data_array) 
+
+    # make blank uvc
+    uvc = UVCal()
+    params = ['Nants_data', 'Nants_telescope', 'ant_array', 'antenna_names', 'antenna_numbers', 'Nfreqs',
+              'freq_array', 'Njones', 'Ntimes', 'time_range', 'Nspws', 'spw_array', 'data_array', 'gain_array',
+              'delay_array', 'jones_array', 'time_array', 'freq_array', 'cal_type', 'flag_array', 'quality_array', 'channel_width', 'gain_convention',
+              'history', 'telescope_name', 'x_orientation', 'integration_time', 'freq_range']
+    for p in params:
+        uvc.__setattr__(p, locals()[p])
+
+    uvc.write_calfits(fname)
+
