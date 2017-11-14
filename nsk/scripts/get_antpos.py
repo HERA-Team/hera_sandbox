6e8452be006c408ecf783e54f64c45f504928d29
Nicholas Kern <nkern@berkeley.edu>
2017-11-14 12:03:21 -0800

    modified:   get_antpos.py

--- nsk/scripts/get_antpos.py
+++ nsk/scripts/get_antpos.py
@@ -10,13 +10,16 @@ def get_antpos(uvd, center=True, pick_data_ants=False):
     """
     antpos = uvutils.ENU_from_ECEF((uvd.antenna_positions + uvd.telescope_location).T, *uvd.telescope_location_lat_lon_alt).T
     ants = uvd.antenna_numbers
-    if center is True:
-        antpos -= np.median(antpos, axis=0)
+
     if pick_data_ants:
         data_ants = np.unique(np.concatenate([uvd.ant_1_array, uvd.ant_2_array]))
         telescope_ants = uvd.antenna_numbers
         select = map(lambda x: x in data_ants, telescope_ants)
         antpos = antpos[select, :]
         ants = data_ants
+
+    if center is True:
+        antpos -= np.median(antpos, axis=0)
+
     return antpos, ants
 
