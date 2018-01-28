f14a11611ef995529794b8dba68b8caa671f9300
Nicholas Kern <nkern@berkeley.edu>
2018-01-27 18:11:31 -0800

    modified:   uv_select.py

--- nsk/scripts/uv_select.py
+++ nsk/scripts/uv_select.py
@@ -2,8 +2,8 @@
 """
 read in miriad file, select on data and write out
 """
-import pyuvdata
-import numpy
+from pyuvdata import UVData
+import numpy as np
 import os
 import glob
 import argparse
@@ -11,14 +11,14 @@ import hera_cal as hc
 from collections import OrderedDict as odict
 
 args = argparse.ArgumentParser(description="read-in miriad file, select subset of data and write out")
-args.add_argument("files", nargs='*', type=str, help="files to select data on")
+args.add_argument("--files", nargs='+', type=str, help="files to select data on")
 args.add_argument("--ext", default="S", type=str, help="file extension for output miriad file")
 args.add_argument("--overwrite", default=False, action='store_true', help='overwrite output data files')
 args.add_argument("--outdir", default=None, type=str, help="output directory")
-args.add_argument("--ants", default=None, nargs='*', type=int, help="antenna numbers to keep")
+args.add_argument("--ants", default=None, nargs='+', type=int, help="antenna numbers to keep")
 args.add_argument("--freq_thin", default=None, type=int, help="factor by which to thin frequency axis")
 args.add_argument("--time_thin", default=None, type=int, help="factor by which to thin time axis")
-args.add_argument("--bl_types", default=None, nargs='*', type=tuple, help="(ant1, ant2) tuples specifying unique baseline types to keep")
+args.add_argument("--bl_types", default=None, nargs='+', type=str, help="space-delimited sequence of ant1,ant2 pairs specifying unique baseline types to keep")
 
 # parse args
 a = args.parse_args()
@@ -46,7 +46,6 @@ for i, f in enumerate(a.files):
     times = np.unique(uvd.time_array)
     antpos, ants = uvd.get_ENU_antpos(pick_data_ants=True)
     antpos = dict(zip(ants, antpos))
-    bls = odict(map(lambda i: ((i[0], i[1]), None), zip(uvd.ant_1_array, uvd.ant_2_array))).keys()
 
     # select antennas
     if a.ants is not None:
@@ -54,14 +53,19 @@ for i, f in enumerate(a.files):
 
     # select bls
     if a.bl_types is not None:
+        bls = odict(map(lambda i: ((i[0], i[1]), None), zip(uvd.ant_1_array, uvd.ant_2_array))).keys()
+        bl_types = map(lambda k: tuple(map(lambda a: int(a), k.split(','))), a.bl_types)
         reds = map(lambda blg: map(lambda bl: bl[:2], blg), hc.redcal.get_reds(antpos))
         keep = []
         for bl_group in reds:
-            for bl in a.bl_types:
+            for bl in bl_types:
                 if (bl in bl_group and bl in bls) or (bl[::-1] in bl_group and bl[::-1] in bls):
                     for b in bl_group:
                         if b in bls or b[::-1] in bls:
                             keep.append(b)
+        if len(keep) == 0:
+            print "no baselines kept, skipping..."
+            continue
 
         uvd.select(ant_pairs_nums=keep)
 
@@ -77,9 +81,6 @@ for i, f in enumerate(a.files):
 
     # write
     print "saving {}".format(fout)
-    uvd.write_miriad(fout)
-
-
-
+    uvd.write_miriad(fout, clobber=True)
 
 
