85be1e0e64d613a671db6f8cf53e92381d1cee5e
James Kent <jck42@cam.ac.uk>
2018-06-05 13:49:21 +0100

    Add averaging/std_dev, as well as flagging/triad list

--- jck/align_lst.py
+++ jck/align_lst.py
@@ -15,9 +15,7 @@ import matplotlib.pyplot as plt
 import os
 import numpy
 import argparse
-
-
-
+import textwrap
 
 
 class LST_Binner(object):
@@ -49,7 +47,14 @@ class LST_Binner(object):
     
     outlst_array  [Numpy array] of shape (no_lsts,no_days)Exact LST's of closures outputted.
 
-    data_array    [Numpy array] of shape (no_lsts,no_days,no_triads,no_channels). Contains closures of shape.
+    data_array    [Numpy array] of shape (no_lsts,no_days,no_triads,no_channels). Contains 
+                  closure phases.
+
+    flag_array    [Numpy array] of shape (no_lsts,no_days,no_triads,no_channels). Contains
+                  individual channel flags from the real time processor(RTP).
+
+    triad_array   [Numpy array] of shape (no_triads,3). Contains antennas which make up the
+                  individual triads.
 
     Member Functions:
 
@@ -71,24 +76,108 @@ class LST_Binner(object):
                  lst_range = 15,
                  triad_no = 26,
                  channels = 1024):
-        
+        """
+        Instantiates the LST_Binner class which takes a numpy array of aligned LST's and
+        a dictionary describing all of our closure data and outputs the items of interest.
+
+        Inputs:
+
+        lst_array     [Numpy Array] Array of aligned LST's. Individual elements used as 
+                      keys for closure_dict.
+
+        closure_dict  [Dictionary] Three-layer dictionary with all the closure information,
+                      as well as flags, triads, days etc.
+
+        lst_start     [Integer] Column index for [lst_array] to output binned LST's from.
+
+        lst_range     [Integer] Range of LST's to bin.
+
+        triad_no      [Integer] Number of triads in dataset.
+
+        channels      [Integer] Number of frequency channels.
+
+        """
+
+        #Parameters
         self.lst_array = lst_array
         self.closure_dict = closure_dict
         self.lst_start = lst_start
         self.lst_range = lst_range
         self.triad_no = triad_no
         self.channels = channels
-
+        #Our output arrays for our LST binned data.
         self.day_array = None
         self.outlst_array = None
         self.data_array = None
-        
-    def bin_lsts(self):
+        self.flag_array = None
+        self.triad_array = None
+        self.averaged_data_array = None
+        self.std_dev_lst = None
+        self.std_dev_triad = None
 
+    def __project_unit_circle(self,closures):
+        """
+        Project angular data onto unit circle / convert to cartesian
+        co-ordinates.
+
+        Inputs:
+
+        closures    [Numpy Array] Binned closures of shape [lst, day,
+                     triad, channel]
+
+        """
+
+        return numpy.cos(closures),numpy.sin(closures)
+
+    def __stddev_circular(self,closures,axis):
+        """
+        Finds the standard deviation of the angular data using the defined
+        statistics for a von Mises distribution on the unit circle, or a 
+        wrapped gaussian.
+
+        Inputs:
+
+        closures    [Numpy Array] Binned closures of shape [lst, day,
+                     triad, channel]
+        axis        [Integer] Which axis of [closures] to find std_dev over.
+        
+        """
+
+
+        closures_x, closures_y = self.__project_unit_circle(closures)
+        averaged_x = numpy.mean(closures_x,axis=axis)
+        averaged_y = numpy.mean(closures_y,axis=axis)
+        r = numpy.sqrt(numpy.square(averaged_x) + numpy.square(averaged_y))
+        return numpy.sqrt(-2 * numpy.log(r))
+
+    def __average_circular(self,closures,axis):
+        """
+        Takes the average angle of angular closure data.
+
+        Inputs:
+
+        closures    [Numpy Array] Binned closures of shape [lst, day,
+                     triad, channel]
+        axis        [Integer] Which axis of [closures] to average over.
+        """
+
+        closures_x, closures_y = self.__project_unit_circle(closures)
+        averaged_x = numpy.mean(closures_x,axis=axis)
+        averaged_y = numpy.mean(closures_y,axis=axis)
+        return numpy.arctan2(averaged_y, averaged_x)
+    
+    def bin_lsts(self,average=False,std_dev=False):
+        
         """
         Takes our binned LST_Array and uses it to index our dictionary of all
         closures. Then extracts the LST's of interest and saves them to 
         day_array, outlst_array, data_array. See class description.
+
+        Inputs:
+
+        average   [Bool] Wether to average across LST's or not.
+        std_dev   [Bool] Wether to calculate std_deviations across both
+                  LST's and Triads(to check redundancy) or not.
         """
 
         # Describes the days in our LST bins
@@ -100,6 +189,15 @@ class LST_Binner(object):
                                         len(self.closure_dict.keys()),
                                         self.triad_no,
                                         self.channels))
+        # All of the RTP flags, aligned with closure phases.
+        self.flag_array = numpy.zeros(shape=(self.lst_range,
+                                        len(self.closure_dict.keys()),
+                                        self.triad_no,
+                                        self.channels),
+                                      dtype=numpy.int8)
+        # Our triads.
+        self.triad_array = numpy.zeros(shape=(self.triad_no, 3))
+        
                                         
         xvar = numpy.arange(0,1024)
         for i, date in enumerate(sorted(self.closure_dict.keys())):
@@ -110,11 +208,24 @@ class LST_Binner(object):
                 
                 lst_index = self.lst_array[i,self.lst_start+lst]
                 self.outlst_array[lst,i] = lst_index 
-                closures_at_lst = self.closure_dict[date][lst_index]
+                closures_at_lst = self.closure_dict[date][lst_index]['phase']
+                flags_at_lst = self.closure_dict[date][lst_index]['flags']
+                
                 #print(numpy.shape(closures_at_lst))
                 #print(lst_index)
 
-                self.data_array[lst,i,:,:] = closures_at_lst 
+                self.data_array[lst,i,:,:] = closures_at_lst
+                self.flag_array[lst,i,:,:] = flags_at_lst
+                self.triad_array = self.closure_dict[date][lst_index]['triads']
+
+        if average == True:
+            self.averaged_data_array = self.__average_circular(self.data_array,1)
+
+        if std_dev == True:
+            self.std_dev_lst = self.__stddev_circular(self.data_array,1)
+            self.std_dev_triad = self.__stddev_circular(self.data_array,2)
+
+        
 
     def save_binned_lsts(self,filename):
 
@@ -127,7 +238,43 @@ class LST_Binner(object):
         """
 
         if self.day_array is not None:
-            numpy.savez(filename, days=self.day_array, last=self.outlst_array, closures=self.data_array)
+            if (self.averaged_data_array is None) and (self.std_dev_lst is None):
+                numpy.savez(filename,
+                            days=self.day_array,
+                            last=self.outlst_array,
+                            closures=self.data_array,
+                            flags=self.flag_array,
+                            triads=self.triad_array)
+            elif (self.averaged_data_array is not None) and (self.std_dev_lst is None):
+                numpy.savez(filename,
+                            days=self.day_array,
+                            last=self.outlst_array,
+                            closures=self.data_array,
+                            flags=self.flag_array,
+                            triads=self.triad_array,
+                            averaged_closures=self.averaged_data_array)
+            elif (self.averaged_data_array is None) and (self.std_dev_lst is not None):
+                numpy.savez(filename,
+                            days=self.day_array,
+                            last=self.outlst_array,
+                            closures=self.data_array,
+                            flags=self.flag_array,
+                            triads=self.triad_array,
+                            std_dev_lst=self.std_dev_lst,
+                            std_dev_triad=self.std_dev_triad)
+            else:
+                numpy.savez(filename,
+                            days=self.day_array,
+                            last=self.outlst_array,
+                            closures=self.data_array,
+                            flags=self.flag_array,
+                            triads=self.triad_array,
+                            averaged_closures=self.averaged_data_array,
+                            std_dev_lst=self.std_dev_lst,
+                            std_dev_triad=self.std_dev_triad)
+                            
+            
+        
         else:
             raise ValueError("LST's not binned")
 
@@ -212,7 +359,9 @@ class LST_Alignment(object):
         which is keyed by both date and LST. Thus the key mechanism is as so:
 
         |
-        | - Date_1 - LST_1
+        | - Date_1 - LST_1 - 'phase' - [Numpy Array]
+        |                  - 'flags' - [Numpy Array]
+        |                  - 'triads' - [Numpy Array]
         |	   - LST_2	
         |
         | - Date_2 - LST_1
@@ -227,10 +376,20 @@ class LST_Alignment(object):
             closure_dict[date] = {}
             for npz_file in npz_files:
                 with numpy.load(self.closure_directory + npz_file) as data:
+
+                    # LST's is used to build the keys of the second tree layer,
+                    # as we want to organise by LST for our alignment.
                     lsts = data['LAST']
+                    
                     phases = data['phase']
+                    flags = data['flags']
+                    triads = data['tr']
                     for i,lst in enumerate(lsts):
-                        closure_dict[date][lst] = phases[:,0,:,i]
+                        closure_dict[date][lst] = {} 
+                        closure_dict[date][lst]['phase'] = phases[:,0,:,i]
+                        closure_dict[date][lst]['flags'] = flags[:,0,:,i]
+                        closure_dict[date][lst]['triads'] = triads
+                        
         return(closure_dict)
 
     def __align_closures(self, reference_lst, closures):
@@ -433,9 +592,32 @@ def main():
 
 
     # Parse directory for input/output.
-    command = argparse.ArgumentParser()
+    command = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent('''
+    ---------------------------------------------------------------------------------------
+    HERA LST Aligner and Binner
+
+    Author: James Kent
+    Institution: University of Cambridge, 2018
+    Email: jck42@cam.ac.uk
+
+    Takes a directory of heracasa created closure phases (best created using recipe), 
+    and concatenates all of the .npz files together, aligns the sidereal times, and 
+    outputs and LST range of your choice.
+
+    Can also optinally average closures across LSTs as well as calculate the standard
+    deviations, which can be helpful in telling you if you there are days/times/triads
+    that are not behaving as wanted.
+'''))
     command.add_argument('filepath', help = ('.npz files generated from miriad files and ran through heracasa'))
     command.add_argument('working_directory', help = ('where aligned .npz files will be put.'))
+    command.add_argument('--average', action='store_true',required=False, 
+                         help='Calculate average across LSTs.')
+    command.add_argument('--std_dev', action='store_true',required=False, 
+                         help='Calculate standard deviations across LSTs.')
+    command.add_argument('--lst_start', required=True, metavar='S', type = int,
+                         help='Start index of LSTs, bit of trial and error to get it right..')
+    command.add_argument('--lst_range', required=True, metavar='R', type = int,
+                         help='Range of LSTs (number of columns) to bin.')
     args = command.parse_args()
 
     if (os.path.exists(args.working_directory) == False):
@@ -459,14 +641,9 @@ def main():
     aligner = LST_Alignment(args.filepath,dates,files)
     aligned_lsts, closures = aligner.align_timestamps()
         
-    #print(aligned_lsts)
-    #print(closures.keys())
-    #print(numpy.shape(aligned_lsts))
-    #print(len(closures.keys()))
-
     # Instantiate LST_binner class class, then bin LSTS and save to file.
-    binner = LST_Binner(aligned_lsts, closures, lst_start=1600, lst_range=20)
-    binner.bin_lsts()
+    binner = LST_Binner(aligned_lsts, closures, lst_start=args.lst_start, lst_range=args.lst_range)
+    binner.bin_lsts(average=args.average,std_dev=args.std_dev)
     binner.save_binned_lsts(args.working_directory+"output_bin.npz")
     
 
