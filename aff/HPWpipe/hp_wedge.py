dad71f3d0b696c5ea23a640f04b6f6b14c32a3ee
Austin Fox Fortino <fortino@sas.upenn.edu>
2018-07-29 00:41:04 -0600

    Just updating the script, still work in progress

--- aff/HPWpipe/hp_wedge.py
+++ aff/HPWpipe/hp_wedge.py
@@ -1,13 +1,13 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 import os
-import copy
 import glob
 import argparse
+import copy
 
 import numpy as np
 import astropy.units as u
@@ -22,215 +22,189 @@ from hera_pspec.data import DATA_PATH
 from pyuvdata import UVData
 
 
-# In[2]:
+# In[3]:
 
 
 parser = argparse.ArgumentParser()
+
 parser.add_argument(
     '-F',
     '--files',
-    help='Designate the hdf5 files to be analyzed.',
+    help='Designate the hdf5 files to be concatenated in time.',
     nargs='*',
     required=True)
 parser.add_argument(
     '-L',
-    '--LSTrange',
+    '--LSTrng',
     help='Designate which LST in hours that will be analyzed (e.g.: "6.0 7.0").',
     type=float,
     nargs=2,
     required=True)
 parser.add_argument(
-    '-C',
-    '--freq_chans',
-    help='Designate which frequency channels that will be analyzed (e.g.: "580 680").',
+    '-X',
+    '--xants',
+    help='Designate which antenna numbers that should be excluded from analysis (e.g.: "0 50 98").',
+    type=int,
+    nargs='*',
+    default=list())
+parser.add_argument(
+    '-R',
+    '--FREQrng',
+    help='Designate the frequency range, in channels, that will be analyzed (e.g.: "580 680").',
     type=int,
     nargs=2,
     required=True)
 parser.add_argument(
-    '-X',
-    '--xants',
-    help='Input a comma delimitted list of antenna numbers that should be excluded from \
-    analysis for the chosen data files (e.g.: "0,50,98").')
-
-
-# In[ ]:
-
+    '-S',
+    '--savepath',
+    help='Designate the path where the new hdf5 files will be saved.',
+    default='./')
 
 """Uncomment this code when running as .py"""
 # args = parser.parse_args()
 # dfiles = np.array(sorted(args.files))
 
-
-# In[3]:
-
-
 """Uncomment this code when running as .ipynb"""
 args = parser.parse_args(
-    "-F /lustre/aoc/projects/hera/H1C_IDR2/IDR2_1/2458111/zen.2458111.?????.xx.HH.uvh5.OCRS \
+    "-F /lustre/aoc/projects/hera/H1C_IDR2/IDR2_1/2458098/zen.2458098.?????.xx.HH.uvh5.OCRS \
     -L 5.0 6.0 \
-    -C 580 680 \
-    -X 0,136,50".split())
+    -X 0 136 50 2 98 137 11 \
+    -R 530 730".split())
 dfiles = sorted(glob.glob(args.files[0]))
+
+LSTrng = args.LSTrng
+xants = sorted(args.xants)
+FREQrng = args.FREQrng
+savepath = args.savepath
+os.system('mkdir -p {}'.format(savepath))
+print 'Saving files to:\n{}'.format(savepath)
+print 'LST Range: Hour {} to Hour {} '.format(LSTrng[0], LSTrng[-1])
+print 'Excluded Antennae: {}'.format(xants)
+print 'Frequency Channel Range: {}'.format(FREQrng)
 
 
 # In[4]:
 
 
-LSTrange = args.LSTrange
-freq_chans = np.arange(args.freq_chans[0], args.freq_chans[-1] + 1)
-if args.xants is not None:
-    xants = sorted([int(xant) for xant in args.xants.split(',')])
-else:
-    xants = []
+get_ipython().run_cell_magic(u'time', u'', u'# Finding the correct files based on the provided LST range\nuvd = UVData()\nfiles = []\ntimes = []\nfor dfile in dfiles:\n    uvd.read_uvh5(dfile, read_data=False)\n    LSTrads = np.unique(uvd.lst_array * u.rad)\n    LSThrs = aco.Angle(LSTrads).hour\n    LSTindices = np.where(np.logical_and(LSThrs >= LSTrng[0], LSThrs <= LSTrng[-1]))[0]\n\n    if LSTindices.size > 0:\n        JDtimes = np.take(np.unique(uvd.time_array), LSTindices)\n        files.append(dfile)\n        times.append(JDtimes.tolist())')
 
 
 # In[5]:
 
 
-uvd = UVData()
-files = []
-times = []
-lsts = []
-for dfile in dfiles:
-    uvd.read_uvh5(dfile, read_data=False)
-    LSTrads = np.unique(uvd.lst_array * u.rad)
-    LSThours = aco.Angle(LSTrads).hour
-    LSTindices = np.where(np.logical_and(LSThours >= LSTrange[0], LSThours <= LSTrange[-1]))[0]
-    
-    if LSTindices.size > 0:
-        JDtimes = np.take(np.unique(uvd.time_array), LSTindices)
-        files.append(dfile)
-        times.append(JDtimes.tolist())
-        lsts.append(LSThours.tolist())
+get_ipython().run_cell_magic(u'time', u'', u"# Loading in the correct data based on the provided LST range\nuvd = UVData()\nuvd.read_uvh5(\n    files[0],\n    ant_str='cross',\n    times=times[0])\nfor file, time in zip(files[1:], times[1:]):\n    uvdi = UVData()\n    uvdi.read_uvh5(\n        file, \n        ant_str='cross',\n        times=time)\n    uvd += uvdi")
 
 
 # In[6]:
 
 
-uvd = UVData()
-uvd.read_uvh5(
-    files[0],
-    ant_str='cross',
-    freq_chans=freq_chans,
-    times=times[0])
-for file, time in zip(files[1:], times[1:]):
-    uvdi = UVData()
-    uvdi.read_uvh5(
-        file, 
-        ant_str='cross',
-        freq_chans=freq_chans,
-        times=time)
-    uvd += uvdi
+get_ipython().run_cell_magic(u'time', u'', u'# Apply flags\nuvd.data_array *= np.logical_not(uvd.flag_array)')
+
+
+# In[7]:
+
+
+get_ipython().run_cell_magic(u'time', u'', u"# Intialize a cosmology and a beam\ncosmo = hp.conversions.Cosmo_Conversions()\nbeamfile = os.path.join(DATA_PATH, 'NF_HERA_Beams.beamfits')\nuvb = hp.pspecbeam.PSpecBeamUV(beamfile, cosmo=cosmo)")
+
+
+# In[8]:
+
+
+get_ipython().run_cell_magic(u'time', u'', u'# Convert to cosmological units (mK)\nJy_to_mK = uvb.Jy_to_mK(np.unique(uvd.freq_array), pol="xx")\nuvd.data_array *= Jy_to_mK[None, None, :, None]')
+
+
+# In[9]:
+
+
+get_ipython().run_cell_magic(u'time', u'', u'# Shift data and load datasets\nuvd1 = uvd.select(times=np.unique(uvd.time_array)[:-1:2], inplace=False)\nuvd2 = uvd.select(times=np.unique(uvd.time_array)[1::2], inplace=False)\nds = hp.PSpecData(dsets=[uvd1, uvd2], wgts=[None, None], beam=uvb)')
+
+
+# In[10]:
+
+
+get_ipython().run_cell_magic(u'time', u'', u"# Set visibility units\nds.dsets[0].vis_units = 'mK'\nds.dsets[1].vis_units = 'mK'")
+
+
+# In[11]:
+
+
+get_ipython().run_cell_magic(u'time', u'', u'# Phase data (What does this do?)\nds.rephase_to_dset(0)')
+
+
+# In[121]:
+
+
+get_ipython().run_cell_magic(u'time', u'', u'# Categorize baselines into physical separation length\nBIN_WIDTH = 0.3\nNORM_BINS = np.arange(0.0, 10000.0, BIN_WIDTH)\n\nantpos = {ant: pos for ant, pos in zip(uvd.get_ENU_antpos()[1], uvd.get_ENU_antpos()[0])}\n\nantpairs = uvd.get_antpairs()\n\nblpairs, blp_reds = [], {}\nbaselines, bls_reds = [], {}\nnorms = []\nfor antpair in antpairs:\n    ant0, ant1 = antpair\n    if (ant0 in xants) or (ant1 in xants) or (ant0 >= ant1):\n        continue\n    baselines.append(antpair)\n    blpair = (antpair, antpair)\n    blpairs.append(blpair)\n    norm = np.linalg.norm(antpos[ant0] - antpos[ant1])\n    norm = np.round(np.digitize(norm, NORM_BINS) * BIN_WIDTH, 1)\n    norms.append(norm)\n\n    if norm in bls_reds:\n        bls_reds[norm].append(antpair)\n        blp_reds[norm].append(blpair)\n    else:\n        bls_reds[norm] = [antpair]\n        blp_reds[norm] = [blpair]\nnorms = sorted(np.unique(norms))')
+
+
+# In[124]:
+
+
+blpairs
 
 
 # In[13]:
 
 
-np.unique(aco.Angle(uvd.lst_array*u.rad).hour)
+get_ipython().run_cell_magic(u'time', u'', u'# Make UVPspec object\nuvp = ds.pspec(\n    baselines,\n    baselines,\n    (0, 1),\n    pols=("xx", "xx"),\n    spw_ranges=[(FREQrng[0], FREQrng[-1])],\n    taper="blackman-harris",\n    verbose=False)')
 
 
-# In[24]:
+# In[14]:
 
 
-uvd.extra_keywords.keys()
+get_ipython().run_cell_magic(u'time', u'', u'# Make UVPspec objects for each redundant baseline group\nuvps = []\nfor norm in norms:\n    uvpi = uvp.select(bls=bls_reds[norm], inplace=False)\n    uvps.append(uvpi)')
 
 
-# In[ ]:
+# In[115]:
 
 
-# Apply flags
-uvd.data_array *= np.logical_not(uvd.flag_array)
+# UVP = copy.deepcopy(uvp)
+# BLP = copy.deepcopy(blp_reds.values())
+# UVP = UVP.average_spectra(blpair_groups=BLP, inplace=False)
 
-# Intialize a cosmology and a beam
-cosmo = hp.conversions.Cosmo_Conversions()
-beamfile = os.path.join(DATA_PATH, 'NF_HERA_Beams.beamfits')
-uvb = hp.pspecbeam.PSpecBeamUV(beamfile, cosmo=cosmo)
+# BLPg = [bl[0] for bl in BLP]
 
-# Convert to cosmological units (mK)
-Jy_to_mK = uvb.Jy_to_mK(np.unique(uvd.freq_array), pol="xx")
-uvd.data_array *= Jy_to_mK[None, None, :, None]
-
-# Shift data and load datasets
-uvd1 = uvd.select(times=np.unique(uvd.time_array)[:-1:2], inplace=False)
-uvd2 = uvd.select(times=np.unique(uvd.time_array)[1::2], inplace=False)
-ds = hp.PSpecData(dsets=[uvd1, uvd2], wgts=[None, None], beam=uvb)
-
-ds.dsets[0].vis_units = 'mK'
-ds.dsets[1].vis_units = 'mK'
-
-
-# In[ ]:
-
-
-# Phase data (What does this do?)
-ds.rephase_to_dset(0)
-
-
-# In[ ]:
+# f, ax = plt.subplots(nrows=1, ncols=1, sharex=True, sharey=True, figsize=(12, 12))
+# hp.plot.delay_spectrum(
+#     UVP,
+#     BLPg,
+#     0,
+#     'xx',
+#     average_blpairs=False,
+#     average_times=True,
+#     fold=True,
+#     plot_noise=True,
+#     delay=False,
+#     deltasq=False,
+#     legend=True,
+#     ax=ax,
+#     component='abs',
+#     lines=True,
+#     markers=False,
+#     error=None)
+# ax.legend(loc='upper right', ncol=3)
 
 
-# Categorize baselines into physical separation length
-BIN_WIDTH = 0.3
-NORM_BINS = np.arange(0.0, 10000.0, BIN_WIDTH)
-
-antpos = {ant: pos for ant, pos in zip(uvd.get_ENU_antpos()[1], uvd.get_ENU_antpos()[0])}
-
-antpairs = uvd.get_antpairs()
-xants1, xants2 = hp.utils.calc_reds(uvd1, uvd2)[3:]
-xants = np.unique(xants1 + xants2)
-
-reds = {}
-for antpair in antpairs:
-    ant0, ant1 = antpair
-    if (ant0 in xants) or (ant1 in xants) or (ant0 >= ant1):
-        continue
-    norm = np.linalg.norm(antpos[ant0] - antpos[ant1])
-    norm = np.round(np.digitize(norm, NORM_BINS) * BIN_WIDTH, 1)
-
-    if norm in reds:
-        reds[norm].append(antpair)
-    else:
-        reds[norm] = [antpair]
-norms = sorted(reds.keys())
-
-
-# In[ ]:
-
-
-# Initialize UVPspec objects for each baseline bin
-uvps = [] 
-for norm in norms:
-    uvp = ds.pspec(
-        reds[norm],
-        reds[norm],
-        (0, 1),
-        pols=("xx", "xx"),
-        spw_ranges=[(0, freq_chans.size)],
-        taper="blackman-harris",
-        verbose=False)
-    uvps.append(uvp)
-
-
-# In[ ]:
+# In[110]:
 
 
 # Average each UVPspec object in time and baseline bin and fold into wedge
 for uvp, norm in zip(uvps, norms):
-    blpairs = [[(bl, bl) for bl in reds[norm]]]
+    blpairs = [[(bl, bl) for bl in bls_reds[norm]]]
     uvp.average_spectra(blpair_groups=blpairs, time_avg=True)
     uvp.fold_spectra()
     uvp.data_array[0] = uvp.data_array[0].reshape(
         (len(uvp.freq_array)))[np.nonzero(uvp.data_array[0].reshape((len(uvp.freq_array))))]
 
 
-# In[ ]:
+# In[111]:
 
 
 # Load data from UVPspec objects into an array
 wedge = np.array([uvp.data_array[0] for uvp in uvps])
 
 
-# In[ ]:
+# In[112]:
 
 
 """Plotting"""
@@ -259,7 +233,7 @@ for i, (uvp, norm, pspec) in enumerate(zip(uvps, norms, wedge)):
         c=cmap(i),
         ls='-',
         lw=1,
-        label='{norm}m ({ants} ants)'.format(norm=norm, ants=len(reds[norm])))
+        label='{norm}m ({bls} bls)'.format(norm=norm, bls=len(bls_reds[norm])))
 
     noise = uvp.generate_noise_spectra(0, 'xx', Tsys)
     noise = noise[noise.keys()[0]]
