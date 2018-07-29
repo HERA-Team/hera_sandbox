f1f7fa81e3412dd9bfce428045b79d5f41a47d21
Austin Fox Fortino <fortino@sas.upenn.edu>
2018-07-29 00:38:55 -0600

    makes only 1 UVPspec object now, much quicker. saves files to a folder depending on the frequency channels chosen

--- aff/HPWpipe/2_HPWpipe.py
+++ aff/HPWpipe/2_HPWpipe.py
@@ -4,11 +4,11 @@
 # In[ ]:
 
 
-"""This script will convert 4 hdf5 files into N*4 hdf5 files containing 
-UVPspec objects where N is the number of redundnant baseline groups."""
+"""This script will take one uvf5 file with a UVData object and turn it into multiple
+uvf5 files with UVPspec objects in them that each represent the power spectra of a group
+of redundant baselines."""
 # Python Standard Library Packages
 import os
-# import copy
 import glob
 import argparse
 
@@ -19,78 +19,139 @@ import numpy as np
 import hera_pspec as hp
 from hera_pspec.data import DATA_PATH
 from pyuvdata import UVData
-from hera_cal import redcal
 
 
 # In[ ]:
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-d",
-    "--day",
-    help="Designate the night of IDR2.1 observation to be analyzed.",
+parser.add_argument(
+    '-F',
+    '--files',
+    help='Designate the hdf5 files to be analyzed.',
+    nargs='*',
     required=True)
-parser.add_argument("-e",
-    "--ext",
-    help="Designate the file extension of the files to be analyzed.",
+parser.add_argument(
+    '-P',
+    '--pols',
+    help='Designate which polarizations to analyze (e.g.: "pI pQ yx yy xx").',
+    nargs='*',
     required=True)
-parser.add_argument("-s",
-    "--spw",
-    help="Designate the spectral window range in channels (i.e., 0_1023).",
-    Nargs=*,
+parser.add_argument(
+    '-R',
+    '--FREQrng',
+    help='Designate the frequency range, in channels, that will be analyzed (e.g.: "580 680").',
+    type=int,
+    nargs=2,
     required=True)
+parser.add_argument(
+    '-S',
+    '--savepath',
+    help='Designate the path where the new hdf5 files will be saved. Default is path to data files.')
+
+
+# In[ ]:
+
+
+"""Uncomment this code when running as .py:"""
 args = parser.parse_args()
-DAY = args.day
-EXT = args.ext
-SPW = [(rng.split('_')[0], rng.split('_')[1]) for rng in args.spw]
+dfiles = np.array(sorted(args.files))
 
 
 # In[ ]:
 
 
-DAY = '2458111'
-EXT = 'uvOCRSD'
-SPW = [(580, 680)]
+"""Uncomment this code when running as .ipynb:"""
+# args = parser.parse_args(
+#     "-F /lustre/aoc/projects/hera/afortino/H1C_IDR2_1/OCRS/{day}/LSThrs_5.0_to_6.0/*OCRS \
+#     -P pI pQ pU pV xx xy yx yy \
+#     -R 530 730".format(day=day).split())
+# dfiles = sorted(glob.glob(args.files[0]))
 
 
 # In[ ]:
 
 
-# Path to hdf5 files
-hdf5_DATA_PATH = '/lustre/aoc/projects/hera/afortino/{}/zen_{}_times_1pol_HH_{}_hdf5/'.format(DAY, DAY, EXT)
-
-# Retrieves hdf5 files
-FILE_SKELETON = '*{}*{}.hdf5'.format(DAY, EXT)
-FILE_SKELETON = os.path.join(hdf5_DATA_PATH, FILE_SKELETON)
-hdf5_FILES = np.array(sorted(glob.glob(FILE_SKELETON)))
-
-# Saves new hdf5 files
-SAVE_PATH = '/lustre/aoc/projects/hera/afortino/{}/zen_{}_times_1pol_PS_{}_hdf5/'.format(DAY, DAY, EXT)
-os.system('mkdir -p {}'.format(SAVE_PATH))
+"""Formatting command line arguments:"""
+pols = sorted(args.pols)
+FREQrng = args.FREQrng
+if args.savepath is None:
+    savepath = os.path.dirname(args.files[0])
+else:
+    savepath = args.savepath
+savepath = os.path.join(savepath, 'FREQrng_{}_to_{}'.format(FREQrng[0], FREQrng[1]))
+os.system('mkdir -p {}'.format(savepath))
+print 'Saving files to:\n{}'.format(savepath)
+print 'Polarizations: {}'.format(pols)
+print 'Frequency Channel Range: {}'.format(FREQrng)
 
 
 # In[ ]:
 
 
-for dfile in hdf5_FILES:
-    print 'Reading: {}'.format(dfile)
-    
-    # Initialize UVData object to read and contain MIRIAD information
+"""Defining pol constants:"""
+STD_POLS = ['xx', 'xy', 'yx', 'yy']
+pS_POLS = ['pI', 'pQ', 'pU', 'pV']
+
+
+# In[ ]:
+
+
+"""Loading input files as UVData objects:"""
+uvds_std_pols = {std_pol: None for std_pol in STD_POLS}
+for dfile in dfiles:
     uvd = UVData()
     uvd.read_uvh5(dfile)
     pol = uvd.get_pols()[0].lower()
+    uvds_std_pols[pol] = uvd
+    del uvd
 
+
+# In[ ]:
+
+
+"""Creating pseudo stokes UVData objects (if requested) and formatting UVdata objects into a dict:"""
+uvds = {pol: None for pol in pols}
+for pol in pols:
+
+    if pol in STD_POLS:
+        uvds[pol] = uvds_std_pols[pol]
+
+    elif pol in pS_POLS:
+        if pol == 'pI':
+            uvdI = hp.pstokes.construct_pstokes(dset1=uvds_std_pols['xx'], dset2=uvds_std_pols['yy'], pstokes='pI')
+            uvds[pol] = uvdI
+        if pol == 'pQ':
+            uvdQ = hp.pstokes.construct_pstokes(dset1=uvds_std_pols['xx'], dset2=uvds_std_pols['yy'], pstokes='pQ')
+            uvds[pol] = uvdQ
+        if pol == 'pU':
+            uvdU = hp.pstokes.construct_pstokes(dset1=uvds_std_pols['xy'], dset2=uvds_std_pols['yx'], pstokes='pU')
+            uvds[pol] = uvdU
+        if pol == 'pV':
+            uvdV = hp.pstokes.construct_pstokes(dset1=uvds_std_pols['xy'], dset2=uvds_std_pols['yx'], pstokes='pV')
+            uvds[pol] = uvdV
+
+
+# In[ ]:
+
+
+"""Making UVPspec objects:"""
+for pol, uvd in uvds.items():
     # Apply flags
     uvd.data_array *= np.logical_not(uvd.flag_array)
 
     # Intialize a cosmology and a beam
+    if pol in STD_POLS:
+        beamfile = os.path.join(DATA_PATH, 'HERA_NF_dipole_power.beamfits')
+    elif pol in pS_POLS:
+        beamfile = os.path.join(DATA_PATH, 'HERA_NF_pstokes_power.beamfits')
     cosmo = hp.conversions.Cosmo_Conversions()
-    beamfile = os.path.join(DATA_PATH, 'NF_HERA_Beams.beamfits')
     uvb = hp.pspecbeam.PSpecBeamUV(beamfile, cosmo=cosmo)
 
     # Convert to cosmological units (mK)
-    Jy_to_mK = uvb.Jy_to_mK(np.unique(uvd.freq_array), pol="xx")
-    uvd.data_array *= Jy_to_mK[None, None, :, None]
+    if ('C' in uvd.extra_keywords['ext']) or ('K' in uvd.extra_keywords['ext']):
+        Jy_to_mK = uvb.Jy_to_mK(np.unique(uvd.freq_array), pol=pol)
+        uvd.data_array *= Jy_to_mK[None, None, :, None]
 
     # Shift data and load datasets
     uvd1 = uvd.select(times=np.unique(uvd.time_array)[:-1:2], inplace=False)
@@ -98,12 +159,13 @@ for dfile in hdf5_FILES:
     ds = hp.PSpecData(dsets=[uvd1, uvd2], wgts=[None, None], beam=uvb)
 
     # Set visibility units
-    ds.dsets[0].vis_units = 'mK'
-    ds.dsets[1].vis_units = 'mK'
-    
+    if ('C' in uvd.extra_keywords['ext']) or ('K' in uvd.extra_keywords['ext']):
+        ds.dsets[0].vis_units = 'mK'
+        ds.dsets[1].vis_units = 'mK'
+
     # Phase data (What does this do?)
     ds.rephase_to_dset(0)
-    
+
     """Categorize baselines into physical separation length"""
     # Setup norm binning
     BIN_WIDTH = 0.3
@@ -114,59 +176,67 @@ for dfile in hdf5_FILES:
 
     # Retrieve antenna pairs and bad antennae
     antpairs = uvd.get_antpairs()
-    xants1, xants2 = hp.utils.calc_reds(uvd1, uvd2)[3:]
-    xants = np.unique(xants1 + xants2)
+    xants = uvd.extra_keywords['xants']
 
     # Sort antenna pairs by their physical separation
-    reds = {}
+    blpairs, blp_reds = [], {}
+    baselines, bls_reds = [], {}
+    norms = []
     for antpair in antpairs:
         ant0, ant1 = antpair
         if (ant0 in xants) or (ant1 in xants) or (ant0 >= ant1):
             continue
+        baselines.append(antpair)
+        blpair = (antpair, antpair)
+        blpairs.append(blpair)
         norm = np.linalg.norm(antpos[ant0] - antpos[ant1])
         norm = np.round(np.digitize(norm, NORM_BINS) * BIN_WIDTH, 1)
+        norms.append(norm)
 
-        if norm in reds:
-            reds[norm].append(antpair)
+        if norm in bls_reds:
+            bls_reds[norm].append(antpair)
+            blp_reds[norm].append(blpair)
         else:
-            reds[norm] = [antpair]
-    norms = sorted(reds.keys())
-    
-    """Make UVPspec objects for each baseline bin"""
-    for norm in norms:
-        print 'Making Power Spectrum for baseline group {}m'.format(norm)
-        uvp = ds.pspec(
-            reds[norm],
-            reds[norm],
-            (0, 1),
-            pols=(pol, pol),
-            spw_ranges=SPW,
-            taper="blackman-harris",
-            verbose=False)
-        hdf5 = '{}m.{}.hdf5'.format(norm, pol)
-        hdf5 = os.path.join(SAVE_PATH, hdf5)
-        uvp.write_hdf5(hdf5, overwrite=True)
+            bls_reds[norm] = [antpair]
+            blp_reds[norm] = [blpair]
+    norms = sorted(np.unique(norms))
+
+    """Make UVPspec object"""
+    uvp = ds.pspec(
+        baselines,
+        baselines,
+        (0, 1),
+        pols=[(pol, pol)],
+        spw_ranges=[(FREQrng[0], FREQrng[-1])],
+        taper="blackman-harris",
+        verbose=False)
+
+    """Name and save UVPspec object"""
+    hdf5 = 'zen.{JD}.{JDt0}_{JDtf}.{pol}.HH.hdf5.{ext}.UVP'.format(
+        JD=uvd.extra_keywords['JD'],
+        JDt0=uvd.extra_keywords['JDt0'],
+        JDtf=uvd.extra_keywords['JDtf'],
+        pol=pol,
+        ext=uvd.extra_keywords['ext'])
+    hdf5 = os.path.join(savepath, hdf5)
+    print 'Writing:'
+    print hdf5
+    uvp.write_hdf5(hdf5, overwrite=True)
 
 
 # In[ ]:
 
 
-JDt0 = uvd.extra_keywords['JDt0']
-JDtf = uvd.extra_keywords['JDtf']
-JD = uvd.extra_keywords['JD']
-numfiles = uvd.extra_keywords['numfiles']
-EXT = uvd.extra_keywords['ext']
-
-metadata = os.path.join(SAVE_PATH, 'metadata.npz')
+"""Saving additional metadata:"""
+metadata = os.path.join(savepath, 'metadata.npz')
 np.savez(
     metadata,
-    JDt0=JDt0,
-    JDtf=JDtf,
-    JD=JD,
-    numfiles=numfiles,
-    EXT=EXT,
-    reds=reds,
+    uvd_extra_keywords=uvd.extra_keywords,
+    bls_reds=bls_reds,
+    blp_reds=blp_reds,
+    baselines=baselines,
+    blpairs=blpairs,
     norms=norms,
     antpos=antpos,
-    xants=xants)
+    FREQrng=FREQrng)
 
